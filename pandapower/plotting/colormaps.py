a0f41170da5d08d9de8fa96a7566fac0dc2a609f
Leon Thurner <leon.thurner@uni-kassel.de>
2016-11-02 15:46:14 +0100

    colorbars

--- pandapower/plotting/colormaps.py
+++ pandapower/plotting/colormaps.py
@@ -1,95 +1,76 @@
 # -*- coding: utf-8 -*-
+"""
+Created on Tue Nov  1 15:11:50 2016
 
-# Copyright (c) 2016 by University of Kassel and Fraunhofer Institute for Wind Energy and Energy
-# System Technology (IWES), Kassel. All rights reserved. Use of this source code is governed by a 
-# BSD-style license that can be found in the LICENSE file.
+@author: thurner
+"""
 
-from matplotlib.colors import LinearSegmentedColormap
+from matplotlib.colors import LinearSegmentedColormap, Normalize, ListedColormap
+import matplotlib.pyplot as plt
+
+def cmap_loading_discrete(cmap_list=[((20, 50), "green"), ((50, 70), "yellow"), 
+                                         ((70, 100), "red")]):
+    cmap_colors = []
+    min_loading = cmap_list[0][0][0]
+    max_loading = cmap_list[-1][0][1]
+    x2_before = None
+    for (x1, x2), color in cmap_list:
+        if x2_before and x2_before != x1:
+            raise ValueError("Ranges for colormap must be continous")
+        cmap_colors += [color]*(x2-x1)
+        x2_before = x2
+    cmap = ListedColormap(cmap_colors)
+    norm = Normalize(min_loading, max_loading)
+    return cmap, norm
+
+def cmap_voltage_discrete(cmap_list=[((0.95, 0.97), "blue"), ((0.97, 1.03), "green"), 
+                                         ((1.03, 1.05), "red")]):
+    cmap_colors = []
+    min_loading = cmap_list[0][0][0]
+    max_loading = cmap_list[-1][0][1]
+    x2_before = None
+    for (x1, x2), color in cmap_list:
+        if x2_before and x2_before != x1:
+            raise ValueError("Ranges for colormap must be continous")
+        cmap_colors += [color]*int((x2-x1)*1000)
+        x2_before = x2
+    cmap = ListedColormap(cmap_colors)
+    norm = Normalize(min_loading, max_loading)
+    return cmap, norm
     
-def set_cmap_line_loading(net, collection, min_loading, max_loading, 
-                          colors=['green', 'yellow', 'orange'], max_color=None, min_color=None, 
-                          title="Line Loading [%]"):
-    """
-    Sets a colormap for a line collection.
+def cmap_loading_continous(cmap_list=[(0, "green"), (50, "yellow"), (100, "red")]):
+    min_loading = cmap_list[0][0]
+    max_loading = cmap_list[-1][0]
+    cmap_colors = [((loading-min_loading)/(max_loading - min_loading), color) for
+                 (loading, color) in cmap_list]
+    cmap = LinearSegmentedColormap.from_list('name', cmap_colors)
+    norm = Normalize(min_loading, max_loading)
+    return cmap, norm
+
+def cmap_voltage_continous(cmap_list=[(0.975, "blue"), (1.0, "green"), (1.03, "red")]):
+    min_voltage = cmap_list[0][0]
+    max_voltage = cmap_list[-1][0]
+    cmap_colors = [((voltage-min_voltage)/(max_voltage - min_voltage), color) for
+                 (voltage, color) in cmap_list]
+    cmap = LinearSegmentedColormap.from_list('name', cmap_colors)
+    norm = Normalize(min_voltage, max_voltage)
+    return cmap, norm
+        
+if __name__ == '__main__':
+    import pandapower as pp
+    import pandapower.plotting as plot
+    import pandapower.networks as nw
+    from matplotlib.colors import LogNorm
+    net = nw.mv_oberrhein("load")
+    pp.runpp(net)
     
-    Input:
-
-        **net** (PandapowerNet) - The pandapower network
-        
-        **collection** (matplotlib collection) - pandapower line collection
-        
-        **min_loading** (float) - lower line loading limit of the colorbar
-
-        **max_loading** (loat) - upper line loading limit of the colorbar
-
-               
-    Optional:
+    cmap = plt.get_cmap('PuBu_r')
+    norm = LogNorm(vmin=20, vmax=100)
+    lc = plot.create_line_collection(net, net.line.index, zorder=1, color="grey", linewidths=2,
+                                     cmap=cmap, norm=norm)
+    bc = plot.create_bus_collection(net, net.bus.index, size=80, zorder=2)
+    plot.draw_collections([lc, bc], figsize=(12,10))
+#    cb = plt.colorbar(lc)
+#    cb.set_ticks([40, 50])
+#    cb.ax.set_ylabel("This is a individual colorbar title")
     
-        **colors** (list) - list of colors for matpolotlib linear segmented colormap
-
-        **max_color** (color) - color for all lines above max_loading
-        
-        **min_color** (color) - color for all lines below min_loading
-
-        **min_color** (color) - color for all lines below min_loading
-        
-        *title** (str) - colorbar title
-    """
-    cmap = LinearSegmentedColormap.from_list('name', colors)
-    collection.set_cmap(cmap)
-    collection.set_array(net.res_line.loading_percent)
-    collection.set_clim(min_loading, max_loading)
-    collection.cbar_title = title
-    collection.has_colormap = True
-    if max_color:
-        cmap.set_over(max_color)
-        collection.extend = "max"
-    if min_color:
-        cmap.set_under(min_color)
-        collection.extend = "min"
-    if max_color and min_color:
-        collection.extend = "both"
-                    
-def set_cmap_bus_voltage(net, collection, colors=["blue", "yellow", "red"], min_voltage=0.95,
-                         max_voltage = 1.05, max_color=None, min_color=None,
-                         title="Bus Voltage [pu]"):
-    """
-    Sets a colormap for a bus collection.
-    
-    Input:
-
-        **net** (PandapowerNet) - The pandapower network
-        
-        **collection** (matplotlib collection) - pandapower bus collection
-        
-        **min_loading** (float) - lower voltage limit of the colorbar
-
-        **max_loading** (loat) - upper voltage limit of the colorbar
-
-               
-    Optional:
-    
-        **colors** (list) - list of colors for matpolotlib linear segmented colormap
-
-        **max_color** (color) - color for all buses above max_voltage
-        
-        **min_color** (color) - color for all buses below min_voltage
-
-        **min_color** (color) - color for all buses below min_voltage
-        
-        *title** (str) - colorbar title
-    """
-    cmap = LinearSegmentedColormap.from_list('name', colors)
-    collection.set_cmap(cmap)
-    collection.set_array(net.res_bus.vm_pu)
-    collection.set_clim(min_voltage, max_voltage)
-    collection.cbar_title = title
-    collection.has_colormap = True
-    if max_color:
-        cmap.set_over(max_color)
-        collection.extend = "max"
-    if min_color:
-        cmap.set_under(min_color)
-        collection.extend = "min"
-    if max_color and min_color:
-        collection.extend = "both"
