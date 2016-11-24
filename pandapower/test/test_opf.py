762a7f202e777da4d71fe0bc535852922762a1ac
smeinecke <smeinecke>
2016-11-24 11:05:15 +0100

    some small improvements of unfinished test_opf

--- pandapower/test/test_opf.py
+++ pandapower/test/test_opf.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 # Copyright (c) 2016 by University of Kassel and Fraunhofer Institute for Wind Energy and Energy
-# System Technology (IWES), Kassel. All rights reserved. Use of this source code is governed by a 
+# System Technology (IWES), Kassel. All rights reserved. Use of this source code is governed by a
 # BSD-style license that can be found in the LICENSE file.
 
 import pandapower as pp
@@ -43,9 +43,9 @@ def test_simplest_voltage():
     logger.debug("res_ext_grid:\n%s" % net.res_ext_grid)
     logger.debug("res_bus.vm_pu: \n%s" % net.res_bus.vm_pu)
     assert max(net.res_bus.vm_pu) < vm_max
-    assert net.OPF_converged
-    
-    
+    assert min(net.res_bus.vm_pu) > vm_min
+
+
 
 def test_eg_voltage():
     """ Testing a very simple network without transformer for voltage
@@ -76,9 +76,8 @@ def test_eg_voltage():
     logger.debug("res_ext_grid:\n%s" % net.res_ext_grid)
     logger.debug("res_bus.vm_pu: \n%s" % net.res_bus.vm_pu)
     assert net.res_bus.vm_pu.at[0] == net.ext_grid.vm_pu.values
-    assert net.OPF_converged
-    
-    
+
+
 def test_simplest_dispatch():
     """ Testing a very simple network without transformer for voltage
     constraints with OPF """
@@ -108,7 +107,7 @@ def test_simplest_dispatch():
     logger.debug("res_est_grid:\n%s" % net.res_ext_grid)
     logger.debug("res_bus.vm_pu: \n%s" % net.res_bus.vm_pu)
     assert max(net.res_bus.vm_pu) < vm_max
-    assert net.OPF_converged
+    assert min(net.res_bus.vm_pu) > vm_min
 
 
 
@@ -154,7 +153,6 @@ def test_opf_gen_voltage():
     logger.debug("res_bus.vm_pu: \n%s" % net.res_bus.vm_pu)
     assert max(net.res_bus.vm_pu) < vm_max
     assert min(net.res_bus.vm_pu) > vm_min
-    assert net.OPF_converged
 
 
 def test_opf_sgen_voltage():
@@ -198,7 +196,7 @@ def test_opf_sgen_voltage():
     logger.debug("res_sgen:\n%s" % net.res_sgen)
     logger.debug("res_bus.vm_pu: \n%s" % net.res_bus.vm_pu)
     assert max(net.res_bus.vm_pu) < vm_max
-    assert net.OPF_converged
+    assert min(net.res_bus.vm_pu) > vm_min
 
 
 def test_opf_gen_loading():
@@ -245,7 +243,8 @@ def test_opf_gen_loading():
     assert max(net.res_line.loading_percent) < max_line_loading
     logger.debug("res_trafo.loading_percent:\n%s" % net.res_trafo.loading_percent)
     assert max(net.res_trafo.loading_percent) < 145
-    assert net.OPF_converged
+    assert max(net.res_bus.vm_pu) < vm_max
+    assert min(net.res_bus.vm_pu) > vm_min
 
 
 def test_opf_sgen_loading():
@@ -291,7 +290,8 @@ def test_opf_sgen_loading():
     assert max(net.res_line.loading_percent) < max_line_loading
     logger.debug("res_trafo.loading_percent:\n%s" % net.res_trafo.loading_percent)
     assert max(net.res_trafo.loading_percent) < max_trafo_loading
-    assert net.OPF_converged
+    assert max(net.res_bus.vm_pu) < vm_max
+    assert min(net.res_bus.vm_pu) > vm_min
 
 #def test_opf_oberrhein():
 #    """ Testing a  simple network with transformer for loading
@@ -312,6 +312,10 @@ def test_opf_sgen_loading():
 #    # run OPF
 #    pp.runopp(net, verbose=False)
 ##    assert net["OPF_converged"]
+
+
+#def test_linear_minloss_cost_fnc():
+
 
 if __name__ == "__main__":
     """ test for optimal power flow using default cost function "maxp"
