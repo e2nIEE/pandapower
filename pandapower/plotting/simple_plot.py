36b136e2ab008e0821270dfc46519e7c3837c0d4
smeinecke <smeinecke>
2016-12-19 14:45:53 +0100

    pep8

--- pandapower/plotting/simple_plot.py
+++ pandapower/plotting/simple_plot.py
@@ -13,9 +13,9 @@ except:
 
 logger = logging.getLogger(__name__)
 
-def simple_plot(net=None, respect_switches=False
-                , line_width=1.0, bus_size=None, ext_grid_size=None
-                , bus_color=colors[0], line_color='grey', trafo_color = 'g', ext_grid_color='y'):
+
+def simple_plot(net=None, respect_switches=False, line_width=1.0, bus_size=None, ext_grid_size=None,
+                bus_color=colors[0], line_color='grey', trafo_color='g', ext_grid_color='y'):
     if net is None:
         import pandapower.networks as nw
         logger.warning("No Pandapower network provided -> Plotting mv_oberrhein")
@@ -23,14 +23,18 @@ def simple_plot(net=None, respect_switches=False
 
     # create geocoord if none are available
     if len(net.line_geodata) == 0 and len(net.bus_geodata) == 0:
-        logger.warning("No or insufficient geodata available --> Creating artificial coordinates. This may take some time")
+        logger.warning("No or insufficient geodata available --> Creating artificial coordinates." +
+                       " This may take some time")
         plot.create_generic_coordinates(net, respect_switches=respect_switches)
 
     if bus_size or ext_grid_size is None:
-        # if either bus_size or ext_grid size is None -> calc size from distance between min and max geocoord
-        mean_distance_between_buses = sum((net['bus_geodata'].max() - net['bus_geodata'].min()) / 200)
+        # if either bus_size or ext_grid size is None -> calc size from distance between min and
+        # max geocoord
+        mean_distance_between_buses = sum((net['bus_geodata'].max() - net[
+            'bus_geodata'].min()) / 200)
         # set the bus / ext_grid sizes accordingly
-        # Comment: This is implemented because if you would choose a fixed values (e.g. bus_size = 0.2), the size
+        # Comment: This is implemented because if you would choose a fixed values
+        # (e.g. bus_size = 0.2), the size
         # could be to small for large networks and vice versa
         if bus_size is None:
             bus_size = mean_distance_between_buses
@@ -45,8 +49,10 @@ def simple_plot(net=None, respect_switches=False
 
     # create bus collections ti plot
     bc = plot.create_bus_collection(net, net.bus.index, size=bus_size, color=bus_color, zorder=10)
-    lc = plot.create_line_collection(net, net.line.index, color=line_color, linewidths=line_width, use_line_geodata=use_line_geodata)
-    sc = plot.create_bus_collection(net, net.ext_grid.bus.values, patch_type="rect", size=ext_grid_size, color=ext_grid_color, zorder=11)
+    lc = plot.create_line_collection(net, net.line.index, color=line_color, linewidths=line_width,
+                                     use_line_geodata=use_line_geodata)
+    sc = plot.create_bus_collection(net, net.ext_grid.bus.values, patch_type="rect",
+                                    size=ext_grid_size, color=ext_grid_color, zorder=11)
     # create trafo collection if trafo is available
     if len(net.trafo):
         tc = plot.create_trafo_collection(net, net.trafo.index, color=trafo_color)
