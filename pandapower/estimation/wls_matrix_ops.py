b09ad90d16353f21c612b575e8f14b1a01cfcc5e
Leon Thurner <leon.thurner@uni-kassel.de>
2016-12-20 11:39:57 +0100

    py 2/3 compatibility

--- pandapower/estimation/wls_matrix_ops.py
+++ pandapower/estimation/wls_matrix_ops.py
@@ -1,8 +1,12 @@
-__author__ = 'menke'
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2016 by University of Kassel and Fraunhofer Institute for Wind Energy and Energy
+# System Technology (IWES), Kassel. All rights reserved. Use of this source code is governed by a 
+# BSD-style license that can be found in the LICENSE file.
+
 import numpy as np
 import warnings
 from pypower.makeYbus import makeYbus
-
 
 class wls_matrix_ops:
     def __init__(self, ppc, slack_buses, non_slack_buses, s_ref, bs_cols, br_cols):
