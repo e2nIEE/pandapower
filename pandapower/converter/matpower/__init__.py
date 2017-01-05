78e9b78c12be7641159b008accecbb2b6744a4b5
smeinecke <smeinecke>
2017-01-05 11:21:19 +0100

    rename old matpower_converter remove test.m.at

--- pandapower/converter/matpower/__init__.py
+++ pandapower/converter/matpower/__init__.py
@@ -1,4 +1,2 @@
-from __future__ import absolute_import
-
-from .matpower_converter import *
+from .to_ppc import *
 from .mpc_to_ppc import mpc_to_ppc
--- pandapower/converter/pypower/__init__.py
+++ pandapower/converter/pypower/__init__.py
@@ -1,3 +1,1 @@
-from __future__ import absolute_import
-
 from .from_ppc import *
