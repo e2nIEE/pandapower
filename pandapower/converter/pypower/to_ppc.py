7aff6cebda54f9db1edc3c7a637fc5dae610eb8b
smeinecke <smeinecke>
2017-01-10 09:44:20 +0100

    remove un usefull parameters in to_ppc

--- pandapower/converter/pypower/to_ppc.py
+++ pandapower/converter/pypower/to_ppc.py
@@ -13,44 +13,13 @@ except:
 logger = logging.getLogger(__name__)
 
 
-def to_ppc(net, init="results", calculate_voltage_angles=False, trafo_model="t"):
+def to_ppc(net):
     """
      This function converts a pandapower net to a pypower case file.
 
     INPUT:
 
         **net** - The pandapower net.
-
-    OPTIONAL:
-
-        **init** (str, "results") - initialization method of the loadflow
-        For the conversion to a ppc, the following options can be chosen:
-
-            - "flat"- flat start with voltage of 1.0pu and angle of 0° at all buses as initial solution
-            - "results" - voltage vector of last loadflow from net.res_bus is copied to the ppc
-
-        **calculate_voltage_angles** (bool, False) - copy the voltage angles from pandapower to the ppc
-
-            If True, voltage angles are copied from pandapower to the ppc. In some cases with
-            large differences in voltage angles (for example in case of transformers with high
-            voltage shift), the difference between starting and end angle value is very large.
-            In this case, the loadflow might be slow or it might not converge at all. That is why
-            the possibility of neglecting the voltage angles of transformers and ext_grids is
-            provided to allow and/or accelarate convergence for networks where calculation of
-            voltage angles is not necessary.
-
-            The default value is False because pandapower was developed for distribution networks.
-            Please be aware that this parameter has to be set to True in meshed network for correct
-            results!
-
-        **trafo_model** (str, "t")  - transformer equivalent circuit model
-        Pandapower provides two equivalent circuit models for the transformer:
-
-            - "t" - transformer is modelled as equivalent with the T-model. This is consistent with PowerFactory
-                and is also more accurate than the PI-model. We recommend using this transformer model.
-            - "pi" - transformer is modelled as equivalent PI-model. This is consistent with Sincal,
-                but the method is questionable since the transformer is physically T-shaped. We therefore
-                recommend the use of the T-model.
 
     OUTPUT:
 
@@ -68,15 +37,19 @@ def to_ppc(net, init="results", calculate_voltage_angles=False, trafo_model="t")
 
     """
 
-    # convert to matpower
-    net["converged"] = False
-    if not init == "results":
-        reset_results(net)
+    # always convert results if available
+    init_results = True
 
-    # select elements in service (time consuming, so we do it once)
+    # matpower and pypower uses pi trafo model
+    trafo_model = "pi"
+
+    # copy the voltage angles from pandapower to the ppc
+    calculate_voltage_angles = True
+
+    # select elements in service
     is_elems = _select_is_elements(net)
 
-    init_results = True if init == "results" else False
+    #  do the conversion
     ppc, ppci, bus_lookup = _pd2ppc(net, is_elems, calculate_voltage_angles, enforce_q_lims=False,
                                     trafo_model=trafo_model, init_results=init_results,
                                     copy_voltage_boundaries=True)
