381acb293522b32386d06f3b93d002d8933feb22
Leon Thurner <leon.thurner@uni-kassel.de>
2017-02-16 09:15:13 +0100

    numpy 1.10

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     url='http://www.uni-kassel.de/go/pandapower',
     license='BSD',
     install_requires=["pypower>=5.0.1",
-                      "numpy",
+                      "numpy>=1.10.0",
                       "scipy",
                       "pandas>=0.17.0",
                       "networkx"],
