0abc32c3e7dc2e40a8e2d3ac5185dd686d3a4aaa
Leon Thurner <leon.thurner@uni-kassel.de>
2017-04-19 18:41:02 +0200

    motors for short circuit calculation

--- pandapower/shortcircuit/impedance.py
+++ pandapower/shortcircuit/impedance.py
@@ -20,21 +20,25 @@ except ImportError:
 def _calc_equiv_sc_impedance(net, ppc):
     r_fault = net["_options"]["r_fault_ohm"]
     x_fault = net["_options"]["x_fault_ohm"]
-    zbus = _calc_zbus(ppc)
+    Ybus = _calc_ybus(ppc)
+    Zbus = _calc_zbus(Ybus)
     if r_fault > 0 or x_fault > 0:
         base_r = np.square(ppc["bus"][:, BASE_KV]) / ppc["baseMVA"]
         fault_impedance = (r_fault + x_fault * 1j) / base_r
-        np.fill_diagonal(zbus, zbus.diagonal() + fault_impedance)
-    z_equiv = np.diag(zbus)
+        np.fill_diagonal(Zbus, Zbus.diagonal() + fault_impedance)
+    z_equiv = np.diag(Zbus)
     ppc["bus_sc"][:, R_EQUIV] = z_equiv.real
     ppc["bus_sc"][:, X_EQUIV] = z_equiv.imag
-    ppc["internal"]["zbus"] = zbus
+    ppc["internal"]["Zbus"] = Zbus
 
-def _calc_zbus(ppc):
+def _calc_ybus(ppc):
     Ybus, Yf, Yt = makeYbus(ppc["baseMVA"], ppc["bus"],  ppc["branch"])
     ppc["internal"]["Yf"] = Yf
     ppc["internal"]["Yt"] = Yt
     ppc["internal"]["Ybus"] = Ybus
+    return Ybus
+
+def _calc_zbus(ybus):
     with warnings.catch_warnings():
         warnings.simplefilter("ignore")
-        return inv(Ybus).toarray()
+        return inv(ybus).toarray()
