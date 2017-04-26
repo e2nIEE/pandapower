2c11b6607e546f3e7246bf4678e77de595d6ed62
Steffen Meinecke <steffen.meinecke@uni-kassel.de>
2017-04-26 09:21:48 +0200

    simple_plot: bug fix of scaling condition for bus_size and ext_grid_size

--- pandapower/plotting/simple_plot.py
+++ pandapower/plotting/simple_plot.py
@@ -26,7 +26,8 @@ logger = logging.getLogger(__name__)
 
 
 def simple_plot(net=None, respect_switches=False, line_width=1.0, bus_size=1.0, ext_grid_size=1.0,
-                bus_color=colors[0], line_color='grey', trafo_color='g', ext_grid_color='y'):
+                scale_size=True, bus_color=colors[0], line_color='grey', trafo_color='g',
+                ext_grid_color='y'):
     """
         Plots a pandapower network as simple as possible. If no geodata is available, artificial
         geodata is generated. For advanced plotting see the tutorial
@@ -35,7 +36,7 @@ def simple_plot(net=None, respect_switches=False, line_width=1.0, bus_size=1.0, 
             **net** - The pandapower format network.
 
         OPTIONAL:
-            **respect_switches** (bool, False) - Respect switches when artificial geodata is created
+            **respect_switches** (bool, False) - Respect switches if artificial geodata is created
 
             **line_width** (float, 1.0) - width of lines
 
@@ -50,6 +51,9 @@ def simple_plot(net=None, respect_switches=False, line_width=1.0, bus_size=1.0, 
             **ext_grid_size** (float, 1.0) - Relative size of ext_grids to plot.
 
                 See bus sizes for details. Note: ext_grids are plottet as rectangles
+
+            **scale_size** (bool, True) - Flag if bus_size and ext_grid_size will be scaled with
+            respect to grid mean distances
 
             **bus_color** (String, colors[0]) - Bus Color. Init as first value of color palette.
             Usually colors[0] = "b".
@@ -71,9 +75,8 @@ def simple_plot(net=None, respect_switches=False, line_width=1.0, bus_size=1.0, 
                        " This may take some time")
         create_generic_coordinates(net, respect_switches=respect_switches)
 
-    if bus_size or ext_grid_size is None:
-        # if either bus_size or ext_grid size is None -> calc size from distance between min and
-        # max geocoord
+    if scale_size:
+        # if scale_size -> calc size from distance between min and max geocoord
         mean_distance_between_buses = sum((net['bus_geodata'].max() - net[
             'bus_geodata'].min()) / 200)
         # set the bus / ext_grid sizes accordingly
