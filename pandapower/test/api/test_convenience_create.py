920fb0e492a1863a85c54f8a41d6adcc46dddee4
Leon Thurner <leon.thurner@uni-kassel.de>
2017-05-04 16:46:08 +0200

    test convenience create functions fixes #25

--- pandapower/test/api/test_convenience_create.py
+++ pandapower/test/api/test_convenience_create.py
@@ -18,7 +18,7 @@ def test_convenience_create_functions():
                                    x_ohm_per_km=0.1382301, c_nf_per_km=160., max_i_ka=0.664)
 
     l0 = pp.create_load_from_cosphi(net, b2, 10e3, 0.95, "ind", name="load")
-    pp.runpp(net)
+    pp.runpp(net, init="flat")
     assert net.load.p_kw.at[l0] == 9.5e3
     assert net.load.q_kvar.at[l0] > 0
     assert np.sqrt(net.load.p_kw.at[l0]**2 +  net.load.q_kvar.at[l0]**2) == 10e3
@@ -26,17 +26,18 @@ def test_convenience_create_functions():
     assert net.load.name.at[l0] == "load"
 
     sh0 = pp.create_shunt_as_condensator(net, b2, 10e3, loss_factor=0.01, name="shunt")
-    pp.runpp(net)
+    pp.runpp(net, init="flat")
     assert np.isclose(net.res_shunt.q_kvar.at[sh0], -10,043934174e3)
     assert np.isclose(net.res_shunt.p_kw.at[sh0], 100.43933665)
     assert np.isclose(net.res_bus.vm_pu.at[b2], 1.0021942964)
     assert net.shunt.name.at[sh0] == "shunt"
 
     sg0 = pp.create_sgen_from_cosphi(net, b2, 5e3, 0.95, "cap", name="sgen")
+    pp.runpp(net, init="flat")
     assert np.sqrt(net.sgen.p_kw.at[sg0]**2 +  net.sgen.q_kvar.at[sg0]**2) == 5e3
     assert net.sgen.p_kw.at[sg0] == -4.75e3
     assert net.sgen.q_kvar.at[sg0] < 0
-    assert np.isclose(net.res_bus.vm_pu.at[b2], 1.0021942964)
+    assert np.isclose(net.res_bus.vm_pu.at[b2], 1.0029376578)
     assert net.sgen.name.at[sg0] == "sgen"
 
 if __name__ == '__main__':
