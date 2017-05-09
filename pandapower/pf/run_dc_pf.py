8982faec8e89ce594c3a451d81f667f8804be678
Leon Thurner <leon.thurner@uni-kassel.de>
2017-05-09 17:15:12 +0200

    license files

--- pandapower/pf/run_dc_pf.py
+++ pandapower/pf/run_dc_pf.py
@@ -1,3 +1,10 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2016-2017 by University of Kassel and Fraunhofer Institute for Wind Energy and
+# Energy System Technology (IWES), Kassel. All rights reserved. Use of this source code is governed
+# by a BSD-style license that can be found in the LICENSE file.
+
+
 from time import time
 
 from numpy import flatnonzero as find, pi, exp, zeros, ones, real
