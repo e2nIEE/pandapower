77cb8fc51a760c41f5a15f22f1ff00e18aa29b07
mdipierro <massimo.dipierro@gmail.com>
2017-05-14 21:41:55 -0500

    more customizable js graph

--- pandapower/html.py
+++ pandapower/html.py
@@ -29,10 +29,10 @@ class Tag(object):
         contents = ''.join(a.html if isinstance(a, Raw) else escape(str(a)) for a in args)
         return Raw('<%s%s>%s</%s>' % (self.name, attr.rstrip(), contents, self.name))
 
-
-def _net_to_html(net, respect_switches, include_lines, include_trafos, show_tables):
+def _net_to_graphjs(net, respect_switches=True, include_lines=True, include_trafos=True, images=None):
     """
-     Converts a pandapower network into an html page which contains a simplified representation
+     Converts a pandapower network into a graph for vis.js 
+     which contains a simplified representation
      of a network's topology, reduced to nodes and edges. Busses are being represented by nodes
      (Note: only buses with in_service = 1 appear in the graph), edges represent physical
      connections between buses (typically lines or trafos).
@@ -54,58 +54,109 @@ def _net_to_html(net, respect_switches, include_lines, include_trafos, show_tabl
 
      EXAMPLE:
 
-         from pandapower.html import to_html
-         html = to_html(net, respect_switches = False)
-         open('C:\\index.html', "w").write(html)
-
+         from pandapower.html import _net_to_graphjs
+         print _net_to_graph(net, respect_switches = False)
     """
 
     nodes = [{'id': int(id), 'label': str(id)} for id in net.bus[net.bus.in_service == 1].index]
     edges = []
 
+    if images:
+        for node in nodes:
+            node['shape'] = 'image'
+            node['image'] = images.get(str(node['id']), images['default'])
+
     if include_lines:
         # lines with open switches can be excluded
         nogolines = set(net.switch.element[(net.switch.et == "l") & (net.switch.closed == 0)]) \
             if respect_switches else set()
+
         edges += [{'from': int(fb),
                    'to': int(tb),
-                   'label': 'weight %f, key: %i, type %s, capacity: %f, path: %i' %
-                            (l, idx, 'l', imax, 1)}
-                  for fb, tb, l, idx, inservice, imax in
+                   'label': '%.2f Mw %.2f Km' % (imax, length)}
+                  for fb, tb, length, idx, inservice, imax in
                   list(zip(net.line.from_bus, net.line.to_bus, net.line.length_km,
                            net.line.index, net.line.in_service, net.line.max_i_ka))
-                  if inservice == 1 and not idx in nogolines]
-        edges += [{'from': int(fb),
-                   'to': int(tb),
-                   'label': 'key: %i, type %s, path: %i' % (idx, 'i', 1)}
+                  if inservice == 1 and not idx in nogolines]        
+
+        edges += [{'from': int(fb), 'to': int(tb)}
                   for fb, tb, idx, inservice in
                   list(zip(net.impedance.from_bus, net.impedance.to_bus,
                            net.impedance.index, net.impedance.in_service))
                   if inservice == 1]
 
+        edges += [{'from': int(fb),
+                   'to': int(tb), 'dashes': 'true',
+                   'label': '%.2f Mw %.2f Km' % (imax, length)}
+                  for fb, tb, length, idx, inservice, imax in
+                  list(zip(net.line.from_bus, net.line.to_bus, net.line.length_km,
+                           net.line.index, net.line.in_service, net.line.max_i_ka))
+                  if inservice != 1 or idx in nogolines]        
+
+        edges += [{'from': int(fb), 'to': int(tb), 'dashes': 'true'}
+                  for fb, tb, idx, inservice in
+                  list(zip(net.impedance.from_bus, net.impedance.to_bus,
+                           net.impedance.index, net.impedance.in_service))
+                  if inservice != 1]
+
     if include_trafos:
         nogotrafos = set(net.switch.element[(net.switch.et == "t") & (net.switch.closed == 0)])
-        edges += [{'from': int(hvb),
-                   'to': int(lvb),
-                   'label': 'weight %f, key: %i, type %s' % (0, idx, 't')}
+        edges += [{'from': int(hvb), 'to': int(lvb), 'label': 'trafo'}
                   for hvb, lvb, idx, inservice in
                   list(zip(net.trafo.hv_bus, net.trafo.lv_bus,
                            net.trafo.index, net.trafo.in_service))
                   if inservice == 1 and not idx in nogotrafos]
         for trafo3, t3tab in net.trafo3w.iterrows():
-            edges += [{'from': int(bus1),
-                       'to': int(bus2),
-                       'label': 'weight %f, key: %i, type %s' % (0, trafo3, 't3')}
+            edges += [{'from': int(bus1),'to': int(bus2),'label': 'trafo3'}
                       for bus1, bus2 in combinations([t3tab.hv_bus, t3tab.mv_bus, t3tab.lv_bus], 2)
                       if t3tab.in_service]
 
     # add bus-bus switches
     bs = net.switch[(net.switch.et == "b") &
                     ((net.switch.closed == 1) | (not respect_switches))]
-    edges += [{'from': int(b),
-               'to': int(e),
-               'label': 'weight %f, key: %i, type %s' % (0, i, 's')}
+    edges += [{'from': int(b), 'to': int(e), 'label': 'switch'}
               for b, e, i in list(zip(bs.bus, bs.element, bs.index))]
+
+    script = "var data = {nodes: new vis.DataSet(%s), edges: new vis.DataSet(%s)};" % (
+        json.dumps(nodes), json.dumps(edges))
+    script += "var container = document.getElementById('net');"
+    script += "var network = new vis.Network(container, data);"
+    script += "network.setOptions({interaction: {zoomView: false}});"
+
+    return script
+
+
+def _net_to_html(net, respect_switches=True, include_lines=True, include_trafos=True, show_tables=True, images=None):
+    """
+     Converts a pandapower network into an html page which contains a simplified representation
+     of a network's topology, reduced to nodes and edges. Busses are being represented by nodes
+     (Note: only buses with in_service = 1 appear in the graph), edges represent physical
+     connections between buses (typically lines or trafos).
+
+     INPUT:
+        **net** (pandapowerNet) - variable that contains a pandapower network
+
+
+     OPTIONAL:
+        **respect_switches** (boolean, True) - True: open line switches are being considered
+                                                     (no edge between nodes)
+                                               False: open line switches are being ignored
+
+        **include_lines** (boolean, True) - determines, whether lines get converted to edges
+
+        **include_trafos** (boolean, True) - determines, whether trafos get converted to edges
+
+        **show_tables** (boolean, True) - shows pandapower element tables
+
+        **images** (dict) - an optional dict of images to map a node id into an image
+
+     EXAMPLE:
+
+         from pandapower.html import to_html
+         html = to_html(net, respect_switches = False)
+         open('C:\\index.html', "w").write(html)
+
+    """
 
     HTML, HEAD, STYLE, BODY, DIV = Tag('html'), Tag('head'), Tag('style'), Tag('body'), Tag('div')
     TABLE, TR, TH, TD, SCRIPT = Tag('table'), Tag('tr'), Tag('th'), Tag('td'), Tag('script')
@@ -113,15 +164,12 @@ def _net_to_html(net, respect_switches, include_lines, include_trafos, show_tabl
 
     style = 'tr:first {background:#e1e1e1;} th,td {text-align:center; border:1px solid #e1e1e1;}'
 
-    script = "var data = {nodes: new vis.DataSet(%s), edges: new vis.DataSet(%s)};" % (
-        json.dumps(nodes), json.dumps(edges))
-    script += "var container = document.getElementById('net');"
-    script += "var network = new vis.Network(container, data, {zoomable: false});"
+    script = _net_to_graphjs(net, respect_switches, include_lines, include_trafos,  images)
 
     tables = []
     if show_tables:
-        for name in ['bus', 'trafo', 'line', 'load', 'ext_grid',
-                     'res_bus', 'res_trafo', 'res_line', 'res_load', 'res_ext_grid']:
+        for name in ['bus', 'trafo', 'line', 'load', 'ext_grid', 'gen',
+                     'res_bus', 'res_trafo', 'res_line', 'res_load', 'res_ext_grid', 'res_gen']:
             item = getattr(net, name)
             table = TABLE(TR(*map(TH, item.columns)),
                           *[TR(*map(TD, row)) for row in item.values])
