ed822ceba91f6f3d22a063b2d1eeacff282e7786
Steffen Meinecke <steffen.meinecke@uni-kassel.de>
2017-08-23 14:08:23 +0200

    remove old comment

--- pandapower/create.py
+++ pandapower/create.py
@@ -2375,12 +2375,6 @@ def create_polynomial_cost(net, element, element_type, coefficients, type="p", i
             (net["piecewise_linear_cost"].type == type)].empty:
         raise UserWarning("A piecewise_linear_cost for %s with index %s already exists" %
                           (element_type, element))
-# =======
-#     typecosts=  net["polynomial_cost"][net["polynomial_cost"].element_type == element_type]
-#
-#     if not typecosts[typecosts.type == type].loc[typecosts.element == element].empty:
-#         raise UserWarning("A polynomial_cost for this element already exists")
-# >>>>>>> Need to commit for merging my changes to make_objective with steffens changes.
 
     net.polynomial_cost.loc[index, ["type", "element", "element_type"]] = \
         [type, element, element_type]
