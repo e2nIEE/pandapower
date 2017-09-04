43d278bba2790cce6201c0f95791c969d8e6e21d
Alexander Scheidler <alexander.scheidler@iwes.fraunhofer.de>
2017-09-04 15:29:28 +0200

    round corners for line collections

--- pandapower/plotting/__init__.py
+++ pandapower/plotting/__init__.py
@@ -6,3 +6,16 @@ from pandapower.plotting.simple_plot import *
 from pandapower.plotting.plotly import *
 from pandapower.plotting.geo import *
 from pandapower.plotting.to_html import to_html
+
+import types
+from matplotlib.backend_bases import GraphicsContextBase, RendererBase
+
+class GC(GraphicsContextBase):
+    def __init__(self):
+        super().__init__()
+        self._capstyle = 'round'
+
+def custom_new_gc(self):
+    return GC()
+
+RendererBase.new_gc = types.MethodType(custom_new_gc, RendererBase)
