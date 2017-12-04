77b9818bc1a8753e33995667d9306976c02099f5
Leon Thurner <leon.thurner@uni-kassel.de>
2017-12-04 23:54:58 +0100

    legacy compatibiltiy pandas

--- pandapower/file_io.py
+++ pandapower/file_io.py
@@ -238,7 +238,11 @@ def from_pickle(filename, convert=True):
                 # TODO: is this legacy code?
                 net[key] = pd.DataFrame.from_dict(df_dict)
                 if "columns" in item:
-                    net[key] = net[key].reindex(item["columns"], axis=1)
+                    try:
+                        net[key] = net[key].reindex(item["columns"], axis=1)
+                    except: #legacy for pandas <0.21
+                        net[key] = net[key].reindex_axis(item["columns"], axis=1)
+                      
 
             if "dtypes" in item:
                 if "columns" in df_dict and "geometry" in df_dict["columns"]:
