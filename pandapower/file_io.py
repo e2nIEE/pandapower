b6a0efbaed4cd5f1c527cffa92fb65a1c88a7457
Florian Schaefer <florian.schaefer@uni-kassel.de>
2017-12-04 15:47:16 +0100

    :bug: partlyfix #83 file_io try expect changed...

--- pandapower/file_io.py
+++ pandapower/file_io.py
@@ -278,11 +278,11 @@ def from_excel(filename, convert=True):
     if not os.path.isfile(filename):
         raise UserWarning("File %s does not exist!" % filename)
     try:
-        # pandas 0.21
-        xls = pd.ExcelFile(filename).parse(sheet_name=None)
-    except:
         # pandas < 0.21
         xls = pd.ExcelFile(filename).parse(sheetname=None)
+    except:
+        # pandas 0.21
+        xls = pd.ExcelFile(filename).parse(sheet_name=None)
 
     try:
         net = from_dict_of_dfs(xls)
