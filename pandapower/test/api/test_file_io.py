722c57600fb7125d05675843674988030ed38e4d
rbolgaryn <roman.bolgaryn@iwes.fraunhofer.de>
2018-03-05 18:48:15 +0100

    test for failing from_json with unknown parameters

--- pandapower/test/api/test_file_io.py
+++ pandapower/test/api/test_file_io.py
@@ -6,10 +6,11 @@
 
 import os
 import pytest
-
+import pandas as pd
 
 import pandapower as pp
 from pandapower.test.toolbox import assert_net_equal, create_test_network, tempdir, net_in
+from pandapower.io_utils import collect_all_dtypes_df, restore_all_dtypes
 
 
 def test_pickle(net_in, tempdir):
@@ -39,11 +40,42 @@ def test_sqlite(net_in, tempdir):
     net_out = pp.from_sqlite(filename)
     assert_net_equal(net_in, net_out)
 
+
 def test_convert_format():  # TODO what is this thing testing ?
     folder = os.path.abspath(os.path.dirname(pp.__file__))
     net = pp.from_pickle(os.path.join(folder, "test", "api", "old_net.p"))
     pp.runpp(net)
     assert net.converged
+
+
+def test_restore_all_dtypes():
+    net = create_test_network()
+    pp.runpp(net)
+    net['res_test'] = pd.DataFrame(columns=['test'], data=[1, 2, 3])
+    net['test'] = pd.DataFrame(columns=['test'], data=[1, 2, 3])
+    net.line['test'] = 123
+    net.res_line['test'] = 123
+    net.bus['test'] = 123
+    net.res_bus['test'] = 123
+    net.res_load['test'] = 123
+    dtdf = collect_all_dtypes_df(net)
+    restore_all_dtypes(net, dtdf)
+
+
+@pytest.mark.xfail
+def test_to_json_dtypes(tempdir):
+    filename = os.path.join(tempdir, "testfile.json")
+    net = create_test_network()
+    pp.runpp(net)
+    net['res_test'] = pd.DataFrame(columns=['test'], data=[1, 2, 3])
+    net['test'] = pd.DataFrame(columns=['test'], data=[1, 2, 3])
+    net.line['test'] = 123
+    net.res_line['test'] = 123
+    net.bus['test'] = 123
+    net.res_bus['test'] = 123
+    net.res_load['test'] = 123
+    pp.to_json(net, filename)
+    net1 = pp.from_json(filename)
 
 
 if __name__ == "__main__":
