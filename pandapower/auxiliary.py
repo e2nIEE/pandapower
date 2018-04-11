b5494f832ae89e57debcf8c99f3b08fc42cc7256
WinfriedL <37797648+WinfriedL@users.noreply.github.com>
2018-04-11 15:44:20 +0200

    Add check if generator table is empty

--- pandapower/auxiliary.py
+++ pandapower/auxiliary.py
@@ -400,6 +400,8 @@ def _check_bus_index_and_print_warning_if_high(net, n_max=1e7):
             "create_continous_bus_index()" % max_bus)
 
 def _check_gen_index_and_print_warning_if_high(net, n_max=1e7):
+    if net.gen.empty:
+        return
     max_gen = max(net.gen.index.values)
     if max_gen >= n_max and len(net["gen"]) < n_max:
         logger.warning(
