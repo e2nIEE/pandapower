Fixing bug in voltage dependant load result calculation: bus index of res_bus table is not set at this stage in results_buy.py. We need to get the bus voltages from the ppc, as we can not access them in res_bus with wrong index.

pandapower/test/loadflow/test_scenarios.py
@@ -284,7 +284,7 @@ def test_volt_dep_load_at_inactive_bus():
     pp.runpp(net)
     assert not np.isnan(net.res_load.p_kw.at[1])
     assert not np.isnan(net.res_bus.p_kw.at[5])
-    assert np.isnan(net.res_bus.p_kw.at[3])
+    assert net.res_bus.p_kw.at[3] == 0
 
 
 if __name__ == "__main__":