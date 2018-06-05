798c92d6c391c4126570bb2f469f16580a2bdd2b
Jan-Hendrik Menke <jan-hendrik.menke@uni-kassel.de>
2018-06-05 11:31:11 +0200

    copyright 2018 update

--- doc/conf.py
+++ doc/conf.py
@@ -55,7 +55,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'pandapower'
-copyright = u'2016 by Fraunhofer IEE and University of Kassel'
+copyright = u'2016-2018 by Fraunhofer IEE and University of Kassel'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
