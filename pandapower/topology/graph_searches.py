ab7e6df9586e71eb3755fb76900504ee008b46c5
rbolgaryn <roman.bolgaryn@iwes.fraunhofer.de>
2018-09-07 16:19:02 +0200

    change x = x or a >> if x is None: x = a

--- pandapower/topology/graph_searches.py
+++ pandapower/topology/graph_searches.py
@@ -154,7 +154,8 @@ def unsupplied_buses(net, mg=None, in_service_only=False, slacks=None, respect_s
     """
 
     mg = mg or create_nxgraph(net, respect_switches=respect_switches)
-    slacks = slacks or set(net.ext_grid[net.ext_grid.in_service].bus.values)
+    if slacks is None:
+        slacks = set(net.ext_grid[net.ext_grid.in_service].bus.values)
     not_supplied = set()
     for cc in nx.connected_components(mg):
         if not set(cc) & slacks:
