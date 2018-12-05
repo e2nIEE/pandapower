b92fbccaddfb055b91ddf975b58e9fce5b8e358d
Florian Schaefer <florian.schaefer@uni-kassel.de>
2018-12-05 13:52:34 +0100

    added ax to simple plot

--- pandapower/plotting/simple_plot.py
+++ pandapower/plotting/simple_plot.py
@@ -22,7 +22,7 @@ logger = logging.getLogger(__name__)
 def simple_plot(net, respect_switches=False, line_width=1.0, bus_size=1.0, ext_grid_size=1.0, trafo_size=1.0,
                 plot_loads=False, plot_sgens=False, load_size=1.0, sgen_size=1.0, switch_size=2.0, switch_distance=1.0,
                 plot_line_switches=False, scale_size=True, bus_color="b", line_color='grey', trafo_color='k',
-                ext_grid_color='y', switch_color='k', library="igraph", show_plot=True):
+                ext_grid_color='y', switch_color='k', library="igraph", show_plot=True, ax=None):
     """
     Plots a pandapower network as simple as possible. If no geodata is available, artificial
     geodata is generated. For advanced plotting see the tutorial
@@ -170,7 +170,7 @@ def simple_plot(net, respect_switches=False, line_width=1.0, bus_size=1.0, ext_g
         collections.append(bsc2)
         collections.append(bsc1)
 
-    ax = draw_collections(collections)
+    ax = draw_collections(collections, ax=ax)
     if show_plot:
         plt.show()
     return ax
