5108e769663de377ba2b567aeb0ea8084c2b2024
Zhenqi Wang <zhenqi.wang@uni-kassel.de>
2019-01-19 23:11:58 +0100

    Fixed functionality of zero constraint Some structure and parameter optimizations

--- pandapower/estimation/estimator/lav.py
+++ pandapower/estimation/estimator/lav.py
@@ -3,78 +3,15 @@
 # Copyright (c) 2016-2018 by University of Kassel and Fraunhofer Institute for Energy Economics
 # and Energy System Technology (IEE), Kassel. All rights reserved.
 
-import warnings
 import numpy as np
 from scipy.sparse import csr_matrix, vstack, hstack
-from scipy.sparse.linalg import spsolve
 
-from pandapower.estimation.ppc_conversions import _build_measurement_vectors
-from pandapower.estimation.estimator.wls_matrix_ops import WLSAlgebra, WLSAlgebraZeroInjectionConstraints
+from pandapower.estimation.estimator.wls_matrix_ops import WLSAlgebra
 #from pandapower.estimation.estimator.wls import WLSEstimator
 from pandapower.idx_brch import F_BUS, T_BUS
 from pandapower.idx_bus import BUS_TYPE
 
-try:
-    from pandapower.pf.makeYbus import makeYbus
-except ImportError:
-    from pandapower.pf.makeYbus_pypower import makeYbus
 
+class MAlgebra(WLSAlgebra):
+    pass
 
-#class MAlgebra(WLSAlgebra):
-#    def ypsilon(self):
-#        pass
-#    
-#    def delta_ypsilon(self):
-#        pass
-#
-#class MEstimator(WLSEstimator):
-#    def estimate(self, ppci):
-#        slack_buses, non_slack_buses, n_active, r_inv, v_m, delta_masked, delta, z = self.wls_preprocessing(ppci)
-#
-#        # state vector
-#        E = np.concatenate((delta_masked.compressed(), v_m))
-#        # matrix calculation object
-#        sem = WLSAlgebra(ppci, slack_buses, non_slack_buses)
-#
-#        current_error = 100.
-#        cur_it = 0
-#        G_m, r, H, h_x = None, None, None, None
-#
-#        while current_error > self.tolerance and cur_it < self.max_iterations:
-#            self.logger.debug("Starting iteration {:d}".format(1 + cur_it))
-#            try:
-#                # create h(x) for the current iteration
-#                h_x = sem.create_hx(v_m, delta)
-#
-#                # residual r
-#                r = csr_matrix(z - h_x).T
-#
-#                # jacobian matrix H
-#                H = csr_matrix(sem.create_jacobian(v_m, delta))
-#
-#                # gain matrix G_m
-#                # G_m = H^t * R^-1 * H
-#                G_m = H.T * (r_inv * H)
-#
-#                # state vector difference d_E
-#                # d_E = G_m^-1 * (H' * R^-1 * r)
-#                d_E = spsolve(G_m, H.T * (r_inv * r))
-#                E += d_E
-#
-#                # update V/delta
-#                delta[non_slack_buses] = E[:len(non_slack_buses)]
-#                v_m = np.squeeze(E[len(non_slack_buses):])
-#
-#                # prepare next iteration
-#                cur_it += 1
-#                current_error = np.max(np.abs(d_E))
-#                self.logger.debug("Current error: {:.7f}".format(current_error))
-#
-#            except np.linalg.linalg.LinAlgError:
-#                self.logger.error("A problem appeared while using the linear algebra methods."
-#                                  "Check and change the measurement set.")
-#                return False
-#
-#        # check if the estimation is successfull
-#        self.check_result(current_error, cur_it)
-#        return delta, v_m
--- pandapower/estimation/estimator/wls_matrix_ops.py
+++ pandapower/estimation/estimator/wls_matrix_ops.py
@@ -6,7 +6,6 @@
 import warnings
 import numpy as np
 from scipy.sparse import csr_matrix, vstack, hstack, diags
-from scipy.sparse.linalg import spsolve
 
 from pandapower.idx_brch import F_BUS, T_BUS
 from pandapower.idx_bus import BUS_TYPE
@@ -18,18 +17,22 @@ except ImportError:
 
 
 class WLSAlgebra:
-    def __init__(self, ppci, slack_buses, non_slack_buses):
+    def __init__(self, ppci, non_nan_meas_mask):
         np.seterr(divide='ignore', invalid='ignore')
         self.ppci = ppci
-        self.fb = self.ppci["branch"][:, 0].real.astype(int)
-        self.tb = self.ppci["branch"][:, 1].real.astype(int)
-#        self.slack_buses = slack_buses
-#        self.non_slack_buses = non_slack_buses
+        self.fb = self.ppci["branch"][:, F_BUS].real.astype(int)
+        self.tb = self.ppci["branch"][:, T_BUS].real.astype(int)
         self.Y_bus = None
         self.Yf = None
         self.Yt = None
         self.G = None
         self.B = None
+        # TODO CHECK!
+        # Double the size of the bus to get mask for v_a and v_m
+        self.non_nan_meas_mask = non_nan_meas_mask
+        self.non_slack_bus_mask = (self.ppci['bus'][:, BUS_TYPE] != 3).ravel()
+        self.delta_v_bus_mask = np.r_[self.non_slack_bus_mask,
+                                      np.ones(self.non_slack_bus_mask.shape[0], dtype=bool)].ravel()
         self.createY()
 
     # Function which builds a node admittance matrix out of the topology data
@@ -39,7 +42,7 @@ class WLSAlgebra:
             warnings.simplefilter("ignore")
             Ybus, Yf, Yt = makeYbus(self.ppci["baseMVA"], self.ppci["bus"], self.ppci["branch"])
             self.ppci['internal']['Yf'], self.ppci['internal']['Yt'],\
-            self.ppci['internal']['Ybus'] = Yf, Yt, Ybus
+                self.ppci['internal']['Ybus'] = Yf, Yt, Ybus
 
         # create relevant matrices
         self.Ybus, self.Yf, self.Yt = Ybus, Yf, Yt
@@ -49,8 +52,8 @@ class WLSAlgebra:
     # Get Y as tuple (real, imaginary)
     def get_y(self):
         return self.G, self.B
-    
-    def create_hx(self, v, delta, meas_mask):
+
+    def create_hx(self, v, delta):
         f_bus, t_bus = self.fb, self.tb
         V = v * np.exp(1j * delta)
         Sfe = V[f_bus] * np.conj(self.Yf * V)
@@ -59,7 +62,7 @@ class WLSAlgebra:
         Ife = np.abs(Sfe)**2**(0.5)/v[f_bus]
         Ite = np.abs(Ste)**2**(0.5)/v[t_bus]
         hx = np.r_[np.real(Sbuse),
-                   np.real(Sfe), 
+                   np.real(Sfe),
                    np.real(Ste),
                    np.imag(Sbuse),
                    np.imag(Sfe),
@@ -67,43 +70,9 @@ class WLSAlgebra:
                    v,
                    Ife,
                    Ite]
-        return hx[meas_mask]
-    
-    def _dSbus_dv(self, V, diagV, diagVnorm):
-        diagIbus = csr_matrix(np.diagflat(self.Ybus * V))
-        dSbus_dth = 1j * diagV * np.conj(diagIbus - self.Ybus * diagV)
-        dSbus_dv = diagV * np.conj(self.Ybus * diagVnorm) + np.conj(diagIbus) * diagVnorm
-        return dSbus_dth, dSbus_dv
+        return hx[self.non_nan_meas_mask]
 
-    def _dSbr_dv(self, V, diagV, diagVnorm):
-        n_bus, n_branch = self.ppci['bus'].shape[0], self.ppci['branch'].shape[0]
-        f_bus, t_bus = self.fb, self.tb
-        If, It = self.Yf * V, self.Yt * V
-        diagIf, diagIt = np.diagflat(If), np.diagflat(It)
-        diagVf, diagVt = np.diagflat(V[f_bus]), np.diagflat(V[t_bus])
-        
-        dSf_dth = 1j * (np.conj(diagIf) * csr_matrix((V[f_bus], (range(n_branch), f_bus)),shape=(n_branch, n_bus)) -\
-                        diagVf * np.conj(self.Yf * diagV))
-        dSf_dv = diagVf * np.conj(self.Yf * diagVnorm) + np.conj(diagIf) *\
-                    csr_matrix((V[f_bus], (range(n_branch), f_bus)),shape=(n_branch, n_bus))
-        dSt_dth = 1j * (np.conj(diagIt) * csr_matrix((V[t_bus], (range(n_branch), t_bus)),shape=(n_branch, n_bus)) -\
-                        diagVt * np.conj(self.Yt * diagV))
-        dSt_dv = diagVt * np.conj(self.Yt * diagVnorm) + np.conj(diagIt) *\
-                    csr_matrix((V[t_bus], (range(n_branch), t_bus)),shape=(n_branch, n_bus))
-        return dSf_dth, dSt_dth, dSf_dv, dSt_dv
-
-    def _dVbus_dv(self, V):
-        dv_dth, dv_dv = np.diagflat(np.zeros(V.shape)), np.diagflat(np.ones(V.shape))
-        return dv_dth, dv_dv
-
-    def _dibr_dv(self, diagV, diagVnorm):
-        dif_dth = np.abs(self.Yf * 1j * diagV)
-        dif_dv = np.abs(self.Yf * diagVnorm)
-        dit_dth = np.abs(self.Yt * 1j * diagV)
-        dit_dv = np.abs(self.Yt * diagVnorm)
-        return dif_dth, dit_dth, dif_dv, dit_dv
-
-    def create_h_jacobian(self, v, delta, meas_mask):
+    def create_hx_jacobian(self, v, delta):
         # Using sparse matrix in creation sub-jacobian matrix
         V = v * np.exp(1j * delta)
         diagV, diagVnorm = diags(V).tocsr(), diags(V/np.abs(V)).tocsr()
@@ -132,10 +101,58 @@ class WLSAlgebra:
                           dif_dv,
                           dit_dv])
         h_jac = hstack([h_jac_th, h_jac_v])
-        # TODO CHECK!
-        bus_pq = np.r_[(self.ppci['bus'][:, BUS_TYPE] != 3),
-                       np.ones(self.ppci['bus'].shape[0], dtype=bool)].ravel()
-        return h_jac.todense()[meas_mask, :][:, bus_pq]
+        return h_jac.todense()[self.non_nan_meas_mask, :][:, self.delta_v_bus_mask]
+
+    def _dSbus_dv(self, V, diagV, diagVnorm):
+        diagIbus = csr_matrix(np.diagflat(self.Ybus * V))
+        dSbus_dth = 1j * diagV * np.conj(diagIbus - self.Ybus * diagV)
+        dSbus_dv = diagV * np.conj(self.Ybus * diagVnorm) + np.conj(diagIbus) * diagVnorm
+        return dSbus_dth, dSbus_dv
+
+    def _dSbr_dv(self, V, diagV, diagVnorm):
+        n_bus, n_branch = self.ppci['bus'].shape[0], self.ppci['branch'].shape[0]
+        f_bus, t_bus = self.fb, self.tb
+        If, It = self.Yf * V, self.Yt * V
+        diagIf, diagIt = np.diagflat(If), np.diagflat(It)
+        diagVf, diagVt = np.diagflat(V[f_bus]), np.diagflat(V[t_bus])
+
+        dSf_dth = 1j * (np.conj(diagIf) * csr_matrix((V[f_bus], (range(n_branch), f_bus)), shape=(n_branch, n_bus)) -
+                        diagVf * np.conj(self.Yf * diagV))
+        dSf_dv = (diagVf * np.conj(self.Yf * diagVnorm) + np.conj(diagIf) *
+                  csr_matrix((V[f_bus], (range(n_branch), f_bus)), shape=(n_branch, n_bus)))
+        dSt_dth = 1j * (np.conj(diagIt) * csr_matrix((V[t_bus], (range(n_branch), t_bus)), shape=(n_branch, n_bus)) -
+                        diagVt * np.conj(self.Yt * diagV))
+        dSt_dv = (diagVt * np.conj(self.Yt * diagVnorm) + np.conj(diagIt) *
+                  csr_matrix((V[t_bus], (range(n_branch), t_bus)), shape=(n_branch, n_bus)))
+        return dSf_dth, dSt_dth, dSf_dv, dSt_dv
+
+    def _dVbus_dv(self, V):
+        dv_dth, dv_dv = np.diagflat(np.zeros(V.shape)), np.diagflat(np.ones(V.shape))
+        return dv_dth, dv_dv
+
+    def _dibr_dv(self, diagV, diagVnorm):
+        dif_dth = np.abs(self.Yf * 1j * diagV)
+        dif_dv = np.abs(self.Yf * diagVnorm)
+        dit_dth = np.abs(self.Yt * 1j * diagV)
+        dit_dv = np.abs(self.Yt * diagVnorm)
+        return dif_dth, dit_dth, dif_dv, dit_dv
+
 
 class WLSAlgebraZeroInjectionConstraints(WLSAlgebra):
-    pass
+    def create_cx(self, v, delta, p_zero_inj, q_zero_inj):
+        V = v * np.exp(1j * delta)
+        Sbus = V * np.conj(self.Ybus * V)
+        c = np.r_[np.real(Sbus[p_zero_inj]),
+                  np.imag(Sbus[q_zero_inj])]
+        return c
+
+    def create_cx_jacobian(self, v, delta, p_zero_inj, q_zero_inj):
+        V = v * np.exp(1j * delta)
+        diagV, diagVnorm = diags(V).tocsr(), diags(V/np.abs(V)).tocsr()
+        dSbus_dth, dSbus_dv = self._dSbus_dv(V, diagV, diagVnorm)
+        c_jac_th = np.r_[np.real(dSbus_dth.todense())[p_zero_inj],
+                         np.imag(dSbus_dth.todense())[q_zero_inj]]
+        c_jac_v = np.r_[np.real(dSbus_dv.todense())[p_zero_inj],
+                        np.imag(dSbus_dv.todense())[q_zero_inj]]
+        c_jac = np.c_[c_jac_th, c_jac_v]
+        return c_jac[:, self.delta_v_bus_mask]
