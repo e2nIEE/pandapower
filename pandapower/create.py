db480eedd95ebcee2eeb798b491e88e4852c7f3f
Steffen Meinecke <steffen.meinecke@uni-kassel.de>
2019-02-11 15:09:20 +0100

    improve xward documentation

--- pandapower/create.py
+++ pandapower/create.py
@@ -2206,7 +2206,11 @@ def create_xward(net, bus, ps_kw, qs_kvar, pz_kw, qz_kvar, r_ohm, x_ohm, vm_pu, 
 
         **qz_kvar** (float) - reactive power of the impedance load in kVar at 1.pu voltage
 
-        **vm_pu** (float)
+        **r_ohm** (float) - internal resistance of the voltage source
+
+        **x_ohm** (float) - internal reactance of the voltage source
+
+        **vm_pu** (float) - voltage magnitude at the additional PV-node
 
     OUTPUT:
         xward id
