fd4421d2d8cb0b10b4f0566865b81ee367fc909c
Leon Thurner <leon.thurner@uni-kassel.de>
2019-03-04 18:24:42 +0100

    create_nxgraph: trafo3w switches, bb switches, performance #271

--- pandapower/topology/create_graph.py
+++ pandapower/topology/create_graph.py
@@ -13,23 +13,29 @@ except ImportError:
 
 from pandapower.pd2ppc import _init_ppc
 from pandapower.auxiliary import _init_runpp_options, _select_is_elements_numba 
-from pandapower.idx_brch import BR_R, BR_X, F_BUS, T_BUS
 from pandapower.idx_bus import BASE_KV
 
 from pandapower.build_branch import _calc_impedance_parameters_from_dataframe,\
                                     _calc_branch_values_from_trafo_df, \
                                     _trafo_df_from_trafo3w
 from pandapower.build_bus import _build_bus_ppc
-BR_Z = 4
-WEIGHT = 5
-INDEX = 6
+
+INDEX = 0
+F_BUS = 1
+T_BUS = 2
+
+WEIGHT = 0
+BR_R = 1
+BR_X = 2
+BR_Z = 3
+
 logger = logging.getLogger(__name__)
 
 
 def create_nxgraph(net, respect_switches=True, include_lines=True,
                    include_trafos=True, include_impedances=True,
                    nogobuses=None, notravbuses=None, multi=True,
-                   add_impedances=False):
+                   calc_z=False):
     """
      Converts a pandapower network into a NetworkX graph, which is a is a simplified representation
      of a network's topology, reduced to nodes and edges. Busses are being represented by nodes
@@ -85,83 +91,113 @@ def create_nxgraph(net, respect_switches=True, include_lines=True,
     else:
         mg = nx.Graph()
     mg.add_nodes_from(net.bus.index)
-    
-    if add_impedances:
-        ppc = get_nx_ppc(net)
-    if respect_switches:
-        closed_sw = net.switch.closed.values == False
+   
+    open_sw = net.switch.closed.values == False   
+    if calc_z:
+        ppc = get_nx_ppc(net)        
+        
     if include_lines:        
-        if respect_switches:
-            mask = (net.switch.et.values == "l") & closed_sw
-            nogolines = set(net.switch.element.values[mask])
-        else:
-            nogolines = None
         line = net.line
-        edge_par = init_par(line)
-        edge_par[:, F_BUS] = line.from_bus.values
-        edge_par[:, T_BUS] = line.to_bus.values
-        if add_impedances:
+        indices, parameter, in_service = init_par(line, calc_z)
+        indices[:, F_BUS] = line.from_bus.values
+        indices[:, T_BUS] = line.to_bus.values
+
+        if respect_switches:
+            mask = (net.switch.et.values == "l") & open_sw
+            if mask.any():
+                open_switch = np.isin(indices[:, INDEX], net.switch.element.values[mask])
+                in_service &= ~open_switch
+
+        parameter[:, WEIGHT] = line.length_km.values
+        if calc_z:
             line_length = line.length_km.values / line.parallel.values
             r = line.r_ohm_per_km.values * line_length
             x = line.x_ohm_per_km.values * line_length
-            edge_par[:, BR_R] = r
-            edge_par[:, BR_X] = x
-        edge_par[:, WEIGHT] = line.length_km.values
-        add_edges(mg, edge_par, net, "line", add_impedances, nogolines)
+            parameter[:, BR_R] = r
+            parameter[:, BR_X] = x
+            
+        add_edges(mg, indices, parameter, in_service, net, "line", calc_z)
 
     if include_impedances and len(net.impedance):
         impedance = net.impedance
-        edge_par = init_par(impedance)
-        edge_par[:, F_BUS] = impedance.from_bus.values
-        edge_par[:, T_BUS] = impedance.to_bus.values
-        if add_impedances:
-            baseR = get_baseR(net, ppc, impedance.bus.values)
+        indices, parameter, in_service = init_par(impedance, calc_z)
+        indices[:, F_BUS] = impedance.from_bus.values
+        indices[:, T_BUS] = impedance.to_bus.values
+        
+        if calc_z:
+            baseR = get_baseR(net, ppc, impedance.from_bus.values)
             r, x, _, _ = _calc_impedance_parameters_from_dataframe(net)
-            edge_par[:, BR_R] = r
-            edge_par[:, BR_X] = x
-        add_edges(mg, edge_par, net, "impedance", add_impedances)
+            parameter[:, BR_R] = r * baseR
+            parameter[:, BR_X] = x * baseR
+            
+        add_edges(mg, indices, parameter, in_service, net, "impedance", calc_z)
         
     if include_trafos:
         trafo = net.trafo
         if len(trafo.index):
+            indices, parameter, in_service = init_par(trafo, calc_z)
+            indices[:, F_BUS] = trafo.hv_bus.values
+            indices[:, T_BUS] = trafo.lv_bus.values
+    
             if respect_switches:
-                mask = (net.switch.et.values == "t") & closed_sw
-                nogotrafos = set(net.switch.element.values[mask])
-            else:
-                nogotrafos = None
-            edge_par = init_par(trafo)
-            edge_par[:, F_BUS] = trafo.hv_bus.values
-            edge_par[:, T_BUS] = trafo.lv_bus.values
-            if add_impedances:
+                mask = (net.switch.et.values == "t") & open_sw
+                if mask.any():
+                    open_switch = np.isin(indices[:, INDEX], net.switch.element.values[mask])
+                    in_service &= ~open_switch
+
+            if calc_z:
                 baseR = get_baseR(net, ppc, trafo.hv_bus.values)
                 r, x, _, _, _ = _calc_branch_values_from_trafo_df(net, ppc, trafo)
-                edge_par[:, BR_R] = r * baseR
-                edge_par[:, BR_X] = x * baseR
-            add_edges(mg, edge_par, net, "trafo", add_impedances, nogotrafos)
+                parameter[:, BR_R] = r * baseR
+                parameter[:, BR_X] = x * baseR
+            
+            add_edges(mg, indices, parameter, in_service, net, "trafo", calc_z)
 
         trafo3w = net.trafo3w
         if len(trafo3w):
-            if respect_switches:
-                mask = (net.switch.et.values == "t3") & closed_sw
-                nogotrafo3w = net.switch[["element", "bus"]].values[mask].tolist()
-            else:
-                nogotrafo3w = None
             sides = ["hv", "mv", "lv"]
-            if add_impedances:     
+            if calc_z:     
                 trafo_df = _trafo_df_from_trafo3w(net)
                 r_all, x_all, _, _, _ = _calc_branch_values_from_trafo_df(net, ppc, trafo_df)
                 r = {side: r for side, r in zip(sides, np.split(r_all, 3))}
                 x = {side: x for side, x in zip(sides, np.split(x_all, 3))}
                 baseR = get_baseR(net, ppc, trafo3w.hv_bus.values)
+            open_switch = np.zeros(trafo3w.shape[0], dtype=bool)
+            if respect_switches:
+                mask = (net.switch.et.values == "t3") & open_sw
+                if mask.any():
+                    open_switch = np.isin(net.trafo3w.index, net.switch.element.values[mask])
+                    open_element_bus_combos = net.switch[["element", "bus"]].values[mask].tolist()
             for f, t in combinations(sides, 2):
-                edge_par = init_par(trafo3w)
-                edge_par[:, F_BUS] = trafo3w["%s_bus"%f].values
-                edge_par[:, T_BUS] = trafo3w["%s_bus"%t].values
-                if add_impedances:     
-                    edge_par[:, BR_R] = (r[f] + r[t]) * baseR
-                    edge_par[:, BR_X] = (x[f] + x[t]) * baseR
-                add_edges(mg, edge_par, net, "trafo3w", add_impedances, 
-                          nogotrafo3w)
+                indices, parameter, in_service = init_par(trafo3w, calc_z)
+                indices[:, F_BUS] = trafo3w["%s_bus"%f].values
+                indices[:, T_BUS] = trafo3w["%s_bus"%t].values
+                
+                if open_switch.any():
+                    no_branch_at = [([b[INDEX], b[F_BUS]] in open_element_bus_combos or
+                                     [b[INDEX], b[T_BUS]] in open_element_bus_combos) 
+                                      for b in indices[open_switch]]
+                    in_service[open_switch] = ~np.array(no_branch_at)
+                
+                if calc_z:     
+                    parameter[:, BR_R] = (r[f] + r[t]) * baseR
+                    parameter[:, BR_X] = (x[f] + x[t]) * baseR
+                        
+                add_edges(mg, indices, parameter, in_service, net, "trafo3w", calc_z)
+
+
+    switch = net.switch
+    if len(switch):
+        if respect_switches:
+            # add edges for closed bus-bus switches
+            in_service = (switch.et.values == "b") & ~open_sw
+        else:
+            # add edges for any bus-bus switches
+            in_service = (switch.et.values == "b")
+        indices, parameter = init_par(switch, calc_z)
+        indices[:, F_BUS] = switch.bus.values
+        indices[:, T_BUS] = switch.element.values
+        add_edges(mg, indices, parameter, in_service, net, "switch", calc_z)
                 
     # nogobuses are a nogo
     if nogobuses is not None:
@@ -177,35 +213,38 @@ def create_nxgraph(net, respect_switches=True, include_lines=True,
     mg.remove_nodes_from(net.bus.index[~net.bus.in_service.values])
     return mg
 
-def add_edges(mg, parameters, net, element, add_impedances, nogo=None):
-    parameters[:, BR_Z] = np.sqrt(parameters[:, BR_R]**2 + parameters[:, BR_X]**2)
-    is_par = parameters[net[element].in_service.values]
-    for p in is_par:
-        fb = int(p[F_BUS])
-        tb = int(p[T_BUS])
-        index = int(p[INDEX])
-        if nogo is not None:
-            #for trafo3w the bus of the open switch is relevant
-            if element == "trafo3w":
-                #don't add an edge with an open switch
-                if [index, fb] in nogo or [index, tb] in nogo:
-                    continue        
-            elif index in nogo:
-                continue
-        weights = {"weight": p[WEIGHT], "path": 1}
-        if add_impedances:
-            weights.update({"r_ohm": p[BR_R], "x_ohm": p[BR_X], "z_ohm": p[BR_Z]})   
-        mg.add_edge(fb, tb, key=(element, index),  **weights)
+
+def add_edges(mg, indices, parameter, in_service, net, element, calc_z):
+    if calc_z:
+        parameter[:, BR_Z] = np.sqrt(parameter[:, BR_R]**2 + parameter[:, BR_X]**2)
+    for idx, p in zip(indices[in_service], parameter[in_service]):
+        if calc_z:
+            weights = {"weight": p[WEIGHT], "path": 1, "r_ohm": p[BR_R], "x_ohm": p[BR_X],
+                       "z_ohm": p[BR_Z]}
+        else:
+             weights = {"weight": p[WEIGHT], "path": 1}           
+        mg.add_edge(idx[F_BUS], idx[T_BUS], key=(element, idx[INDEX]),  **weights)
+
 
 def get_baseR(net, ppc, buses):
     bus_lookup = net._pd2ppc_lookups["bus"]
     base_kv = ppc["bus"][bus_lookup[buses], BASE_KV]
     return np.square(base_kv) / net.sn_mva
+  
     
-def init_par(tab):
-    parameters = np.zeros((tab.shape[0], 8))
-    parameters[:, INDEX] = tab.index
-    return parameters
+def init_par(tab, calc_z):
+    n = tab.shape[0]
+    indices = np.zeros((n, 3), dtype=np.int)
+    indices[:, INDEX] = tab.index
+    if calc_z:
+        parameters = np.zeros((n, 4), dtype=np.float)
+    else:
+        parameters = np.zeros((n, 1), dtype=np.float)
+    
+    if "in_service" in tab:
+        return indices, parameters, tab.in_service.values.copy()
+    else:
+        return indices, parameters
             
 
 def get_nx_ppc(net):
@@ -218,44 +257,3 @@ def get_nx_ppc(net):
     ppc = _init_ppc(net)
     _build_bus_ppc(net, ppc)
     return ppc
-
-    
-if __name__ == '__main__':
-    import pandapower.networks as nw
-    net = nw.case9241pegase()
-    net = nw.mv_oberrhein(include_substations=True)
-#    net = nw.example_simple()
-#    pp.runpp(net)
-#    net.line.at[0, "in_service"] = False
-
-#    ppc = get_nx_ppc(net)
-#    
-#    line = net.line
-#    r = line.r_ohm_per_km.values * line.length_km.values / line.parallel.values
-#    x = line.x_ohm_per_km.values * line.length_km.values / line.parallel.values
-#    
-##    pp.runpp(net)
-#
-##    line = net.line.iloc[0]
-##    print(mg.get_edge_data(line.from_bus, line.to_bus))
-##    
-##    trafo = net.trafo.iloc[0]
-##    print(mg.get_edge_data(trafo.hv_bus, trafo.lv_bus))
-#    
-#    
-    net = nw.example_multivoltage()
-    trafo3 = net.trafo3w.iloc[0]
-#    pp.create_switch(net, bus=trafo3.mv_bus, element=0, et="t3", closed=False)
-
-    mg = create_nxgraph(net)
-    print(mg.get_edge_data(trafo3.hv_bus, trafo3.lv_bus))
-
-    print(mg.get_edge_data(trafo3.hv_bus, trafo3.mv_bus))
-
-    print(mg.get_edge_data(trafo3.mv_bus, trafo3.lv_bus))
-#    r, x, z = get_z_from_ppc(net)
-#    f, t = net._pd2ppc_lookups["branch"]["trafo"]
-#    pp.runpp(net)
-#    print(net._ppc["branch"][f, BR_R])
-#    print(net._ppc["branch"][f, BR_X])
-#    max(r[f:t] - (net.line.length_km*net.line.r_ohm_per_km).values)
