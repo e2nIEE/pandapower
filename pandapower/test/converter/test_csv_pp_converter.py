96c2111787555697cba355a4d3fbefbce79d9219
Florian Schaefer <florian.schaefer@uni-kassel.de>
2019-04-25 09:15:45 +0200

    fix import

--- pandapower/test/converter/test_csv_pp_converter.py
+++ pandapower/test/converter/test_csv_pp_converter.py
@@ -19,7 +19,7 @@ except ImportError:
     igraph_installed = False
 
 from pandapower import pp_dir
-from converter import csv2pp, csv_data2pp, pp2csv, pp2csv_data, \
+from pandapower.converter import csv2pp, csv_data2pp, pp2csv, pp2csv_data, \
     convert_parallel_branches, read_csv_data, ensure_full_column_data_existence, \
     avoid_duplicates_in_column, merge_busbar_coordinates
 
