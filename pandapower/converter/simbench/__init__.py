0286f45c4ffafecd4af75a3d00c4f936b909a032
Steffen Meinecke <steffen.meinecke@uni-kassel.de>
2019-04-30 10:10:09 +0200

    remove pp_to_json

--- pandapower/converter/simbench/__init__.py
+++ pandapower/converter/simbench/__init__.py
@@ -9,5 +9,3 @@ from .pp_net_manipulation import *
 from .csv_data_manipulation import *
 from .read_and_write import *
 from .csv_pp_converter import *
-
-from .pp_to_json import *  # not for open source
