47015784eedc81fc6d76096d64ac94f672dd152e
Zhenqi Wang <zhenqi.wang@uni-kassel.de>
2019-08-15 13:15:00 +0200

    Small update

--- pandapower/estimation/state_estimation.py
+++ pandapower/estimation/state_estimation.py
@@ -234,10 +234,11 @@ class StateEstimation:
             success = estimate(np.array([1.0, 1.0, 1.0]), np.array([0.0, 0.0, 0.0]))
         """
         # check if all parameter are allowed
-        for key in opt_vars:
-            if key not in ALLOWED_OPT_VAR:
-                self.logger.warning("Caution! %s is not defined and will be ignored!" %key)
-        
+        for var_name in opt_vars.keys():
+            if var_name not in ALLOWED_OPT_VAR:
+                self.logger.warning("Caution! %s is not allowed as parameter"%var_name\
+                                    + " for estimate and will be ignored!" )
+
         if self.net is None:
             raise UserWarning("SE Component was not initialized with a network.")
 
