cdc3ab72bc1a7437d73d0815bc287ff1d136abce
Shankho Ghosh <ghosh.shankho@gmail.com>
2019-08-15 16:14:45 +0200

    Test for param and stdtyp passed #96

--- pandapower/test/loadflow/test_runpp_3ph.py
+++ pandapower/test/loadflow/test_runpp_3ph.py
@@ -128,30 +128,26 @@ def test_4bus_network():
     pp.create_ext_grid(net, bus=busn, vm_pu=1.0, name="Grid Connection", s_sc_max_mva=5000, 
                        rx_max=0.1,r0x0_max=0.1,x0x_max=1.0)    
     
-    pp.create_std_type(net, {"r0_ohm_per_km": 0.0848, "x0_ohm_per_km": 0.4649556, "c0_nf_per_km":\
-        230.6,   "max_i_ka": 0.963, "r_ohm_per_km": 0.0212, "x_ohm_per_km": 0.1162389,
-                 "c_nf_per_km":  230}, "example_type1")
-    
-    pp.create_std_type(net, {"r0_ohm_per_km" : .3048, "x0_ohm_per_km" : 0.6031856, 
-                             "c0_nf_per_km" : 140.3, "max_i_ka": 0.531, 
-                              "r_ohm_per_km" : .0762, "x_ohm_per_km" : 0.1507964
-                            , "c_nf_per_km" : 140}, "example_type2")
     pp.create_std_type(net, {"r0_ohm_per_km" : .154, "x0_ohm_per_km" : 0.5277876
                              , "c0_nf_per_km" : 170.4, "max_i_ka": 0.741, 
                               "r_ohm_per_km" : .0385, "x_ohm_per_km" : 0.1319469
                              , "c_nf_per_km" : 170}, "example_type3")
     
-    pp.create_std_type(net, {"r0_ohm_per_km" : .1005, "x0_ohm_per_km" : 0.4900884
-                             , "c0_nf_per_km":  200.5, "max_i_ka" : 0.89
-                             , "r_ohm_per_km": .0251, "x_ohm_per_km" : 0.1225221
-                             , "c_nf_per_km" : 210}, "example_type4")
-    
     pp.create_line(net, from_bus = busn, to_bus = busm, length_km = 1.0, std_type="example_type3")
     pp.create_line(net, from_bus = busn, to_bus = busp, length_km = 1.0, std_type="example_type3")
-    pp.create_line(net, from_bus = busn, to_bus = busk, length_km = 1.0, std_type="example_type4")
-    pp.create_line(net, from_bus = busk, to_bus = busm, length_km = 1.0, std_type="example_type1")
-    pp.create_line(net, from_bus = busk, to_bus = busp, length_km = 1.0, std_type="example_type2")
-    pp.add_zero_impedance_parameters(net)    
+    pp.create_line_from_parameters(net, from_bus = busn, to_bus = busk, length_km = 1.0, r0_ohm_per_km = .1005, x0_ohm_per_km = 0.4900884
+                             , c0_nf_per_km=  200.5, max_i_ka = 0.89
+                             , r_ohm_per_km= .0251, x_ohm_per_km = 0.1225221
+                             , c_nf_per_km = 210)
+    pp.create_line_from_parameters(net, from_bus = busk, to_bus = busm, length_km = 1.0, r0_ohm_per_km= 0.0848, x0_ohm_per_km= 0.4649556, c0_nf_per_km=\
+        230.6,   max_i_ka= 0.963, r_ohm_per_km= 0.0212, x_ohm_per_km= 0.1162389,
+                 c_nf_per_km=  230)
+    pp.create_line_from_parameters(net, from_bus = busk, to_bus = busp, length_km = 1.0, r0_ohm_per_km = .3048, x0_ohm_per_km = 0.6031856, 
+                             c0_nf_per_km = 140.3, max_i_ka= 0.531, 
+                              r_ohm_per_km = .0762, x_ohm_per_km = 0.1507964
+                            , c_nf_per_km = 140)    
+    pp.add_zero_impedance_parameters(net)
+    
     create_asymmetric_load(net, busk, p_A_mw=50, q_A_mvar=20, p_B_mw=80, q_B_mvar=60,
                        p_C_mw=20, q_C_mvar=5)
     create_asymmetric_load(net, busm, p_A_mw=50, q_A_mvar=50, p_B_mw=10, q_B_mvar=15,
