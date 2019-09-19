Bug correction and unit conversion into p.u.

pandapower/estimation/algorithm/matrix_base.py
@@ -73,24 +73,24 @@ class BaseAlgebra:
         Sfe = V[f_bus] * np.conj(self.Yf * V) 
         Ste = V[t_bus] * np.conj(self.Yt * V)
         Sbuse = V * np.conj(self.Ybus * V) 
-        Ife = np.abs(Sfe)/v[f_bus]
-        Ite = np.abs(Ste)/v[t_bus]
-        hx = np.r_[np.real(Sbuse) * self.baseMVA,
-                   np.real(Sfe) * self.baseMVA,
-                   np.real(Ste) * self.baseMVA,
-                   np.imag(Sbuse) * self.baseMVA,
-                   np.imag(Sfe) * self.baseMVA,
-                   np.imag(Ste) * self.baseMVA,
+        Ife = self.Yf * V
+        Ite = self.Yt * V
+        hx = np.r_[np.real(Sbuse),
+                   np.real(Sfe),
+                   np.real(Ste),
+                   np.imag(Sbuse),
+                   np.imag(Sfe),
+                   np.imag(Ste),
                    v,
-                   Ife * self.baseMVA / self.bus_baseKV[f_bus],
-                   Ite * self.baseMVA / self.bus_baseKV[t_bus]] 
+                   np.abs(Ife),
+                   np.abs(Ite)] 
         return hx[self.non_nan_meas_mask]
 
     def create_hx_jacobian(self, E):
         # Using sparse matrix in creation sub-jacobian matrix
         v, delta = self._e2v(E)
 
-        f_bus, t_bus = self.fb, self.tb
+#        f_bus, t_bus = self.fb, self.tb
         V = v * np.exp(1j * delta)
 
         dSbus_dth, dSbus_dv = self._dSbus_dv(V)
@@ -111,12 +111,10 @@ class BaseAlgebra:
                           dSf_dv.imag,
                           dSt_dv.imag))
 
-        s_jac = hstack((s_jac_th, s_jac_v)).toarray() * self.baseMVA
+        s_jac = hstack((s_jac_th, s_jac_v)).toarray()
         v_jac = np.c_[dv_dth, dv_dv]
         i_jac = vstack((hstack((dif_dth, dif_dv)),
-                        hstack((dit_dth, dit_dv)))).toarray() *\
-                        (self.baseMVA / np.r_[self.bus_baseKV[f_bus], 
-                                              self.bus_baseKV[t_bus]]).reshape(-1, 1)
+                        hstack((dit_dth, dit_dv)))).toarray()
         return np.r_[s_jac,
                      v_jac,
                      i_jac][self.non_nan_meas_mask, :][:, self.delta_v_bus_mask]