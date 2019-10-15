ec6f9136e8827f16b6f2d5631bcc404cadfea05f
Florian Schaefer <florian.schaefer@uni-kassel.de>
2019-10-15 09:18:32 +0200

    added nl and mip also to julia function

--- pandapower/opf/run_powermodels_ots.jl
+++ pandapower/opf/run_powermodels_ots.jl
@@ -5,7 +5,7 @@ function run_powermodels(json_path)
     # function to run optimal transmission switching (OTS) optimization from powermodels.jl
     pm = PP2PM.load_pm_from_json(json_path)
     model = PP2PM.get_model(pm["pm_model"])
-    solver = PP2PM.get_solver(pm["pm_solver"], "ipopt", "cbc")
+    solver = PP2PM.get_solver(pm["pm_solver"], pm["pm_nl_solver"], pm["pm_mip_solver"])
     result = run_ots(pm, model, solver,
                         setting = Dict("output" => Dict("branch_flows" => true)))
     return result
