74ae21bd1887b938520c9751f0274c4e40b85699
Steffen Meinecke <steffen.meinecke@uni-kassel.de>
2020-03-09 10:13:52 +0100

    add test to ensure results in power system test cases

--- pandapower/test/networks/test_power_system_test_cases.py
+++ pandapower/test/networks/test_power_system_test_cases.py
@@ -26,8 +26,16 @@ def _ppc_element_test(net, n_bus=None, n_branch=None, n_gen=None, gencost=False)
     assert net.converged
 
 
+def _check_result_existence(net):
+    for elm in pp.pp_elements():
+        n_elm = net[elm].shape[0]
+        if n_elm:
+            assert net["res_" + elm].dropna().shape[0] == n_elm
+
+
 def test_case4gs():
     net = pn.case4gs()
+    _check_result_existence(net)
     pp.runpp(net)
     assert net.converged
     _ppc_element_test(net, 4, 4, 2, False)
@@ -35,6 +43,7 @@ def test_case4gs():
 
 def test_case5():
     net = pn.case5()
+    _check_result_existence(net)
     pp.runpp(net)
     assert net.converged
     _ppc_element_test(net, 5, 6, 5, False)
@@ -42,6 +51,7 @@ def test_case5():
 
 def test_case6ww():
     net = pn.case6ww()
+    _check_result_existence(net)
     pp.runpp(net)
     assert net.converged
     _ppc_element_test(net, 6, 11, 3, True)
@@ -49,6 +59,7 @@ def test_case6ww():
 
 def test_case9():
     net = pn.case9()
+    _check_result_existence(net)
     pp.runpp(net)
     assert net.converged
     _ppc_element_test(net, 9, 9, 3, True)
@@ -56,6 +67,7 @@ def test_case9():
 
 def test_case14():
     net = pn.case14()
+    _check_result_existence(net)
     pp.runpp(net)
     assert net.converged
     _ppc_element_test(net, 14, 20, 5, True)
@@ -63,6 +75,7 @@ def test_case14():
 
 def test_case24_ieee_rts():
     net = pn.case24_ieee_rts()
+    _check_result_existence(net)
     pp.runpp(net)
     assert net.converged
     _ppc_element_test(net, 24)
@@ -70,6 +83,7 @@ def test_case24_ieee_rts():
 
 def test_case30():
     net = pn.case30()
+    _check_result_existence(net)
     pp.runpp(net)
     assert net.converged
     _ppc_element_test(net, 30, 41, 6, True)
@@ -77,6 +91,7 @@ def test_case30():
 
 def test_case_ieee30():
     net = pn.case_ieee30()
+    _check_result_existence(net)
     pp.runpp(net)
     assert net.converged
     _ppc_element_test(net, 30, 41, 6, False)
@@ -84,6 +99,7 @@ def test_case_ieee30():
 
 def test_case33bw():
     net = pn.case33bw()
+    _check_result_existence(net)
     pp.runpp(net, trafo_mode="pi")
     assert net.converged
     _ppc_element_test(net, 33, 37, 1, True)
@@ -91,6 +107,7 @@ def test_case33bw():
 
 def test_case39():
     net = pn.case39()
+    _check_result_existence(net)
     pp.runpp(net)
     assert net.converged
     _ppc_element_test(net, 39, 46, 10, True)
@@ -98,6 +115,7 @@ def test_case39():
 
 def test_case57():
     net = pn.case57()
+    _check_result_existence(net)
     pp.runpp(net)
     assert net.converged
     _ppc_element_test(net, 57, 80, 7, True)
@@ -105,6 +123,7 @@ def test_case57():
 
 def test_case89pegase():
     net = pn.case89pegase()
+    _check_result_existence(net)
     pp.runpp(net)
     assert net.converged
     _ppc_element_test(net, 89, 210, 12+6, 12)
@@ -112,6 +131,7 @@ def test_case89pegase():
 
 def test_case118():
     net = pn.case118()
+    _check_result_existence(net)
     pp.runpp(net)
     assert net.converged
     _ppc_element_test(net, 118, 186, 54, True)
@@ -119,6 +139,7 @@ def test_case118():
 
 def test_case145():
     net = pn.case145()
+    _check_result_existence(net)
     pp.runpp(net)
     assert net.converged
     _ppc_element_test(net, 145, 453, 50+9, 50)
@@ -126,6 +147,7 @@ def test_case145():
 
 def test_case_illinois200():
     net = pn.case_illinois200()
+    _check_result_existence(net)
     pp.runpp(net)
     assert net.converged
     _ppc_element_test(net, 200, 245, 49, False)
@@ -133,6 +155,7 @@ def test_case_illinois200():
 
 def test_case300():
     net = pn.case300()
+    _check_result_existence(net)
     pp.runpp(net)
     assert net.converged
     _ppc_element_test(net, 300, 411, 69+8, 69)
@@ -140,6 +163,7 @@ def test_case300():
 
 def test_case1354pegase():
     net = pn.case1354pegase()
+    _check_result_existence(net)
     pp.runpp(net)
     assert net.converged
     _ppc_element_test(net, 1354, 1991, 260+52, 260)
@@ -147,6 +171,7 @@ def test_case1354pegase():
 
 def test_case1888rte():
     net = pn.case1888rte()
+    _check_result_existence(net)
     pp.runpp(net)
     assert net.converged
     _ppc_element_test(net, 1888, 2531, 354, 297)
@@ -155,6 +180,7 @@ def test_case1888rte():
 def test_case1888rte_changed_slack():
     ref_bus_idx = 1233
     net = pn.case1888rte(ref_bus_idx=ref_bus_idx)
+    _check_result_existence(net)
     pp.runpp(net)
     assert net.converged
     _ppc_element_test(net, 1888, 2531, 354, 297)
@@ -169,6 +195,7 @@ def test_case1888rte_changed_slack():
 
 def test_case2848rte():
     net = pn.case2848rte()
+    _check_result_existence(net)
     pp.runpp(net)
     assert net.converged
     _ppc_element_test(net, 2848, 3776, 670, 547)
@@ -176,6 +203,7 @@ def test_case2848rte():
 
 def test_case2869pegase():
     net = pn.case2869pegase()
+    _check_result_existence(net)
     pp.runpp(net)
     assert net.converged
     _ppc_element_test(net, 2869, 4582, 690, 510)
@@ -183,6 +211,7 @@ def test_case2869pegase():
 
 def test_case3120sp():
     net = pn.case3120sp()
+    _check_result_existence(net)
     pp.runpp(net)
     assert net.converged
     _ppc_element_test(net, 3120, 3693, 505, True)
@@ -190,6 +219,7 @@ def test_case3120sp():
 
 def test_case6470rte():
     net = pn.case6470rte()
+    _check_result_existence(net)
     pp.runpp(net)
     assert net.converged
     _ppc_element_test(net, 6470, 9005, 1578, 1330)
@@ -197,6 +227,7 @@ def test_case6470rte():
 
 def test_case6495rte():
     net = pn.case6495rte()
+    _check_result_existence(net)
     pp.runpp(net)
     assert net.converged
     _ppc_element_test(net, 6495, 9019, 1650, 1372)
@@ -204,6 +235,7 @@ def test_case6495rte():
 
 def test_case6515rte():
     net = pn.case6515rte()
+    _check_result_existence(net)
     pp.runpp(net)
     assert net.converged
     _ppc_element_test(net, 6515, 9037, 1651, 1388)
@@ -211,6 +243,7 @@ def test_case6515rte():
 
 def test_case9241pegase():
     net = pn.case9241pegase()
+    _check_result_existence(net)
     pp.runpp(net)
     assert net.converged
     _ppc_element_test(net, 9241, 16049, 1879, 1445)
@@ -218,6 +251,7 @@ def test_case9241pegase():
 
 def test_GBreducednetwork():
     net = pn.GBreducednetwork()
+    _check_result_existence(net)
     pp.runpp(net)
     assert net.converged
     _ppc_element_test(net, 29, 99, 66, True)
@@ -225,6 +259,7 @@ def test_GBreducednetwork():
 
 def test_GBnetwork():
     net = pn.GBnetwork()
+    _check_result_existence(net)
     pp.runpp(net)
     assert net.converged
     _ppc_element_test(net, 2224, 3207, 394+37, 394)
@@ -232,6 +267,7 @@ def test_GBnetwork():
 
 def test_iceland():
     net = pn.iceland()
+    _check_result_existence(net)
     pp.runpp(net)
     assert net.converged
     _ppc_element_test(net, 189, 206, 35, True)
