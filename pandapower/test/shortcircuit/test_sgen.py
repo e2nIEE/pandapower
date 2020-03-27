b5e40da3d266d6da3a939883ad683101edd9499c
chefPony <alberto90.az@gmail.com>
2020-03-27 18:04:47 +0100

    Add test for issue #675

--- pandapower/test/shortcircuit/test_sgen.py
+++ pandapower/test/shortcircuit/test_sgen.py
@@ -45,6 +45,21 @@ def three_bus_example():
     pp.create_sgen(net, b2, sn_mva=2, p_mw=0, k=1.2)
     return net
 
+@pytest.fixture
+def big_sgen_example():
+    net = pp.create_empty_network()
+    b1 = pp.create_bus(net, 110)
+    b2 = pp.create_bus(net, 110)
+    b3 = pp.create_bus(net, 110)
+
+    pp.create_ext_grid(net, b1, s_sc_max_mva=100., s_sc_min_mva=80., rx_min=0.4, rx_max=0.4)
+    pp.create_line(net, b1, b2, std_type="305-AL1/39-ST1A 110.0" , length_km=20.)
+    pp.create_line(net, b2, b3, std_type="N2XS(FL)2Y 1x185 RM/35 64/110 kV" , length_km=15.)
+    net.line["endtemp_degree"] = 80
+
+    pp.create_sgen(net, b2, sn_mva=200., p_mw=0, k=1.2)
+    return net
+
 def test_max_branch_results(three_bus_example):
     net = three_bus_example
     sc.calc_sc(net, case="max", ip=True, ith=True, branch_results=True)
@@ -52,6 +67,15 @@ def test_max_branch_results(three_bus_example):
     assert np.allclose(net.res_line_sc.ikss_ka.values, np.array([ 0.49593034,  0.4988896 ]))
     assert np.allclose(net.res_line_sc.ip_ka.values, np.array([ 0.92787443,  0.9251165 ]))
     assert np.allclose(net.res_line_sc.ith_ka.values, np.array([ 0.49811957,  0.50106881]))
+
+
+def test_big_sgen_results(big_sgen_example):
+    net = big_sgen_example
+    sc.calc_sc(net, case="max", ip=True, ith=True, branch_results=True)
+    assert np.allclose(net.res_bus_sc.ikss_ka.values, np.array([1.78453722, 1.75560368, 1.72233192]))
+    assert np.allclose(net.res_line_sc.ikss_ka.values, np.array([1.25967331, 1.72233192 ]))
+    assert np.allclose(net.res_line_sc.ip_ka.values, np.array([1.78144709, 2.65532524]))
+    assert np.allclose(net.res_line_sc.ith_ka.values, np.array([1.26511638, 1.7298553]))
 
 def test_min_branch_results(three_bus_example):
     net = three_bus_example
