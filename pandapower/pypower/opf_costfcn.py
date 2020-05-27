2c4ed9ff66e0c6de9d9bdef3b814e87cd632e7a0
lthurner <leon.thurner@iee.fraunhofer.de>
2020-05-27 16:58:29 +0200

    fix opf cost function Closes #770

--- pandapower/pypower/opf_costfcn.py
+++ pandapower/pypower/opf_costfcn.py
@@ -58,7 +58,7 @@ def opf_costfcn(x, om, return_hessian=False):
     # formulation, pwl cost is the sum of the y variables.
     ipol = find(gencost[:, MODEL] == POLYNOMIAL)   ## poly MW and MVAr costs
     xx = r_[ Pg, Qg ] * baseMVA
-    if any(ipol):
+    if len(ipol)>0:
         f = sum( totcost(gencost[ipol, :], xx[ipol]) )  ## cost of poly P or Q
     else:
         f = 0
