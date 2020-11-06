fe9f275da0547a4876aea9c7af11afc49685006e
Glaucon Garcia <glaucongarcia@gmail.com>
2020-11-06 10:12:30 -0300

    updating the converter to powermodels cause it was not considering the power flow limits of the branches

--- pandapower/converter/powermodels/to_pm.py
+++ pandapower/converter/powermodels/to_pm.py
@@ -274,11 +274,6 @@ def ppc_to_pm(net, ppci):
         branch["rate_b"] = row[RATE_B].real / sn_mva
         branch["rate_c"] = row[RATE_C].real / sn_mva
 
-        # making the flows unrestricted
-        # branch["rate_a"] = 99999.
-        # branch["rate_b"] = 99999.
-        # branch["rate_c"] = 99999.
-
         branch["f_bus"] = int(row[F_BUS].real) + 1
         branch["t_bus"] = int(row[T_BUS].real) + 1
         branch["br_status"] = int(row[BR_STATUS].real)
@@ -359,6 +354,17 @@ def add_pm_options(pm, net):
     else:
         pm["pm_time_limit"], pm["pm_nl_time_limit"], pm["pm_mip_time_limit"] = np.inf, np.inf, np.inf
     pm["correct_pm_network_data"] = net._options["correct_pm_network_data"]
+
+    # add report_duals, branch_limits and/or objective options, if present
+    if "report_duals" in net._options:
+        pm["report_duals"] = net._options["report_duals"]
+
+    if "branch_limits" in net._options:
+        pm["branch_limits"] = net._options["branch_limits"]
+    
+    if "objective" in net._options:
+        pm["objective"] = net._options["objective"]
+
     return pm
 
 
--- pandapower/opf/run_powermodels.jl
+++ pandapower/opf/run_powermodels.jl
@@ -8,7 +8,13 @@ function run_powermodels(json_path)
     solver = PP2PM.get_solver(pm["pm_solver"], pm["pm_nl_solver"], pm["pm_mip_solver"], 
     pm["pm_log_level"], pm["pm_time_limit"], pm["pm_nl_time_limit"], pm["pm_mip_time_limit"])
 
-    result = PowerModels.run_opf(pm, model, solver,
-                                    setting = Dict("output" => Dict("branch_flows" => true)))
+    report_duals = get(pm, "report_duals", false)  # true or false
+    branch_limits = get(pm, "branch_limits", "hard")  # "hard", "soft", "none"
+    objective = get(pm, "objective", "default")  # TODO: check best options
+
+    settings = Dict("output" => Dict("branch_flows" => true,
+                                     "duals" => report_duals))
+
+    result = PowerModels.run_opf(pm, model, solver, setting=settings) 
     return result
 end
