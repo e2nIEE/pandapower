1c358816fcb6d201050152f02194ee3afe82b62d
rbolgaryn <roman.bolgaryn@iee.fraunhofer.de>
2020-12-03 15:14:31 +0100

    Revert "control.ConstControl: added self.modify_values to ConstControl to be able to inherit from ConstControl and override self.modify_values for a custom post-processing step of the timeseries values"
    This reverts commit 3aafd037

--- pandapower/test/control/test_const_control.py
+++ pandapower/test/control/test_const_control.py
@@ -5,7 +5,6 @@
 
 import pytest
 import pandas as pd
-import numpy as np
 
 import pandapower as pp
 import pandapower.networks as nw
@@ -16,40 +15,16 @@ import logging as log
 logger = log.getLogger(__name__)
 
 
-class ModifiedConstControl(pp.control.ConstControl):
-    def modify_values(self):
-        self.values = np.square(self.values)
-
-
 def test_write():
     net = nw.simple_four_bus_system()
     ds = pp.timeseries.DFData(pd.DataFrame(data=[[0., 1., 2.], [2., 3., 4.]]))
     c1 = pp.control.ConstControl(net, 'sgen', 'p_mw', element_index=[0, 1], profile_name=[0, 1], data_source=ds)
     pp.create_sgen(net, 0, 0)
-    c2 = pp.control.ConstControl(net, 'sgen', 'p_mw', element_index=[2], profile_name=[2], data_source=ds,
-                                 scale_factor=0.5)
-    for t in range(2):
-        c1.time_step(net, t)
-        c1.control_step(net)
-        c2.time_step(net, t)
-        c2.control_step(net)
-        assert np.all(net.sgen.p_mw.values == ds.df.loc[t].values * np.array([1, 1, 0.5]))
-
-
-def test_modified_values():
-    net = nw.simple_four_bus_system()
-    ds = pp.timeseries.DFData(pd.DataFrame(data=[[0., 1., 2.], [2., 3., 4.]]))
-    c1 = ModifiedConstControl(net, 'sgen', 'p_mw', element_index=[0, 1], profile_name=[0, 1], data_source=ds,
-                              scale_factor=0.5)
-    pp.create_sgen(net, 0, 0)
-    c2 = ModifiedConstControl(net, 'sgen', 'p_mw', element_index=[2], profile_name=[2], data_source=ds,
-                              scale_factor=0.25)
-    for t in range(2):
-        c1.time_step(net, t)
-        c1.control_step(net)
-        c2.time_step(net, t)
-        c2.control_step(net)
-        assert np.all(net.sgen.p_mw.values == np.square(ds.df.loc[t].values * np.array([0.5, 0.5, 0.25])))
+    c2 = pp.control.ConstControl(net, 'sgen', 'p_mw', element_index=[2], profile_name=[2], data_source=ds)
+    c1.time_step(net, 0)
+    c1.control_step(net)
+    c2.time_step(net, 0)
+    c2.control_step(net)
 
 
 if __name__ == '__main__':
