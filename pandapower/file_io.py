db1bf9ce539077990f4c02d35318de11a46233a1
Roman Bolgaryn <roman.bolgaryn@iee.fraunhofer.de>
2020-12-14 21:06:36 +0100

    Update file_io.py

--- pandapower/file_io.py
+++ pandapower/file_io.py
@@ -178,7 +178,7 @@ def from_excel(filename, convert=True):
     elif pd_version < version.parse("0.24"):
         xls = pd.ExcelFile(filename).parse(sheet_name=None)
     else:
-        xls = pd.read_excel(ExcelFile(filename), sheet_name=None, index_col=0, engine="openpyxl"))
+        xls = pd.read_excel(ExcelFile(filename), sheet_name=None, index_col=0, engine="openpyxl")
         
     try:
         net = io_utils.from_dict_of_dfs(xls)
