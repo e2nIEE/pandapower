ccc8a49b23785cf3e9ddb69495355a37f2a92944
Maryam Majidi <maryam.majidi@uni-kassel.de>
2021-01-28 15:35:02 +0100

    small changes

--- pandapower/opf/PpPmInterface/src/PpPmInterface.jl
+++ pandapower/opf/PpPmInterface/src/PpPmInterface.jl
@@ -1,8 +1,14 @@
 module PpPmInterface
 
+# import Pkg
+# Pkg.activate(".")
+# Pkg.add("JSON")
+# Pkg.build("JSON")
+# Pkg.resolve()
+
 import JuMP
 import JSON
-import PowerModels
+using PowerModels
 using Cbc
 using Ipopt
 using Juniper
@@ -15,27 +21,31 @@ catch e
     end
 end
 
-try
-    using KNITRO
-catch e
-    if isa(e, LoadError)
-        println("Cannot import KNITRO. That's fine if you do not plan to use it")
-    end
-end
+# try
+#     using KNITRO
+# catch e
+#     if isa(e, LoadError)
+#         println("Cannot import KNITRO. That's fine if you do not plan to use it")
+#     end
+# end
+#
+# try
+#
+# using SCIP
+# catch e
+#     if isa(e, LoadError)
+#         println("Cannot import SCIP. That's fine if you do not plan to use it")
+#     end
+# end
 
-try
-    using SCIP
-catch e
-    if isa(e, LoadError)
-        println("Cannot import SCIP. That's fine if you do not plan to use it")
-    end
-end
+export load_pm_from_json
+export run_powermodels
 
-include("input/pp_2_pm.jl")
+include("input/pp_to_pm.jl")
 include("pm_models/run_powermodels.jl")
-include("pm_models/run_powermodels_mn_storage.jl")
-include("pm_models/run_powermodels_ots.jl")
-include("pm_models/run_powermodels_powerflow.jl")
-include("pm_models/run_powermodels_tnep.jl")
+# include("pm_models/run_powermodels_mn_storage.jl")
+# include("pm_models/run_powermodels_ots.jl")
+# include("pm_models/run_powermodels_powerflow.jl")
+# include("pm_models/run_powermodels_tnep.jl")
 
 end
