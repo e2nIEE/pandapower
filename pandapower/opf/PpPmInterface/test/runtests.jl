49d14c689c0dff40ee17bfec1844a0030b486154
Maryam Majidi <maryam.majidi@uni-kassel.de>
2021-01-28 15:34:33 +0100

    fix activation mode into test file

--- pandapower/opf/PpPmInterface/test/runtests.jl
+++ pandapower/opf/PpPmInterface/test/runtests.jl
@@ -1,6 +1,15 @@
+import Pkg
+Pkg.activate(joinpath(homedir(), "pandapower", "pandapower", "opf","PpPmInterface"))
+
+# Pkg.activate("test")
+# Pkg.dev(".")
+
 using PpPmInterface
 using Test
 
+json_path = joinpath(pwd(), "test", "test_net.json");
+
 @testset "PpPmInterface.jl" begin
-    # Write your tests here.
+    pm = load_pm_from_json(json_path)
+    # result = run_powermodels(json_path)
 end
