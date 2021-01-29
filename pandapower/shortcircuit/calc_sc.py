Updated example for 1ph SC calculation Fixed some 1ph sc test (TODO) Fix left 1ph test

pandapower/shortcircuit/calc_sc.py
@@ -123,7 +123,7 @@ def calc_sc(net, bus=None,
         logger.warning("Branch results are in beta mode and might not always be reliable, "
                        "especially for transformers")
 
-    # Convert bus to numpy array for better performance
+    # Convert bus to numpy array
     if bus is None:
         bus = net.bus.index.values
     elif isinstance(bus, int):
@@ -216,12 +216,13 @@ def _calc_sc_1ph(net, bus):
     _add_auxiliary_elements(net)
     # pos. seq bus impedance
     ppc, ppci = _pd2ppc(net)
+    # _init_ppc(net)
     _calc_ybus(ppci)
 
     # zero seq bus impedance
     ppc_0, ppci_0 = _pd2ppc_zero(net)
     _calc_ybus(ppci_0)
-
+    
     if net["_options"]["inverse_y"]:
         _calc_zbus(net, ppci)
         _calc_zbus(net, ppci_0)
@@ -230,15 +231,21 @@ def _calc_sc_1ph(net, bus):
         ppci["internal"]["ybus_fact"] = factorized(ppci["internal"]["Ybus"])
         ppci_0["internal"]["ybus_fact"] = factorized(ppci_0["internal"]["Ybus"])
 
-    ppci_bus = net._pd2ppc_lookups["bus"][bus]
-    _calc_rx(net, ppci, ppci_bus)
+    ppci_bus =  net["_pd2ppc_lookups"]["bus"][bus[np.in1d(bus, net._is_elements_final["bus_is_idx"])]]
+    _calc_rx(net, ppci, ppci_bus=ppci_bus)
     _add_kappa_to_ppc(net, ppci)
 
-    _calc_rx(net, ppci_0, ppci_bus)
-    _calc_ikss_1ph(net, ppci, ppci_0, bus=bus)
+    _calc_rx(net, ppci_0, ppci_bus=ppci_bus)
+    _calc_ikss_1ph(net, ppci, ppci_0, ppci_bus=ppci_bus)
 
     if net._options["branch_results"]:
-        _calc_branch_currents(net, ppci, ppci_bus)
+        _calc_branch_currents(net, ppci, ppci_bus=ppci_bus)
+        
+    _copy_result_ppci_orig(ppci, ppci, ppci_bus,
+                           calc_options=net._options)
+    _copy_result_ppci_orig(ppci_0, ppci_0, ppci_bus,
+                           calc_options=net._options)
+
     ppc_0 = _copy_results_ppci_to_ppc(ppci_0, ppc_0, "sc")
     ppc = _copy_results_ppci_to_ppc(ppci, ppc, "sc")
     _extract_results(net, ppc, ppc_0, bus=bus)

pandapower/shortcircuit/currents.py
@@ -18,6 +18,7 @@ from pandapower.shortcircuit.impedance import _calc_zbus_diag
 from pandapower.pypower.pfsoln import pfsoln as pfsoln_pypower
 from pandapower.pf.ppci_variables import _get_pf_variables_from_ppci
 
+
 def _calc_ikss(net, ppc, ppci_bus=None):
     # Vectorized for multiple bus
     if ppci_bus is None:
@@ -59,15 +60,13 @@ def _calc_ikss(net, ppc, ppci_bus=None):
     _current_source_current(net, ppc)
     
 
-
-
-def _calc_ikss_1ph(net, ppc, ppc_0, bus=None):
+def _calc_ikss_1ph(net, ppc, ppc_0, ppci_bus=None):
     # Vectorized for multiple bus
-    if bus is None:
+    if ppci_bus is None:
         # Slice(None) is equal to : select
-        bus_idx = slice(None)
+        bus_idx = np.arange(ppc["bus"].shape[0])
     else:
-        bus_idx = net._pd2ppc_lookups["bus"][bus] #bus where the short-circuit is calculated (j)
+        bus_idx = ppci_bus
 
     case = net._options["case"]
     c = ppc["bus"][bus_idx, C_MIN] if case == "min" else ppc["bus"][bus_idx, C_MAX]

pandapower/shortcircuit/ppc_conversion.py
@@ -54,17 +54,17 @@ def _create_k_updated_ppci(net, ppci_orig, bus):
         ppci["bus"][np.ix_(ps_gen_bus_mask, [GS, BS, GS_P, BS_P])] /=\
             ppci["bus"][np.ix_(ps_gen_bus_mask, [K_SG])]
         ppci["branch"][np.ix_(ps_trafo_mask, [BR_X, BR_R])] *=\
-            ppci["branch"][np.ix_(ps_trafo_mask, [K_ST])]
+            ppci["branch"][np.ix_(ps_trafo_mask, [K_ST])] / ppci["branch"][np.ix_(ps_trafo_mask, [K_T])]
 
     gen_bus_mask = np.isnan(ppci["bus"][:, K_SG]) & (~np.isnan(ppci["bus"][:, K_G]))
     if np.any(gen_bus_mask):
         ppci["bus"][np.ix_(gen_bus_mask, [GS, BS, GS_P, BS_P])] /=\
             ppci["bus"][np.ix_(gen_bus_mask, [K_G])]
 
-    trafo_mask = np.isnan(ppci["branch"][:, K_ST]) & (~np.isnan(ppci["branch"][:, K_T]))
-    if np.any(trafo_mask):
-        ppci["branch"][np.ix_(trafo_mask, [BR_X, BR_R])] *=\
-            ppci["branch"][np.ix_(trafo_mask, [K_T])]
+    # trafo_mask = np.isnan(ppci["branch"][:, K_ST]) & (~np.isnan(ppci["branch"][:, K_T]))
+    # if np.any(trafo_mask):
+    #     ppci["branch"][np.ix_(trafo_mask, [BR_X, BR_R])] *=\
+    #         ppci["branch"][np.ix_(trafo_mask, [K_T])]
     
     bus_ppci = {}
     if ps_gen_bus.size > 0:

pandapower/test/shortcircuit/test_iec60909_4.py
@@ -125,11 +125,13 @@ def iec_60909_4_small():
     H = pp.create_bus(net, vn_kv=30.)
     HG2 = pp.create_bus(net, vn_kv=10) 
 
-    pp.create_ext_grid(net, b1, s_sc_max_mva=38 * 380 * np.sqrt(3), rx_max=0.1)
-    pp.create_ext_grid(net, b5, s_sc_max_mva=16 * 110 * np.sqrt(3), rx_max=0.1)
+    pp.create_ext_grid(net, b1, s_sc_max_mva=38 * 380 * np.sqrt(3), rx_max=0.1, x0x_max=3, r0x0_max=0.15)
+    pp.create_ext_grid(net, b5, s_sc_max_mva=16 * 110 * np.sqrt(3), rx_max=0.1, x0x_max=3.3, r0x0_max=0.2)
 
     t1 = pp.create_transformer_from_parameters(net, b3, HG2, sn_mva=100,
-        pfe_kw=0, i0_percent=0, vn_hv_kv=120., vn_lv_kv=10.5, vk_percent=12, vkr_percent=0.5)
+        pfe_kw=0, i0_percent=0, vn_hv_kv=120., vn_lv_kv=10.5, vk_percent=12, vkr_percent=0.5,
+        vk0_percent=12, vkr0_percent=0.5, mag0_percent=100, mag0_rx=0, si0_hv_partial=0.5, 
+        shift_degree=5, vector_group="YNd")
     pp.create_gen(net, HG2, p_mw=0.9 * 100, vn_kv=10.5,
                   xdss_pu=0.16, rdss_ohm=0.005, cos_phi=0.9, sn_mva=100, pg_percent=7.5,
                   slack=True, power_station_trafo=t1)
@@ -153,16 +155,20 @@ def iec_60909_4_small():
 
     pp.create_line_from_parameters(net, b2, b3, name="L1",
         c_nf_per_km=0, max_i_ka=0,  # FIXME: Optional for SC
-        length_km=20, r_ohm_per_km=0.12, x_ohm_per_km=0.39,)
+        length_km=20, r_ohm_per_km=0.12, x_ohm_per_km=0.39,
+        r0_ohm_per_km=0.32, x0_ohm_per_km=1.26, c0_nf_per_km=0, g0_us_per_km=0)
     pp.create_line_from_parameters(net, b2, b5, name="L3a",
         c_nf_per_km=0, max_i_ka=0,
-        length_km=5, r_ohm_per_km=0.12, x_ohm_per_km=0.39)
+        length_km=5, r_ohm_per_km=0.12, x_ohm_per_km=0.39,
+        r0_ohm_per_km=0.52, x0_ohm_per_km=1.86, c0_nf_per_km=0, g0_us_per_km=0)
     pp.create_line_from_parameters(net, b2, b5, name="L3b",
         c_nf_per_km=0, max_i_ka=0,
-        length_km=5, r_ohm_per_km=0.12, x_ohm_per_km=0.39)
+        length_km=5, r_ohm_per_km=0.12, x_ohm_per_km=0.39,
+        r0_ohm_per_km=0.52, x0_ohm_per_km=1.86, c0_nf_per_km=0, g0_us_per_km=0)
     pp.create_line_from_parameters(net, b5, b3, name="L4",
         c_nf_per_km=0, max_i_ka=0,
-        length_km=10, r_ohm_per_km=0.096, x_ohm_per_km=0.388)
+        length_km=10, r_ohm_per_km=0.096, x_ohm_per_km=0.388,
+        r0_ohm_per_km=0.22, x0_ohm_per_km=1.1, c0_nf_per_km=0, g0_us_per_km=0)
 
     return net
 
@@ -173,7 +179,8 @@ def iec_60909_4_small_gen_only():
     HG2 = pp.create_bus(net, vn_kv=10)
 
     t1 = pp.create_transformer_from_parameters(net, b3, HG2, sn_mva=100,
-        pfe_kw=0, i0_percent=0, vn_hv_kv=120., vn_lv_kv=10.5, vk_percent=12, vkr_percent=0.5)
+        pfe_kw=0, i0_percent=0, vn_hv_kv=120., vn_lv_kv=10.5, vk_percent=12, vkr_percent=0.5,
+        vk0_percent=12, vkr0_percent=0.5, mag0_percent=100, mag0_rx=0, si0_hv_partial=0.5, vector_group="YNd")
     pp.create_gen(net, HG2, p_mw=0.9 * 100, vn_kv=10.5,
                   xdss_pu=0.16, rdss_ohm=0.005, cos_phi=0.9, sn_mva=100, pg_percent=7.5,
                   slack=True, power_station_trafo=t1)
@@ -306,6 +313,11 @@ def test_iec_60909_4_2ph():
 
 
 @pytest.mark.skip("1ph gen-close sc calculation still under develop")
+def test_iec_60909_4_small_1ph():
+    net = iec_60909_4_small()
+    sc.calc_sc(net, fault="1ph", case="max", ip=True, tk_s=0.1, kappa_method="C") 
+
+@pytest.mark.skip("1ph gen-close sc calculation still under develop")
 def test_iec_60909_4_1ph():
     net = iec_60909_4()
     sc.calc_sc(net, fault="1ph", case="max", ip=True, tk_s=0.1, kappa_method="C")
@@ -321,3 +333,4 @@ def test_iec_60909_4_1ph():
 
 if __name__ == '__main__':
     pytest.main(["test_iec60909_4.py"])
+