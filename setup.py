0addcfda03b7b1434883947902f70fc6cb593706
Roman Bolgaryn <roman.bolgaryn@iee.fraunhofer.de>
2021-02-22 17:49:55 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
     install_requires=["pandas>=0.17",
                       "networkx",
                       "scipy",
-                      "numpy>=0.11,<1.2.0",
+                      "numpy>=0.11,<1.20",
                       "packaging",
                       "xlsxwriter",
                       "xlrd",
