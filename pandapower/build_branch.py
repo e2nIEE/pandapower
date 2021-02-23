Updated test in 1ph sc calculation; Updated changelog; Styling fix;

pandapower/build_branch.py
@@ -133,7 +133,7 @@ def _calc_line_parameter(net, ppc, elm="line", ppc_elm="branch"):
     length_km = line["length_km"].values
     parallel = line["parallel"].values
     base_kv = ppc["bus"][from_bus, BASE_KV]
-    baseR = np.square(base_kv) / (3 * net.sn_mva) if mode == "pf_3ph" else   np.square(base_kv) / net.sn_mva 
+    baseR = np.square(base_kv) / (3 * net.sn_mva) if mode == "pf_3ph" else   np.square(base_kv) / net.sn_mva
     branch[f:t, F_BUS] = from_bus
     branch[f:t, T_BUS] = to_bus
     branch[f:t, BR_R] = line["r_ohm_per_km"].values * length_km / baseR / parallel
@@ -313,14 +313,14 @@ def _calc_y_from_dataframe(mode,trafo_df, vn_lv, vn_trafo_lv, sn_mva):
         **subsceptance** (1d array, np.complex128) - The subsceptance in pu in
         the form (-b_img, -b_real)
     """
-   
+
     baseR = np.square(vn_lv) / (3*sn_mva) if mode == 'pf_3ph' else np.square(vn_lv) / sn_mva
     vn_lv_kv = get_trafo_values(trafo_df, "vn_lv_kv")
     pfe = get_trafo_values(trafo_df, "pfe_kw") * 1e-3
     parallel = get_trafo_values(trafo_df, "parallel")
 
     ### Calculate subsceptance ###
-   
+
     vnl_squared = (vn_lv_kv ** 2)/3 if mode == 'pf_3ph' else  vn_lv_kv **2
     b_real = pfe / vnl_squared * baseR
     i0 = get_trafo_values(trafo_df, "i0_percent")
@@ -810,7 +810,8 @@ def _trafo_df_from_trafo3w(net, seq=1):
     if seq==1:
         _calculate_sc_voltages_of_equivalent_transformers(t3, trafo2, mode)
     elif seq==0:
-        assert mode == "sc"
+        if mode != "sc":
+            raise NotImplementedError("0 seq impedance calculation only implemented for short-circuit calculation!")
         _calculate_sc_voltages_of_equivalent_transformers_zero_sequence(t3, trafo2,)
     else:
         raise UserWarning("Unsupported sequence for trafo3w convertion")

pandapower/shortcircuit/currents.py
@@ -19,13 +19,9 @@ from pandapower.pypower.pfsoln import pfsoln as pfsoln_pypower
 from pandapower.pf.ppci_variables import _get_pf_variables_from_ppci
 
 
-def _calc_ikss(net, ppci, ppci_bus=None):
+def _calc_ikss(net, ppci, ppci_bus):
     # Vectorized for multiple bus
-    if ppci_bus is None:
-        # Slice(None) is equal to : select
-        bus_idx = np.arange(ppci["bus"].shape[0])
-    else:
-        bus_idx = ppci_bus
+    bus_idx = ppci_bus
 
     fault = net._options["fault"]
     case = net._options["case"]
@@ -59,13 +55,9 @@ def _calc_ikss(net, ppci, ppci_bus=None):
     _current_source_current(net, ppci)
 
 
-def _calc_ikss_1ph(net, ppci, ppci_0, ppci_bus=None):
+def _calc_ikss_1ph(net, ppci, ppci_0, ppci_bus):
     # Vectorized for multiple bus
-    if ppci_bus is None:
-        # Slice(None) is equal to : select
-        bus_idx = np.arange(ppci["bus"].shape[0])
-    else:
-        bus_idx = ppci_bus
+    bus_idx = ppci_bus
 
     case = net._options["case"]
     c = ppci["bus"][bus_idx, C_MIN] if case == "min" else ppci["bus"][bus_idx, C_MAX]
@@ -135,82 +127,74 @@ def _calc_ith(net, ppci):
     ppci["bus"][:, ITH] = ith
 
 
-def _calc_ib_generator(net, ppci):
-    # Zbus = ppci["internal"]["Zbus"]
-    # baseI = ppci["internal"]["baseI"]
-    tk_s = net._options['tk_s']
-    c = 1.1
+# TODO: Ib for generation close bus
+# def _calc_ib_generator(net, ppci):
+#     # Zbus = ppci["internal"]["Zbus"]
+#     # baseI = ppci["internal"]["baseI"]
+#     tk_s = net._options['tk_s']
+#     c = 1.1
 
-    z_equiv = ppci["bus"][:, R_EQUIV] + ppci["bus"][:, X_EQUIV] * 1j
-    I_ikss = c / z_equiv / ppci["bus"][:, BASE_KV] / np.sqrt(3) * ppci["baseMVA"]
+#     z_equiv = ppci["bus"][:, R_EQUIV] + ppci["bus"][:, X_EQUIV] * 1j
+#     I_ikss = c / z_equiv / ppci["bus"][:, BASE_KV] / np.sqrt(3) * ppci["baseMVA"]
 
-    # calculate voltage source branch current
-    # I_ikss = ppci["bus"][:, IKSS1]
-    # V_ikss = (I_ikss * baseI) * Zbus
+#     # calculate voltage source branch current
+#     # I_ikss = ppci["bus"][:, IKSS1]
+#     # V_ikss = (I_ikss * baseI) * Zbus
 
-    gen = net["gen"][net._is_elements["gen"]]
-    gen_vn_kv = gen.vn_kv.values
+#     gen = net["gen"][net._is_elements["gen"]]
+#     gen_vn_kv = gen.vn_kv.values
 
-    # Check difference ext_grid and gen
-    gen_buses = ppci['gen'][:, GEN_BUS].astype(np.int64)
-    gen_mbase = ppci['gen'][:, MBASE]
-    gen_i_rg = gen_mbase / (np.sqrt(3) * gen_vn_kv)
+#     # Check difference ext_grid and gen
+#     gen_buses = ppci['gen'][:, GEN_BUS].astype(np.int64)
+#     gen_mbase = ppci['gen'][:, MBASE]
+#     gen_i_rg = gen_mbase / (np.sqrt(3) * gen_vn_kv)
 
-    gen_buses_ppc, gen_sn_mva, I_rG = _sum_by_group(gen_buses, gen_mbase, gen_i_rg)
+#     gen_buses_ppc, gen_sn_mva, I_rG = _sum_by_group(gen_buses, gen_mbase, gen_i_rg)
 
-    # shunt admittance of generator buses and generator short circuit current
-    # YS = ppci["bus"][gen_buses_ppc, GS] + ppci["bus"][gen_buses_ppc, BS] * 1j
-    # I_kG = V_ikss.T[:, gen_buses_ppc] * YS / baseI[gen_buses_ppc]
+#     # shunt admittance of generator buses and generator short circuit current
+#     # YS = ppci["bus"][gen_buses_ppc, GS] + ppci["bus"][gen_buses_ppc, BS] * 1j
+#     # I_kG = V_ikss.T[:, gen_buses_ppc] * YS / baseI[gen_buses_ppc]
 
-    xdss_pu = gen.xdss_pu.values
-    rdss_pu = gen.rdss_pu.values
-    cosphi = gen.cos_phi.values
-    X_dsss = xdss_pu * np.square(gen_vn_kv) / gen_mbase
-    R_dsss = rdss_pu * np.square(gen_vn_kv) / gen_mbase
+#     xdss_pu = gen.xdss_pu.values
+#     rdss_pu = gen.rdss_pu.values
+#     cosphi = gen.cos_phi.values
+#     X_dsss = xdss_pu * np.square(gen_vn_kv) / gen_mbase
+#     R_dsss = rdss_pu * np.square(gen_vn_kv) / gen_mbase
 
-    K_G = ppci['bus'][gen_buses, BASE_KV] / gen_vn_kv * c / (1 + xdss_pu * np.sin(np.arccos(cosphi)))
-    Z_G = (R_dsss + 1j * X_dsss)
+#     K_G = ppci['bus'][gen_buses, BASE_KV] / gen_vn_kv * c / (1 + xdss_pu * np.sin(np.arccos(cosphi)))
+#     Z_G = (R_dsss + 1j * X_dsss)
 
-    I_kG = c * ppci['bus'][gen_buses, BASE_KV] / np.sqrt(3) / (Z_G * K_G) * ppci["baseMVA"]
+#     I_kG = c * ppci['bus'][gen_buses, BASE_KV] / np.sqrt(3) / (Z_G * K_G) * ppci["baseMVA"]
 
-    dV_G = 1j * X_dsss * K_G * I_kG
-    V_Is = c * ppci['bus'][gen_buses, BASE_KV] / np.sqrt(3)
+#     dV_G = 1j * X_dsss * K_G * I_kG
+#     V_Is = c * ppci['bus'][gen_buses, BASE_KV] / np.sqrt(3)
 
-    # I_kG_contribution = I_kG.sum(axis=1)
-    # ratio_SG_ikss = I_kG_contribution / I_ikss
-    # close_to_SG = ratio_SG_ikss > 5e-2
+#     # I_kG_contribution = I_kG.sum(axis=1)
+#     # ratio_SG_ikss = I_kG_contribution / I_ikss
+#     # close_to_SG = ratio_SG_ikss > 5e-2
 
-    close_to_SG = I_kG / I_rG > 2
+#     close_to_SG = I_kG / I_rG > 2
 
-    if tk_s == 2e-2:
-        mu = 0.84 + 0.26 * np.exp(-0.26 * abs(I_kG) / I_rG)
-    elif tk_s == 5e-2:
-        mu = 0.71 + 0.51 * np.exp(-0.3 * abs(I_kG) / I_rG)
-    elif tk_s == 10e-2:
-        mu = 0.62 + 0.72 * np.exp(-0.32 * abs(I_kG) / I_rG)
-    elif tk_s >= 25e-2:
-        mu = 0.56 + 0.94 * np.exp(-0.38 * abs(I_kG) / I_rG)
-    else:
-        raise UserWarning('not implemented for other tk_s than 20ms, 50ms, 100ms and >=250ms')
+#     if tk_s == 2e-2:
+#         mu = 0.84 + 0.26 * np.exp(-0.26 * abs(I_kG) / I_rG)
+#     elif tk_s == 5e-2:
+#         mu = 0.71 + 0.51 * np.exp(-0.3 * abs(I_kG) / I_rG)
+#     elif tk_s == 10e-2:
+#         mu = 0.62 + 0.72 * np.exp(-0.32 * abs(I_kG) / I_rG)
+#     elif tk_s >= 25e-2:
+#         mu = 0.56 + 0.94 * np.exp(-0.38 * abs(I_kG) / I_rG)
+#     else:
+#         raise UserWarning('not implemented for other tk_s than 20ms, 50ms, 100ms and >=250ms')
 
-    mu = np.clip(mu, 0, 1)
+#     mu = np.clip(mu, 0, 1)
 
-    I_ikss_G = abs(I_ikss - np.sum((1 - mu) * I_kG, axis=1))
+#     I_ikss_G = abs(I_ikss - np.sum((1 - mu) * I_kG, axis=1))
 
-    # I_ikss_G = I_ikss - np.sum(abs(V_ikss.T[:, gen_buses_ppc]) * (1-mu) * I_kG, axis=1)
+#     # I_ikss_G = I_ikss - np.sum(abs(V_ikss.T[:, gen_buses_ppc]) * (1-mu) * I_kG, axis=1)
 
-    I_ikss_G = abs(I_ikss - np.sum(dV_G / V_Is * (1 - mu) * I_kG, axis=1))
+#     I_ikss_G = abs(I_ikss - np.sum(dV_G / V_Is * (1 - mu) * I_kG, axis=1))
 
-    return I_ikss_G
-
-
-def calc_branch_results(net, ppci, V):
-    Ybus = ppci["internal"]["Ybus"]
-    Yf = ppci["internal"]["Yf"]
-    Yt = ppci["internal"]["Yt"]
-    baseMVA, bus, gen, branch, ref, _, _, _, _, _, ref_gens = _get_pf_variables_from_ppci(ppci)
-    bus, gen, branch = pfsoln_pypower(baseMVA, bus, gen, branch, Ybus, Yf, Yt, V, ref, ref_gens)
-    ppci["bus"], ppci["gen"], ppci["branch"] = bus, gen, branch
+#     return I_ikss_G
 
 
 def _calc_branch_currents(net, ppci, ppci_bus):
@@ -233,7 +217,6 @@ def _calc_branch_currents(net, ppci, ppci_bus):
         V_ikss = V_ikss[:, ppci_bus]
     else:
         ybus_fact = ppci["internal"]["ybus_fact"]
-        # TODO: Check v_ikss size
         V_ikss = np.zeros((n_bus, n_sc_bus), dtype=np.complex)
         for ix, b in enumerate(ppci_bus):
             ikss = np.zeros(n_bus, dtype=np.complex)
@@ -313,5 +296,6 @@ def _calc_branch_currents(net, ppci, ppci_bus):
             ppci["branch"][:, ITH_F] = np.nan_to_num(minmax(ith_all_f, axis=1) / baseI[fb])
             ppci["branch"][:, ITH_T] = np.nan_to_num(minmax(ith_all_t, axis=1) / baseI[fb])
 
+    # Update bus index for branch results
     if net._options["return_all_currents"]:
         ppci["internal"]["br_res_ks_ppci_bus"] = ppci_bus

pandapower/test/shortcircuit/test_1ph.py
@@ -153,7 +153,7 @@ def test_1ph_with_switches():
         check_results(net, vc, [0.52209347338, 2.0620266652, 2.3255761263, 2.3066467489])
 
 
-def iec_60909_4_small(n_t3=1, num_earth=1):
+def iec_60909_4_small(n_t3=1, num_earth=1, with_gen=False):
     net = pp.create_empty_network()
 
     b1 = pp.create_bus(net, vn_kv=380.)
@@ -161,7 +161,7 @@ def iec_60909_4_small(n_t3=1, num_earth=1):
     b3 = pp.create_bus(net, vn_kv=110.)
     b5 = pp.create_bus(net, vn_kv=110.)
     b8 = pp.create_bus(net, vn_kv=30.)
-    # H = pp.create_bus(net, vn_kv=30.)
+    HG2 = pp.create_bus(net, vn_kv=10)
 
     pp.create_ext_grid(net, b1, s_sc_max_mva=38 * 380 * np.sqrt(3), rx_max=0.1, x0x_max=3, r0x0_max=0.15,
                        s_sc_min_mva=38 * 380 * np.sqrt(3) / 10, rx_min=0.1, x0x_min=3, r0x0_min=0.15,)
@@ -216,6 +216,15 @@ def iec_60909_4_small(n_t3=1, num_earth=1):
         length_km=10, r_ohm_per_km=0.096, x_ohm_per_km=0.388,
         r0_ohm_per_km=0.22, x0_ohm_per_km=1.1, c0_nf_per_km=0, g0_us_per_km=0, endtemp_degree=80)
 
+    if with_gen:
+        t1 = pp.create_transformer_from_parameters(net, b3, HG2, sn_mva=100,
+            pfe_kw=0, i0_percent=0, vn_hv_kv=120., vn_lv_kv=10.5, vk_percent=12, vkr_percent=0.5,
+            vk0_percent=12, vkr0_percent=0.5, mag0_percent=100, mag0_rx=0, si0_hv_partial=0.5,
+            shift_degree=5, vector_group="YNd")
+        pp.create_gen(net, HG2, p_mw=0.9 * 100, vn_kv=10.5,
+                      xdss_pu=0.16, rdss_ohm=0.005, cos_phi=0.9, sn_mva=100, pg_percent=7.5,
+                      slack=True, power_station_trafo=t1)
+
     return net
 
 def test_iec60909_example_4_one_trafo3w():
@@ -253,6 +262,18 @@ def test_iec60909_example_4_two_trafo3w_two_earth():
 
     sc.calc_sc(net, fault="1ph", case="min")
     assert np.allclose(net.res_bus_sc.ikss_ka.values[:4], np.array(ikss_pf_min), atol=1e-4)
+
+
+@pytest.mark.skip("1ph gen-close sc calculation still under develop")
+def test_iec_60909_4_small_with_gen_1ph():
+    net = iec_60909_4_small(n_t3=2, num_earth=1, with_gen=True)
+    sc.calc_sc(net, fault="1ph", case="max", ip=True, tk_s=0.1, kappa_method="C")
+
+    ikss_max = [24.6109, 17.4363, 12.7497, 18.6883]
+
+    ikss_min = [3.5001, 8.4362, 7.4743, 7.7707]
+    ip_min = [8.6843, 21.6173, 18.0242, 19.4261]
+    assert np.allclose(net.res_bus_sc.ikss_ka.values[:4], np.array(ikss_max), atol=1e-4)
 
 
 if __name__ == "__main__":

pandapower/test/shortcircuit/test_iec60909_4.py
@@ -43,11 +43,11 @@ def iec_60909_4():
     pp.create_gen(net, HG2, p_mw=0.9 * 100, vn_kv=10.5,
                   xdss_pu=0.16, rdss_ohm=0.005, cos_phi=0.9, sn_mva=100, pg_percent=7.5,
                   slack=True, power_station_trafo=t2)
-    
+
     # Add gen 3
     pp.create_gen(net, b6, p_mw=0.9 * 100, vn_kv=10.5,
                   xdss_pu=0.1, rdss_ohm=0.018, cos_phi=0.8, sn_mva=10, pg_percent=5)
-    
+
     pp.create_transformer3w_from_parameters(net,
         hv_bus=b1, mv_bus=b2, lv_bus=H,
         vn_hv_kv=400, vn_mv_kv=120, vn_lv_kv=30,
@@ -88,7 +88,7 @@ def iec_60909_4():
     for _ in range(2):
         pp.create_motor(net, b7, pn_mech_mw=2.0, cos_phi=0.89, cos_phi_n=0.89,
                         efficiency_n_percent=96.8,
-                        vn_kv=10, rx=0.1, lrc_pu=5.2)        
+                        vn_kv=10, rx=0.1, lrc_pu=5.2)
 
     pp.create_line_from_parameters(net, b2, b3, name="L1",
         c_nf_per_km=0, max_i_ka=0,  # FIXME: Optional for SC
@@ -123,14 +123,14 @@ def iec_60909_4_small():
     b5 = pp.create_bus(net, vn_kv=110.)
     b8 = pp.create_bus(net, vn_kv=30.)
     H = pp.create_bus(net, vn_kv=30.)
-    HG2 = pp.create_bus(net, vn_kv=10) 
+    HG2 = pp.create_bus(net, vn_kv=10)
 
     pp.create_ext_grid(net, b1, s_sc_max_mva=38 * 380 * np.sqrt(3), rx_max=0.1, x0x_max=3, r0x0_max=0.15)
     pp.create_ext_grid(net, b5, s_sc_max_mva=16 * 110 * np.sqrt(3), rx_max=0.1, x0x_max=3.3, r0x0_max=0.2)
 
     t1 = pp.create_transformer_from_parameters(net, b3, HG2, sn_mva=100,
         pfe_kw=0, i0_percent=0, vn_hv_kv=120., vn_lv_kv=10.5, vk_percent=12, vkr_percent=0.5,
-        vk0_percent=12, vkr0_percent=0.5, mag0_percent=100, mag0_rx=0, si0_hv_partial=0.5, 
+        vk0_percent=12, vkr0_percent=0.5, mag0_percent=100, mag0_rx=0, si0_hv_partial=0.5,
         shift_degree=5, vector_group="YNd")
     pp.create_gen(net, HG2, p_mw=0.9 * 100, vn_kv=10.5,
                   xdss_pu=0.16, rdss_ohm=0.005, cos_phi=0.9, sn_mva=100, pg_percent=7.5,
@@ -207,7 +207,7 @@ def iec_60909_4_2gen():
     pp.create_gen(net, HG2, p_mw=0.9 * 100, vn_kv=10.5,
                   xdss_pu=0.16, rdss_ohm=0.005, cos_phi=0.9, sn_mva=100, pg_percent=7.5,
                   slack=True, power_station_trafo=t2)
-    
+
     pp.create_line_from_parameters(net, b3, b4, name="L2",
         c_nf_per_km=0, max_i_ka=0,
         length_km=10, r_ohm_per_km=0.12, x_ohm_per_km=0.39)
@@ -218,29 +218,29 @@ def test_iec_60909_4_3ph_small_without_gen():
     net = iec_60909_4_small()
     # Deactivate all gens
     net.gen = net.gen.iloc[0:0, :]
-    
+
     sc.calc_sc(net, fault="3ph", case="max", ip=True, tk_s=0.1, kappa_method="C")
     ikss_pf = [40.3390,	28.4130, 14.2095, 28.7195, 13.4191]
     ip_pf = [99.7374, 72.6580, 32.1954, 72.1443, 36.5036]
-    
+
     assert np.allclose(net.res_bus_sc.ikss_ka.values[:5], np.array(ikss_pf), atol=1e-3)
     assert np.allclose(net.res_bus_sc.ip_ka.values[:5], np.array(ip_pf), atol=1e-3)
 
 def test_iec_60909_4_3ph_small_with_gen():
     net = iec_60909_4_small()
-    
+
     sc.calc_sc(net, fault="3ph", case="max", ip=True, tk_s=0.1, kappa_method="C")
     ikss_pf = [40.4754, 29.8334, 16.1684, 30.3573]
     ip_pf = [100.1164, 76.1134, 37.3576, 76.2689]
     ib_pf = [40.4754, 29.7337, 15.9593, 30.2245]
     kappa_pf = [1.7490, 1.8040, 1.6338 , 1.7765]
-    
+
     assert np.allclose(net.res_bus_sc.ikss_ka.values[:4], np.array(ikss_pf), atol=1e-3)
     assert np.allclose(net.res_bus_sc.ip_ka.values[:4], np.array(ip_pf), atol=1e-3)
 
 def test_iec_60909_4_3ph_small_gen_only():
     net = iec_60909_4_small_gen_only()
-    
+
     sc.calc_sc(net, fault="3ph", case="max", ip=True, ith=True, tk_s=0.1, kappa_method="C")
     ikss_pf = [1.9755, 39.5042]
     ip_pf = [5.2316, 104.1085]
@@ -249,10 +249,10 @@ def test_iec_60909_4_3ph_small_gen_only():
 
     assert np.allclose(net.res_bus_sc.ikss_ka[:2].values, np.array(ikss_pf), atol=1e-3)
     assert np.allclose(net.res_bus_sc.ip_ka[:2].values, np.array(ip_pf), atol=1e-3)
-    
+
 def test_iec_60909_4_3ph_2gen():
     net = iec_60909_4_2gen()
-    
+
     sc.calc_sc(net, fault="3ph", case="max", ip=True, tk_s=0.1, kappa_method="C")
     ikss_pf = [4.2821, 4.4280, 39.1090, 57.8129]
     ip_pf = [11.1157, 11.6306, 102.7821, 151.5569]
@@ -283,7 +283,7 @@ def test_iec_60909_4_3ph():
     # Ip for kappa B
     ip_pf = [100.5766, 80.8249, 45.8249, 36.8041, 83.6266,
              99.1910, 51.3864, 36.9201, 136.2801, 210.3159]
-    ip_standard_kappa_c = [100.5677, 80.6079, 45.8111, 36.8427, 
+    ip_standard_kappa_c = [100.5677, 80.6079, 45.8111, 36.8427,
                            83.4033, 98.1434, 51.6899, 36.9227]
     ib = [40.645, 31.570, 19.388, 16.017, 32.795, 34.028,
           23.212, 13.578, 42.3867, 68.4172]
@@ -292,7 +292,7 @@ def test_iec_60909_4_3ph():
 
     assert np.allclose(net.res_bus_sc.ikss_ka.values[:10], np.array(ikss), atol=1e-3)
     assert np.allclose(net.res_bus_sc.ip_ka.values[:8], np.array(ip_standard_kappa_c ), atol=1e-3)
-    assert np.allclose(net.res_bus_sc.skss_mw.values[:10], np.array(skss), atol=1e-2)  
+    assert np.allclose(net.res_bus_sc.skss_mw.values[:10], np.array(skss), atol=1e-2)
 
 
 def test_iec_60909_4_2ph():
@@ -311,11 +311,6 @@ def test_iec_60909_4_2ph():
     assert np.allclose(net.res_bus_sc.ip_ka.values[:10], np.array(ip), atol=1e-3)
     assert np.allclose(net.res_bus_sc.skss_mw.values[:10], np.array(skss), atol=1e-1)
 
-
-@pytest.mark.skip("1ph gen-close sc calculation still under develop")
-def test_iec_60909_4_small_1ph():
-    net = iec_60909_4_small()
-    sc.calc_sc(net, fault="1ph", case="max", ip=True, tk_s=0.1, kappa_method="C") 
 
 @pytest.mark.skip("1ph gen-close sc calculation still under develop")
 def test_iec_60909_4_1ph():