8a2e9abc2f30713058a88e42ac570378fc13d9e2
Maryam Majidi <maryam.majidi@uni-kassel.de>
2021-02-26 14:32:31 +0100

    fix getmodel problem

--- pandapower/opf/PpPmInterface/test/runtests.jl
+++ pandapower/opf/PpPmInterface/test/runtests.jl
@@ -1,14 +1,11 @@
 import Pkg
 # Pkg.activate(joinpath(homedir(),"GitHub", "pandapower", "pandapower", "opf","PpPmInterface")) # for lyu
 Pkg.activate(".") #for maryam
-# Pkg.update()
-# Pkg.build()
-# Pkg.resolve()
 
 using PpPmInterface
 using Test
 
-test_path = joinpath(pwd(), "test") # for maryam
+test_path = joinpath(pwd(), "test", "data") # for maryam
 # test_path=joinpath(homedir(),"pandapower", "pandapower", "opf", "PpPmInterface", "test") # for lyu
 
 # test_net=joinpath(test_path, "pm_test.json")
@@ -18,8 +15,9 @@ test_path = joinpath(pwd(), "test") # for maryam
 @testset "test converting net from pandapower to pm" begin
 
     @testset " simbench grid 1-HV-urban--0-sw with ipopt solver for DCOPF" begin
-        
+
         json_path = joinpath(test_path, "test_ipopt.json")
+
         pm = PpPmInterface.load_pm_from_json(json_path)
 
         @test length(pm["bus"]) == 82
@@ -27,6 +25,14 @@ test_path = joinpath(pwd(), "test") # for maryam
         @test length(pm["branch"]) == 116
         @test length(pm["load"]) == 177
 
+        model = PpPmInterface.get_model(pm["pm_model"])
+        # FIXME: when we register the package we need to change it to "DCPPowerModel"
+        @test string(model) == "PowerModels.DCPPowerModel"
+
+        solver = PpPmInterface.get_solver(pm["pm_solver"], pm["pm_nl_solver"], pm["pm_mip_solver"],
+        pm["pm_log_level"], pm["pm_time_limit"], pm["pm_nl_time_limit"], pm["pm_mip_time_limit"])
+
+        @test string(solver.optimizer_constructor) == "Ipopt.Optimizer"
     end
 
     # TODO: you can copy the test above and change some conditions, such as solver or model or even the grid and run test for it
@@ -41,8 +47,26 @@ test_path = joinpath(pwd(), "test") # for maryam
     #
     # end
 
-    end
 end
+
+@testset "test run_powermodels" begin
+    @testset " simbench grid 1-HV-urban--0-sw with ipopt solver for DCOPF" begin
+
+        json_path = joinpath(test_path, "test_ipopt.json")
+
+        result=run_powermodels(json_path)
+
+        @test typeof(result) == Dict{String,Any}
+        @test string(result["termination_status"]) == "LOCALLY_SOLVED"
+        @test isapprox(result["objective"], -96.1; atol = 1e0)
+        @test result["solve_time"] >= 0
+
+    end
+
+    # TODO: you can copy the test above and change some conditions, such as solver or model or even the grid and run test for it
+end
+
+
 
 # @testset "PpPmInterface.jl" begin
 #         # for i in ["","_ots","_powerflow","_tnep"]
@@ -67,7 +91,7 @@ end
 #         @test typeof(result)==Dict{String,Any}
 #         @test result["solve_time"]>=0
 # end
-
+#
 # json_path = joinpath(pwd(), "pandapower","opf","PpPmInterface","test", "test_net.json");
 # @testset "PpPmInterface.jl" begin
 #     pm = load_pm_from_json(json_path)
