1b264a616dd7b04aa1cdb5fc51579d578f685bc4
U_F_O <uk028764@student.uni-kassel.de>
2021-03-11 11:10:32 +0100

    Starting create new test files for julia

--- pandapower/opf/PpPmInterface/test/runtests.jl
+++ pandapower/opf/PpPmInterface/test/runtests.jl
@@ -73,30 +73,24 @@ end
 
 @testset "PpPmInterface.jl" begin
         # for i in ["","_ots","_powerflow","_tnep"]
-        #         comm="run_powermodels"*i*"(test_ipopt)"
-        #         result=Meta.parse($comm)
-        #         @test typeof(result)==Dict{String,Any}
+        #         comm="result=run_powermodels"*i*"(test_ipopt)"
+        #         eval(Meta.parse(comm))
+        #         @test isa(result, Dict{String,Any})
         #         @test result["solve_time"]>=0
         # end
         result=run_powermodels(test_ipopt)
-        @test typeof(result)==Dict{String,Any}
+        @test isa(result, Dict{String,Any})
         @test result["solve_time"]>=0
         # result=run_powermodels_mn_storage(test_Gurobi)
-        # @test typeof(result)==Dict{String,Any}
+        # @test isa(result, Dict{String,Any})
         # @test result["solve_time"]>=0
         result=run_powermodels_ots(test_net) #use Gurobi
-        @test typeof(result)==Dict{String,Any}
+        @test isa(result, Dict{String,Any})
         @test result["solve_time"]>=0
         result=run_powermodels_powerflow(test_ipopt)
-        @test typeof(result)==Dict{String,Any}
+        @test isa(result, Dict{String,Any})
         @test result["solve_time"]>=0
         result=run_powermodels_tnep(test_ipopt)
-        @test typeof(result)==Dict{String,Any}
+        @test isa(result, Dict{String,Any})
         @test result["solve_time"]>=0
 end
-
-# json_path = joinpath(pwd(), "pandapower","opf","PpPmInterface","test", "test_net.json");
-# @testset "PpPmInterface.jl" begin
-#     pm = load_pm_from_json(json_path)
-#     # result = run_powermodels(json_path)
-# end
