29cdcbf188342da4defe93d06892d57134bb329f
Roman Bolgaryn <roman.bolgaryn@iee.fraunhofer.de>
2021-07-19 15:27:25 +0200

    Update ppci_variables.py
    remove unused import

--- pandapower/pf/ppci_variables.py
+++ pandapower/pf/ppci_variables.py
@@ -4,7 +4,7 @@
 # and Energy System Technology (IEE), Kassel. All rights reserved.
 
 from pandapower.pypower.idx_bus import VM, VA
-from pandapower.pypower.idx_gen import GEN_BUS, GEN_STATUS, VG, SL_FAC
+from pandapower.pypower.idx_gen import GEN_BUS, GEN_STATUS, VG
 from pandapower.pypower.bustypes import bustypes
 from numpy import flatnonzero as find, pi, exp
 
