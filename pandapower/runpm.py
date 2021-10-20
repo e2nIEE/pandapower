1129d16f7ee828013188ffbb78e1b8aaf8989680
Maryam Majidi <maryam.majidi@uni-kassel.de>
2021-10-20 19:11:33 +0200

    add vd

--- pandapower/runpm.py
+++ pandapower/runpm.py
@@ -368,7 +368,78 @@ def runpm_storage_opf(net, calculate_voltage_angles=True,
     return storage_results
 
 
+def runpm_vd(net, pp_to_pm_callback=None, calculate_voltage_angles=True,
+                 trafo_model="t", delta=1e-8, trafo3w_losses="hv", check_connectivity=True,
+                 pm_model="ACPPowerModel", pm_solver="ipopt", correct_pm_network_data=True,
+                 pm_time_limits=None, pm_log_level=0, pm_file_path = None, delete_buffer_file=True,
+                 opf_flow_lim="S", pm_tol=1e-8, pdm_dev_mode=False, **kwargs):  # pragma: no cover
+    """
+        Runs a non-linear power system optimization using PowerModels.jl.
 
+        Flexibilities, constraints and cost parameters are defined in the pandapower element tables.
+
+        Flexibilities can be defined in net.sgen / net.gen /net.load
+        net.sgen.controllable if a static generator is controllable. If False,
+        the active and reactive power are assigned as in a normal power flow. If True, the following
+        flexibilities apply:
+
+        - net.sgen.min_p_mw / net.sgen.max_p_mw
+        - net.sgen.min_q_mvar / net.sgen.max_q_mvar
+        - net.load.min_p_mw / net.load.max_p_mw
+        - net.load.min_q_mvar / net.load.max_q_mvar
+        - net.gen.min_p_mw / net.gen.max_p_mw
+        - net.gen.min_q_mvar / net.gen.max_q_mvar
+        - net.ext_grid.min_p_mw / net.ext_grid.max_p_mw
+        - net.ext_grid.min_q_mvar / net.ext_grid.max_q_mvar
+        - net.dcline.min_q_to_mvar / net.dcline.max_q_to_mvar / net.dcline.min_q_from_mvar / net.dcline.max_q_from_mvar
+
+        Controllable loads behave just like controllable static generators. It must be stated if they are controllable.
+        Otherwise, they are not respected as flexibilities.
+        Dc lines are controllable per default
+
+        Network constraints can be defined for buses, lines and transformers the elements in the following columns:
+
+        - net.bus.min_vm_pu / net.bus.max_vm_pu
+        - net.line.max_loading_percent
+        - net.trafo.max_loading_percent
+        - net.trafo3w.max_loading_percent
+
+        How these costs are combined into a cost function depends on the cost_function parameter.
+
+        INPUT:
+            **net** - The pandapower format network
+
+        OPTIONAL:
+            **pp_to_pm_callback** (function, None) - callback function to add data to the PowerModels data structure
+            **pm_model** (str, "ACPPowerModel") - model to use. Default is AC model
+            **pm_solver** (str, "ipopt") - default solver to use. If ipopt is not available use Ipopt
+            **correct_pm_network_data** (bool, True) - checks if network data is correct. If not tries to correct it
+            **pm_time_limits** (Dict, None) - Time limits in seconds for power models interface. To be set as a dict like
+                                              {"pm_time_limit": 300.}
+            **pm_log_level** (int, 0) - solver log level in power models
+            **opf_flow_lim** (str, "I") - Quantity to limit for branch flow constraints, in line with matpower's
+                                                 "opf.flowlim" parameter
+                                                "S" - apparent power flow (limit in MVA),
+                                                "I" - current magnitude (limit in MVA at 1 p.u. voltage)
+            **delete_buffer_file** (Bool, True) - If True, the .json file used by powermodels will be deleted after
+                                                  optimization.
+            **pm_file_path** (str, None) - Specifiy the filename, under which the .json file for powermodels is stored. If
+                                           you want to keep the file after optimization, you should also set
+                                           delete_buffer_file to False!
+            **pdm_dev_mode** (bool, False) - If True, the develope mode of PdM is called.
+    """
+    net._options = {}
+    _add_ppc_options(net, calculate_voltage_angles=calculate_voltage_angles,
+                     trafo_model=trafo_model, check_connectivity=check_connectivity,
+                     mode="opf", switch_rx_ratio=2, init_vm_pu="flat", init_va_degree="flat",
+                     enforce_q_lims=True, recycle=dict(_is_elements=False, ppc=False, Ybus=False),
+                     voltage_depend_loads=False, delta=delta, trafo3w_losses=trafo3w_losses)    
+    _add_opf_options(net, trafo_loading='power', ac=True, init="flat", numba=True,
+                     pp_to_pm_callback=pp_to_pm_callback, julia_file="run_vd", pm_model=pm_model, pm_solver=pm_solver,
+                     correct_pm_network_data=correct_pm_network_data, pm_time_limits=pm_time_limits,
+                     pm_log_level=pm_log_level, opf_flow_lim=opf_flow_lim, pm_tol=pm_tol)
+
+    _runpm(net, pm_file_path=pm_file_path, delete_buffer_file=delete_buffer_file, pdm_dev_mode=pdm_dev_mode)
 
 
     
