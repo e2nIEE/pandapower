f53f097fee5dc609930ec42518bd6b744a775d0a
Steffen Meinecke <steffen.meinecke@uni-kassel.de>
2021-11-15 18:28:36 +0100

    remove base64 from install list

--- setup.py
+++ setup.py
@@ -54,11 +54,12 @@ setup(
         "performance": ["ortools"],
         "fileio": ["xlsxwriter", "openpyxl", "cryptography", "geopandas"],  # "fiona" is a depedency of geopandas and so already available
         "all": ["numpydoc", "sphinx", "sphinx_rtd_theme",
-                "plotly", "matplotlib", "python-igraph", "geopandas", "base64", "hashlib", "zlib",
+                "plotly", "matplotlib", "python-igraph", "geopandas", "hashlib", "zlib",
                 "pytest", "pytest-xdist",
                 "ortools",
                 "xlsxwriter", "openpyxl", "cryptography"
                 ]},  # "shapely", "pyproj", "fiona" are depedencies of geopandas and so already available
+    # base64 produces problems, so it is not included to "all"
     packages=find_packages(),
     include_package_data=True,
     classifiers=classifiers
