857a348a6e527be8b3b0f804d1dd7671af7fb0e6
Roman Bolgaryn <roman.bolgaryn@iee.fraunhofer.de>
2022-07-18 20:29:05 +0200

    trying to set up the test with Actions now - fix import error

--- pandapower/test/api/test_file_io.py
+++ pandapower/test/api/test_file_io.py
@@ -427,6 +427,24 @@ def test_json_io_with_characteristics(net_in):
 
 
 def test_postgresql(net_in):
+    connect_data = {"host": "localhost",
+                    "user": "test_user",
+                    "database": "sandbox",
+                    "password": "secret"}
+    id_columns = {"grid_id": 123, "another_id": "another_id_val"}
+    pp.to_postgresql(net_in, schema="test_schema", include_results=True, **connect_data, **id_columns)
+
+    net_out = pp.from_postgresql(schema="test_schema", include_results=True, **connect_data, **id_columns)
+
+    for element, table in net_in.items():
+        # dictionaries (e.g. std_type) not included
+        # json serialization/deserialization of objects not implemented
+        if not isinstance(table, pd.DataFrame) or table.empty or element == "line_geodata":
+            continue
+        assert pp.dataframes_equal(table, net_out[element]), element
+
+
+def test_postgresql_oberrhein():
     net_in = pp.networks.mv_oberrhein()
     net_in.switch["in_ka"] = np.nan
     connect_data = {"host": "localhost",
