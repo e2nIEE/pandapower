0f08cbc34016b556e38f10e7b26cf246a672afca
sdrauz <Simon.Ruben.Drauz@iee.fraunhofer.de>
2022-07-27 14:56:50 +0200

    include pandapower changes in replace_element_test

--- pandapower/test/api/input_files/test_control.py
+++ pandapower/test/api/input_files/test_control.py
@@ -3,7 +3,7 @@
 # Copyright (c) 2016-2022 by University of Kassel and Fraunhofer Institute for Energy Economics
 # and Energy System Technology (IEE), Kassel. All rights reserved.
 
-from pandapower.control.basic_controller import Controller
+from pandapower.control.controller.const_control import ConstControl as ConstController
 
 try:
     import pandaplan.core.pplog as logging
@@ -13,11 +13,14 @@ except ImportError:
 logger = logging.getLogger(__name__)
 
 
-class TestControl(Controller):
+class TestControl(ConstController):
+
+    def __new__(cls):
+        obj = super().__new__(cls)
+        return obj
+
+class ConstControl(ConstController):
 
     def is_converged(self, net):
         self.check_word = 'banana'
-        return self.applied
-
-    def control_step(self, net):
-        self.applied = True
+        return super().is_converged(net)
