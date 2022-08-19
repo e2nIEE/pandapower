ecbeb6162a2e1baf852bc500b63f2ed69ec14280
Sebastian Elias <77836184+elodin@users.noreply.github.com>
2022-08-19 16:02:34 +0200

    corrected search for low_impedance elements in service
    Before, only the last check of the OR statement was combined with the element beeing in_service. Now the whole OR statement is combined

--- pandapower/diagnostic.py
+++ pandapower/diagnostic.py
@@ -519,16 +519,16 @@ def impedance_values_close_to_zero(net, min_r_ohm, min_x_ohm, min_r_pu, min_x_pu
     check_results = []
     implausible_elements = {}
 
-    line = net.line[((net.line.r_ohm_per_km * net.line.length_km) <= min_r_ohm)
-                    | ((net.line.x_ohm_per_km * net.line.length_km) <= min_x_ohm) & net.line.in_service].index
+    line = net.line[(((net.line.r_ohm_per_km * net.line.length_km) <= min_r_ohm)
+                    | ((net.line.x_ohm_per_km * net.line.length_km) <= min_x_ohm)) & net.line.in_service].index
 
-    xward = net.xward[(net.xward.r_ohm <= min_r_ohm)
-                      | (net.xward.x_ohm <= min_x_ohm) & net.xward.in_service].index
+    xward = net.xward[((net.xward.r_ohm <= min_r_ohm)
+                      | (net.xward.x_ohm <= min_x_ohm)) & net.xward.in_service].index
 
-    impedance = net.impedance[(net.impedance.rft_pu <= min_r_pu)
+    impedance = net.impedance[((net.impedance.rft_pu <= min_r_pu)
                               | (net.impedance.xft_pu <= min_x_pu)
                               | (net.impedance.rtf_pu <= min_r_pu)
-                              | (net.impedance.xtf_pu <= min_x_pu) & net.impedance.in_service].index
+                              | (net.impedance.xtf_pu <= min_x_pu)) & net.impedance.in_service].index
     if len(line) > 0:
         implausible_elements['line'] = list(line)
     if len(xward) > 0:
