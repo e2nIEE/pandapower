f21ca49c1c996f021d26f2d1a4ea161ab7a0df66
sdrauz <Simon.Ruben.Drauz@iee.fraunhofer.de>
2022-10-27 17:53:30 +0200

    further bugfix

--- pandapower/io_utils.py
+++ pandapower/io_utils.py
@@ -492,8 +492,7 @@ class FromSerializableRegistry():
             else:
                 net = self.empty_dict_like_object
             net.update(self.obj)
-            if 'format_version' not in self.obj:
-                del net.format_version
+            if 'format_version' not in self.obj and 'format_version' in net: del net.format_version
             return net
 
     @from_serializable.register(class_name="MultiGraph", module_name="networkx")
