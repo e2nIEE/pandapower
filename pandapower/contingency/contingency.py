4bdda9d78437e7ecdf138fe3ddbb3ec373277b63
Roman Bolgaryn <roman.bolgaryn@iee.fraunhofer.de>
2022-11-16 16:28:15 +0100

    clarified text in tutorial

--- pandapower/contingency/contingency.py
+++ pandapower/contingency/contingency.py
@@ -44,6 +44,7 @@ def run_contingency(net, nminus1_cases, pf_options=None, pf_options_nminus1=None
     """
     # set up the dict for results and relevant variables
     # ".get" in case the options have been set in pp.set_user_pf_options:
+    if "recycle" in kwargs: kwargs["recycle"] = False  # so that we can be sure it doesn't happen
     if pf_options is None: pf_options = net.user_pf_options.get("pf_options", net.user_pf_options)
     if pf_options_nminus1 is None: pf_options_nminus1 = net.user_pf_options.get("pf_options_nminus1",
                                                                                 net.user_pf_options)
