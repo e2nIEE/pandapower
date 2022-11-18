efea5e3a44f2937e1ab766127aa614ef4d1f1156
Jolando Kisse <jolando.kisse@uni-kassel.de>
2022-11-18 13:56:18 +0100

    change treatment of x/y in draw_traces

--- pandapower/plotting/plotly/traces.py
+++ pandapower/plotting/plotly/traces.py
@@ -785,150 +785,6 @@ def create_trafo_trace(net, trafos=None, color='green', trafotype='2W', width=5,
     return trafo_traces
 
 
-def draw_traces(traces, on_map=False, map_style='basic', showlegend=True, figsize=1,
-                aspectratio='auto', filename='temp-plot.html', auto_open=True, **kwargs):
-    """
-    plots all the traces (which can be created using :func:`create_bus_trace`, :func:`create_line_trace`,
-    :func:`create_trafo_trace`)
-    to PLOTLY (see https://plot.ly/python/)
-
-    INPUT:
-        **traces** - list of dicts which correspond to plotly traces
-        generated using: `create_bus_trace`, `create_line_trace`, `create_trafo_trace`
-
-    OPTIONAL:
-        **on_map** (bool, False) - enables using mapbox plot in plotly
-
-        **map_style** (str, 'basic') - enables using mapbox plot in plotly
-
-            - 'streets'
-            - 'bright'
-            - 'light'
-            - 'dark'
-            - 'satellite'
-
-        **showlegend** (bool, 'True') - enables legend display
-
-        **figsize** (float, 1) - aspectratio is multiplied by it in order to get final image size
-
-        **aspectratio** (tuple, 'auto') - when 'auto' it preserves original aspect ratio of the
-        network geodata any custom aspectration can be given as a tuple, e.g. (1.2, 1)
-
-        **filename** (str, "temp-plot.html") - plots to a html file called filename
-
-        **auto_open** (bool, 'True') - automatically open plot in browser
-
-    OUTPUT:
-        **figure** (graph_objs._figure.Figure) figure object
-
-    """
-    if on_map:
-        try:
-            on_map = _on_map_test(traces[0]['x'][0], traces[0]['y'][0])
-        except:
-            logger.warning("Test if geo-data are in lat/long cannot be performed using geopy -> "
-                           "eventual plot errors are possible.")
-
-        if on_map is False:
-            logger.warning("Existing geodata are not real lat/lon geographical coordinates. -> "
-                           "plot on maps is not possible.\n"
-                           "Use geo_data_to_latlong(net, projection) to transform geodata from specific projection.")
-
-    if on_map:
-        # change traces for mapbox
-        # change trace_type to scattermapbox and rename x to lat and y to lon
-        for trace in traces:
-            trace['lat'] = trace.pop('y')
-            trace['lon'] = trace.pop('x')
-            trace['type'] = 'scattermapbox'
-            if "line" in trace and isinstance(trace["line"], Line):
-                # scattermapboxplot lines do not support dash for some reason, make it a red line instead
-                if "dash" in trace["line"]._props:
-                    _prps = dict(trace["line"]._props)
-                    _prps.pop("dash", None)
-                    _prps["color"] = "red"
-                    trace["line"] = scmLine(_prps)
-                else:
-                    trace["line"] = scmLine(dict(trace["line"]._props))
-            elif "marker" in trace and isinstance(trace["marker"], Marker):
-                trace["marker"] = scmMarker(trace["marker"]._props)
-
-    # setting Figure object
-    fig = Figure(data=traces,  # edge_trace
-                 layout=Layout(
-                     titlefont=dict(size=16),
-                     showlegend=showlegend,
-                     autosize=(aspectratio == 'auto'),
-                     hovermode='closest',
-                     margin=dict(b=5, l=5, r=5, t=5),
-                     # annotations=[dict(
-                     #     text="",
-                     #     showarrow=False,
-                     #     xref="paper", yref="paper",
-                     #     x=0.005, y=-0.002)],
-                     xaxis=XAxis(showgrid=False, zeroline=False, showticklabels=False),
-                     yaxis=YAxis(showgrid=False, zeroline=False, showticklabels=False),
-                     # legend=dict(x=0, y=1.0)
-                     ),
-                )
-    a = kwargs.get('annotation')
-    if a:
-        fig.add_annotation(a)
-
-    # check if geodata are real geographical lat/lon coordinates using geopy
-    if on_map:
-        try:
-            mapbox_access_token = _get_mapbox_token()
-        except Exception:
-            logger.exception('mapbox token required for map plots. '
-                             'Get Mapbox token by signing in to https://www.mapbox.com/.\n'
-                             'After getting a token, set it to pandapower using:\n'
-                             'pandapower.plotting.plotly.mapbox_plot.set_mapbox_token(\'<token>\')')
-            raise MapboxTokenMissing
-
-        fig['layout']['mapbox'] = dict(accesstoken=mapbox_access_token,
-                                       bearing=0,
-                                       center=dict(lat=pd.Series(traces[0]['lat']).dropna().mean(),
-                                                   lon=pd.Series(traces[0]['lon']).dropna().mean()),
-                                       style=map_style,
-                                       pitch=0,
-                                       zoom=11)
-
-    # default aspectratio: if on_map use auto, else use 'original'
-    aspectratio = 'original' if not on_map and aspectratio == 'auto' else aspectratio
-
-    if aspectratio != 'auto':
-        if aspectratio == 'original':
-            # TODO improve this workaround for getting original aspectratio
-            xs = []
-            ys = []
-            for trace in traces:
-                xs += trace['x']
-                ys += trace['y']
-            x_dropna = pd.Series(xs).dropna()
-            y_dropna = pd.Series(ys).dropna()
-            xrange = x_dropna.max() - x_dropna.min()
-            yrange = y_dropna.max() - y_dropna.min()
-            ratio = xrange / yrange
-            if ratio < 1:
-                aspectratio = (ratio, 1.)
-            else:
-                aspectratio = (1., 1 / ratio)
-
-        aspectratio = np.array(aspectratio) / max(aspectratio)
-        fig['layout']['width'], fig['layout']['height'] = ([ar * figsize * 700 for ar in aspectratio])
-
-    # check if called from ipynb or not in order to consider appropriate plot function
-    if _in_ipynb():
-        from plotly.offline import init_notebook_mode, iplot as plot
-        init_notebook_mode()
-        plot(fig, filename=filename)
-    else:
-        from plotly.offline import plot as plot
-        plot(fig, filename=filename, auto_open=auto_open)
-
-    return fig
-
 def create_weighted_marker_trace(net, elm_type="load", elm_ids=None, column_to_plot="p_mw",
                                  sizemode="area", color="red", patch_type="circle",
                                  marker_scaling=1., trace_name="", infofunc=None,
@@ -1027,3 +883,150 @@ def create_weighted_marker_trace(net, elm_type="load", elm_ids=None, column_to_p
                                   symbol=patch_type, sizemode=sizemode)
 
     return marker_trace
+
+
+def draw_traces(traces, on_map=False, map_style='basic', showlegend=True, figsize=1,
+                aspectratio='auto', filename='temp-plot.html', auto_open=True, **kwargs):
+    """
+    plots all the traces (which can be created using :func:`create_bus_trace`, :func:`create_line_trace`,
+    :func:`create_trafo_trace`)
+    to PLOTLY (see https://plot.ly/python/)
+
+    INPUT:
+        **traces** - list of dicts which correspond to plotly traces
+        generated using: `create_bus_trace`, `create_line_trace`, `create_trafo_trace`
+
+    OPTIONAL:
+        **on_map** (bool, False) - enables using mapbox plot in plotly
+
+        **map_style** (str, 'basic') - enables using mapbox plot in plotly
+
+            - 'streets'
+            - 'bright'
+            - 'light'
+            - 'dark'
+            - 'satellite'
+
+        **showlegend** (bool, 'True') - enables legend display
+
+        **figsize** (float, 1) - aspectratio is multiplied by it in order to get final image size
+
+        **aspectratio** (tuple, 'auto') - when 'auto' it preserves original aspect ratio of the
+        network geodata any custom aspectration can be given as a tuple, e.g. (1.2, 1)
+
+        **filename** (str, "temp-plot.html") - plots to a html file called filename
+
+        **auto_open** (bool, 'True') - automatically open plot in browser
+
+    OUTPUT:
+        **figure** (graph_objs._figure.Figure) figure object
+
+    """
+    if on_map:
+        try:
+            on_map = _on_map_test(traces[0]['x'][0], traces[0]['y'][0])
+        except:
+            logger.warning("Test if geo-data are in lat/long cannot be performed using geopy -> "
+                           "eventual plot errors are possible.")
+
+        if on_map is False:
+            logger.warning("Existing geodata are not real lat/lon geographical coordinates. -> "
+                           "plot on maps is not possible.\n"
+                           "Use geo_data_to_latlong(net, projection) to transform geodata from specific projection.")
+
+    if on_map:
+        # change traces for mapbox
+        # change trace_type to scattermapbox and rename x to lat and y to lon
+        for trace in traces:
+            if 'x' in trace.keys():
+                trace['lon'] = trace.pop('x')
+            if 'y' in trace.keys():
+                trace['lat'] = trace.pop('y')
+            trace['type'] = 'scattermapbox'
+            if "line" in trace and isinstance(trace["line"], Line):
+                # scattermapboxplot lines do not support dash for some reason, make it a red line instead
+                if "dash" in trace["line"]._props:
+                    _prps = dict(trace["line"]._props)
+                    _prps.pop("dash", None)
+                    _prps["color"] = "red"
+                    trace["line"] = scmLine(_prps)
+                else:
+                    trace["line"] = scmLine(dict(trace["line"]._props))
+            elif "marker" in trace and isinstance(trace["marker"], Marker):
+                trace["marker"] = scmMarker(trace["marker"]._props)
+
+    # setting Figure object
+    fig = Figure(data=traces,  # edge_trace
+                 layout=Layout(
+                     titlefont=dict(size=16),
+                     showlegend=showlegend,
+                     autosize=(aspectratio == 'auto'),
+                     hovermode='closest',
+                     margin=dict(b=5, l=5, r=5, t=5),
+                     # annotations=[dict(
+                     #     text="",
+                     #     showarrow=False,
+                     #     xref="paper", yref="paper",
+                     #     x=0.005, y=-0.002)],
+                     xaxis=XAxis(showgrid=False, zeroline=False, showticklabels=False),
+                     yaxis=YAxis(showgrid=False, zeroline=False, showticklabels=False),
+                     # legend=dict(x=0, y=1.0)
+                     ),
+                )
+    a = kwargs.get('annotation')
+    if a:
+        fig.add_annotation(a)
+
+    # check if geodata are real geographical lat/lon coordinates using geopy
+    if on_map:
+        try:
+            mapbox_access_token = _get_mapbox_token()
+        except Exception:
+            logger.exception('mapbox token required for map plots. '
+                             'Get Mapbox token by signing in to https://www.mapbox.com/.\n'
+                             'After getting a token, set it to pandapower using:\n'
+                             'pandapower.plotting.plotly.mapbox_plot.set_mapbox_token(\'<token>\')')
+            raise MapboxTokenMissing
+
+        fig['layout']['mapbox'] = dict(accesstoken=mapbox_access_token,
+                                       bearing=0,
+                                       center=dict(lat=pd.Series(traces[0]['lat']).dropna().mean(),
+                                                   lon=pd.Series(traces[0]['lon']).dropna().mean()),
+                                       style=map_style,
+                                       pitch=0,
+                                       zoom=kwargs.pop('zoomlevel', 11))
+
+    # default aspectratio: if on_map use auto, else use 'original'
+    aspectratio = 'original' if not on_map and aspectratio == 'auto' else aspectratio
+
+    if aspectratio != 'auto':
+        if aspectratio == 'original':
+            # TODO improve this workaround for getting original aspectratio
+            xs = []
+            ys = []
+            for trace in traces:
+                xs += trace.get('x') or trace['lon']
+                ys += trace.get('y') or trace['lat']
+            x_dropna = pd.Series(xs).dropna()
+            y_dropna = pd.Series(ys).dropna()
+            xrange = x_dropna.max() - x_dropna.min()
+            yrange = y_dropna.max() - y_dropna.min()
+            ratio = xrange / yrange
+            if ratio < 1:
+                aspectratio = (ratio, 1.)
+            else:
+                aspectratio = (1., 1 / ratio)
+
+        aspectratio = np.array(aspectratio) / max(aspectratio)
+        fig['layout']['width'], fig['layout']['height'] = ([ar * figsize * 700 for ar in aspectratio])
+
+    # check if called from ipynb or not in order to consider appropriate plot function
+    if _in_ipynb():
+        from plotly.offline import init_notebook_mode, iplot as plot
+        init_notebook_mode()
+        plot(fig, filename=filename)
+    else:
+        from plotly.offline import plot as plot
+        plot(fig, filename=filename, auto_open=auto_open)
+
+    return fig
