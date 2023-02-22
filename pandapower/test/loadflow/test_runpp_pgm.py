b79ea88804f1073bcba146d54cc7489cbd9ae982
Nitish Bharambe <nitish.bharambe@alliander.com>
2023-02-22 11:19:27 +0100

    test lookup
    Signed-off-by: Nitish Bharambe <nitish.bharambe@alliander.com>

--- pandapower/test/loadflow/test_runpp_pgm.py
+++ pandapower/test/loadflow/test_runpp_pgm.py
@@ -3,12 +3,11 @@ from unittest.mock import patch
 import pytest
 
 import pandapower as pp
-# import power_grid_model_io.converters
 from pandapower.test.consistency_checks import runpp_pgm_with_consistency_checks
 
 
 
-def test_minimal_net_pgm(**kwargs):
+def test_minimal_net_pgm():
     # tests corner-case when the grid only has 1 bus and an ext-grid
     net = pp.create_empty_network()
     b = pp.create_bus(net, 110)
@@ -19,7 +18,7 @@ def test_minimal_net_pgm(**kwargs):
     runpp_pgm_with_consistency_checks(net)
 
     b2 = pp.create_bus(net, 110)
-    # pp.create_switch(net, b, b2, 'b')
+    pp.create_switch(net, b, b2, 'b')
     pp.create_sgen(net, b2, p_mw=0.2)
     runpp_pgm_with_consistency_checks(net)
 
@@ -27,11 +26,6 @@ def test_runpp_pgm__asym():
     net = pp.create_empty_network()
     with pytest.raises(NotImplementedError, match="Asymmetric  power flow by power-grid-model is not implemented yet"):
         pp.runpp_pgm(net, symmetric=False)
-
-def test_runpp_pgm__import_fail():
-    net = pp.create_empty_network()
-    # with patch.dict(power_grid_model_io.converters, {"PandaPowerConverter": None}):
-    pp.runpp_pgm(net)
 
 def test_runpp_pgm__non_convergence():
     net = pp.create_empty_network()
@@ -47,7 +41,9 @@ def test_runpp_pgm__validation_fail(mock_errors_to_string):
     net = pp.create_empty_network()
     pp.create_bus(net, -110, index=123)
     pp.runpp_pgm(net, validate_input=True)
-    mock_errors_to_string.assert_called_once_with()
+    expected_lookup = {0: 'Table: bus Index: 123'}
+    mock_errors_to_string.assert_called_once()
+    assert mock_errors_to_string.call_args.kwargs["id_lookup"] == expected_lookup
 
 if __name__ == "__main__":
     pytest.main([__file__])
