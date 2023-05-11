672843162fbd06d90252d40ff56d99ff44587b32
David Heck <david.heck@iee.fraunhofer.de>
2023-05-11 12:46:12 +0200

    fix relative path for cim2pp test

--- pandapower/test/converter/test_from_cim.py
+++ pandapower/test/converter/test_from_cim.py
@@ -7,7 +7,7 @@ from pandapower.converter import from_cim as cim2pp
 
 
 def test_cim2pp():
-    folder_path = os.path.abspath(os.path.join(os.getcwd(), "../../../tutorials/example_cim"))
+    folder_path = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), "../../../tutorials/example_cim"))
 
     cgmes_files = [os.path.join(folder_path, 'CGMES_v2.4.15_SmallGridTestConfiguration_Boundary_v3.0.0.zip'),
                    os.path.join(folder_path, 'CGMES_v2.4.15_SmallGridTestConfiguration_BaseCase_Complete_v3.0.0.zip')]
