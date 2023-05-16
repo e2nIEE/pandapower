99c7a1d6aa0a6596422be6014637ef370c3d5ff9
Baraa Alfakhouri <baraa.alfakhouri@uni-kassel.de>
2023-05-16 19:07:27 +0200

    WIP: formulate function for ssc and the results

--- pandapower/test/loadflow/test_facts.py
+++ pandapower/test/loadflow/test_facts.py
@@ -616,9 +616,7 @@ def test_ssc_simple():
     pp.create_ext_grid(net, 0)
     pp.create_line_from_parameters(net, 0, 1, 30, 0.0487, 0.13823, 160, 0.664)
     pp.create_load(net, 1, 100, 25)
-    dtypes = net.ssc.dtypes
-    net.ssc.loc[0, :] = "None", 1, 1, 2, 0.99, 0, 1, True, True
-    _preserve_dtypes(net.ssc, dtypes)
+    pp.create_ssc(net, 1, 1, 2, 0.99)
 
     pp.runpp(net, max_iteration=100)
 
