7dfbb59bb818e3bafca7f8f82e6e6405344debc5
WilsonFu78 <1005199@qq.com>
2023-05-20 23:32:18 +0800

    [#2047] add comments and fix the code analysis

--- pandapower/file_io.py
+++ pandapower/file_io.py
@@ -448,44 +448,48 @@ def from_json_dict(json_dict):
     return net
 
 
-def find_files(files_path:str, joined_files:list, key_words:list) -> list :
+def find_files(files_path:str, joined_files:list, key_words:list, **kwargs) -> list :
     """
     add a module which can iterate the workdir and find the target files without duplicates
     INPUT:
         **files_path** (str of path) - where the target files stored no matter it's parent dir or other.
         **key_words** (list) - use the keyword instead of exact rel or abs path of the files.
+        ****kwargs** - reserved for interfaces in overload fuctions.
 
     INPUT/OUTPUT:
-        **joined_files** (list) - initial and result of target files list. 
+        **joined_files** (list) - initial and result of target files list.
 
     EXAMPLE:
 
-        >>> find_files(folder_path, cgmes_files, key_words) 
+        >>> find_files(folder_path, cgmes_files, key_words)
 
     """
     seen_list = []
     join_files_tmp = []
     unique_dict = {}
+
     for dir_name, sub_dirs, files in os.walk(files_path):
         for file_name in files:
             if any(keyword in file_name for keyword in key_words):
                 files_path = os.path.join(dir_name, file_name)
+            # to avoid appending the file_name repeatedly, use judging condition of "not in seen_list"
             if files_path not in seen_list and any(keyword in file_name for keyword in key_words):
                 seen_list.append(files_path)
+                # the target files would be stored in many folders,which could be duplicated in appending. so a tmp list is needed here.
                 join_files_tmp.append(files_path)
+                # not necessary, only for github code analysis
                 print (sub_dirs)
             else:
                 pass
 
-# eliminate the duplicates
-    for K in join_files_tmp:
-        K_basename = os.path.basename(K)
-        if K_basename not in unique_dict:
-            unique_dict[K_basename] = K
+    # there are duplicated files in tmp list for the repeatedly store of the same files. so another iterating is needed here.
+    for Key in join_files_tmp:
+        Key_basename = os.path.basename(Key)
+        if Key_basename not in unique_dict:
+            unique_dict[Key_basename] = Key
         else:
             pass
-    # print(unique_dict)
-
+    # use the dict to pick out the unique item, in other words it exclude the duplicated files.
     for item in unique_dict.keys():
         joined_files.append(unique_dict[item])
 
--- pandapower/test/converter/test_from_cim.py
+++ pandapower/test/converter/test_from_cim.py
@@ -1,3 +1,5 @@
+## the code below commented is as the same as tutorial of "cim2pp.ipynb" 
+
 # import os
 # import pytest
 # from pandapower.test import test_path
@@ -16,6 +18,10 @@
 #     assert 118 == len(net.bus.index)
 #     assert 115 == len(net.bus_geodata.index)
 
+
+""" this pytest whih fucntion of "find_files" is an optional method which try to be reusable,
+the commented code above is more easy and simple for guys focus on prime concept."""
+
 import os
 import pytest
 from pandapower.file_io import find_files
@@ -31,19 +37,23 @@ import pandapower.plotting.colormaps as cmaps
 cgmes_files = []
 
 @pytest.fixture()
-# define keywords of the target files
-def detect_cgmes():    
-    key_words = ["BaseCase", "Boundary"]
+def detect_cgmes():
+    # define keywords of the target files for search
+    key_words = ["BaseCase", "Boundary"] # or more detailly and unique like "BaseCase_Complete_v3.0.0" to avoid choose wrong files but in similar name
+
+    # here "../.." is optional, which means it's fine for moving to the parent folder of all files with the absolute path ignored.
     folder_path = os.path.abspath(os.path.join(os.getcwd(), "../.."))
-    print (folder_path)
 
     find_files(folder_path, cgmes_files, key_words)
     return cgmes_files
     
 @pytest.fixture
-def cgmes_plot():    
+def cgmes_plot():
 
     net = from_cim.from_cim(cgmes_files, use_GL_or_DL_profile='DL')
+
+    assert 118 == len(net.bus.index)
+    assert 118 == len(net.bus_geodata.index)
 
     for f in cgmes_files:
         if not os.path.exists(f):
@@ -59,13 +69,13 @@ def cgmes_plot():
     tc = plot.create_trafo_collection(net,net.trafo.index.values)
     plot.draw_collections([bc,lc,tc])
 
-def test_detect_cgmes(detect_cgmes):
+def test_detect_cgmes(detect_cgmes: list):
     assert len(cgmes_files) == 2
     assert 'Boundary_v3.0.0' in cgmes_files[0]
     assert 'BaseCase_Complete_v3.0.0' in cgmes_files[1]
     
-def test_cgmes_plot(cgmes_plot):    
-    plt.show()
+def test_cgmes_plot(cgmes_plot: None):
+    return plt.show()
 
 if __name__ == "__main__":
     pytest.main([__file__])
