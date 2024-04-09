bef1f0db9373b29d5e4217d2d1b185d8f8e01bc7
hkoertge <hjalmar.koertge@iee.fraunhofer.de>
2024-04-09 11:57:49 +0200

    fixed package build

--- setup.py
+++ setup.py
@@ -1,3 +1,8 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2016-2024 by University of Kassel and Fraunhofer Institute for Energy Economics
+# and Energy System Technology (IEE), Kassel. All rights reserved.
+
 from setuptools import setup
 
 setup()
