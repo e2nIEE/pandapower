87aef9e46c8fa8600b0ede3888999cf4221d9511
quant12345 <kamil246@mail.ru>
2024-08-09 15:52:16 +0500

    try with float64 type.

--- pandapower/toolbox/grid_modification.py
+++ pandapower/toolbox/grid_modification.py
@@ -1230,7 +1230,7 @@ def replace_ext_grid_by_gen(net, ext_grids=None, gen_indices=None, slack=False, 
                                          (net[table].element.isin(ext_grids))]
             if len(to_change):
                 net[table].loc[to_change, "et"] = "gen"
-                #net[table]["element"] = net[table]["element"].astype("int64")
+                net[table]["element"] = net[table]["element"].astype("float64")
                 net[table].loc[to_change, "element"] = new_idx
 
     # --- result data
@@ -1397,7 +1397,7 @@ def replace_gen_by_sgen(net, gens=None, sgen_indices=None, cols_to_keep=None,
             to_change = net[table].index[(net[table].et == "gen") & (net[table].element.isin(gens))]
             if len(to_change):
                 net[table].loc[to_change, "et"] = "sgen"
-                #net[table]["element"] = net[table]["element"].astype("int64")
+                net[table]["element"] = net[table]["element"].astype("float64")
                 net[table].loc[to_change, "element"] = new_idx
 
     # --- result data
@@ -1619,7 +1619,7 @@ def replace_pq_elmtype(net, old_element_type, new_element_type, old_indices=None
                                          (net[table].element.isin(old_indices))]
             if len(to_change):
                 net[table].loc[to_change, "et"] = new_element_type
-                #net[table]["element"] = net[table]["element"].astype("int64")
+                net[table]["element"] = net[table]["element"].astype("float64")
                 net[table].loc[to_change, "element"] = new_idx
 
     # --- result data
