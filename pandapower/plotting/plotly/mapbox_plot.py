6808dc81ba9d47d46afad798bda9389f6b83516e
hkoertge <hjalmar.koertge@iee.fraunhofer.de>
2024-11-29 15:19:28 +0100

    deprecated geo_data_to_latlong as it basically is a wrapper, removed commented out code

--- pandapower/plotting/plotly/mapbox_plot.py
+++ pandapower/plotting/plotly/mapbox_plot.py
@@ -5,6 +5,9 @@
 
 
 import os
+
+from typing_extensions import deprecated
+
 from pandapower.plotting import geo
 
 try:
@@ -41,6 +44,7 @@ def _on_map_test(x, y):
     return True
 
 
+@deprecated('geo_data_to_latlong is deprecated and will be removed shortly, use pandapower.geo.convert_crs instead')
 def geo_data_to_latlong(net, projection):
     """
     Transforms network's geodata (in `net.bus_geodata` and `net.line_geodata`) from specified projection to lat/long (WGS84).
@@ -55,40 +59,6 @@ def geo_data_to_latlong(net, projection):
                 - "epsg:2190" - Azores Oriental 1940 / UTM zone 26N
     """
     geo.convert_crs(net, epsg_in=projection.split(':')[1], epsg_out=4326)
-
-    # try:
-    #     from pyproj import Proj, transform
-    # except ImportError:
-    #     raise ImportError('Geo-coordinates check cannot be peformed because pyproj package not available \n\t--> '
-    #                    'if geo-coordinates are not in lat/lon format an empty plot may appear...')
-    #
-    # if projection == 'epsg:4326':
-    #     return
-    #
-    # wgs84 = Proj(init='epsg:4326')  # lat/long
-    #
-    # try:
-    #     projection = Proj(init=projection)
-    # except:
-    #     logger.warning("Transformation of geodata to lat/long failed! because of:]\n"
-    #                    "Unknown projection provided "
-    #                      "(format 'epsg:<number>' required as available at http://spatialreference.org/ref/epsg/ )")
-    #     return
-    #
-    # # transform all geodata to long/lat using set or found projection
-    # try:
-    #     lon, lat = transform(projection, wgs84, net.bus_geodata.loc[:, 'x'].values, net.bus_geodata.loc[:, 'y'].values)
-    #     net.bus_geodata.loc[:, 'x'], net.bus_geodata.loc[:, 'y'] =  lon, lat
-    #
-    #     if net.line_geodata.shape[0] > 0:
-    #         for idx in net.line_geodata.index:
-    #             line_coo = np.array(net.line_geodata.loc[idx, 'coords'])
-    #             lon, lat = transform(projection, wgs84, line_coo[:, 0], line_coo[:, 1])
-    #             net.line_geodata.loc[idx, 'coords'] = np.array([lon, lat]).T.tolist()
-    #     return
-    # except:
-    #     logger.warning('Transformation of geodata to lat/long failed!')
-    #     return
 
 
 def set_mapbox_token(token):
