613c35fbbc1cfc2eb0f2c00fedda02cb14fac952
Mike Vogt <mike.vogt@iee.fraunhofer.de>
2024-12-10 09:22:11 +0100

    updated matpower converter to be able to create version dependent mpc matrices

--- pandapower/converter/matpower/to_mpc.py
+++ pandapower/converter/matpower/to_mpc.py
@@ -5,6 +5,7 @@
 
 
 import copy
+from typing import Optional
 
 import numpy as np
 from scipy.io import savemat
@@ -19,7 +20,7 @@ except ImportError:
 logger = logging.getLogger(__name__)
 
 
-def to_mpc(net, filename=None, **kwargs):
+def to_mpc(net, filename=None, version:Optional[int]=None, **kwargs):
     """
     This function converts a pandapower net to a matpower case files (.mat) version 2.
     Note: python is 0-based while Matlab is 1-based.
@@ -30,6 +31,9 @@ def to_mpc(net, filename=None, **kwargs):
     OPTIONAL:
         **filename** (str, None) - File path + name of the mat file which will be created. If None
             the mpc will only be returned
+
+        **version** (int, None) - The version of the mat file which will be created. If None
+            the ppc will be returned, possibly containing more columns.
 
         ****kwargs** - please look at to_ppc() documentation
 
@@ -44,6 +48,22 @@ def to_mpc(net, filename=None, **kwargs):
 
     mpc = dict()
     mpc["mpc"] = _ppc2mpc(ppc)
+
+    # if no version is set, we will write version 8 since it is the most current one.
+    if version is None:
+        version = 8
+
+    # version is a string
+    mpc["version"] = str(version)
+
+    # if version is below 4 we have to remove some columns which were not in the old formats
+    if version < 4:
+        mpc["bus"] = mpc["bus"][:12]
+        mpc["gen"] = mpc["gen"][:10]
+        mpc["branch"] = mpc["branch"][:11]
+        mpc["area"] = mpc["area"][:2]
+        mpc["cost"] = mpc["cost"][:5]
+
     if filename is not None:
         # savemat
         savemat(filename, mpc)
@@ -57,8 +77,7 @@ def _ppc2mpc(ppc):
     Convert 0-based python to 1-based Matlab
 
     **INPUT**:
-        * net - The pandapower format network
-        * filename - File path + name of the mat file which is created
+        * ppc - internal pandapower structure which will be converted
     """
 
     # convert to matpower
@@ -70,8 +89,6 @@ def _ppc2mpc(ppc):
         mpc["branch"][:, 0:2] = mpc["branch"][:, 0:2] + 1
     # adjust for the matpower converter -> taps should be 0 when there is no transformer, but are 1
     mpc["branch"][np.where(mpc["branch"][:, 8] == 1), 8] = 0
-    # version is a string
-    mpc["version"] = str(mpc["version"])
     # baseMVA has to be a float instead of int
     mpc["baseMVA"] = mpc["baseMVA"] * 1.0
     return mpc
