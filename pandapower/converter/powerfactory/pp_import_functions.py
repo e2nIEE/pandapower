c1a0aafa6345648a23517c59c3b0d22a2572edaa
Dominik Hilbrich <dominik.hilbrich@iee.fraunhofer.de>
2024-12-20 11:34:28 +0100

    test pf_converter for adjusted trafo tap changer working and small bug_fix in converter

--- pandapower/converter/powerfactory/pp_import_functions.py
+++ pandapower/converter/powerfactory/pp_import_functions.py
@@ -2293,8 +2293,8 @@ def create_trafo_type(net, item):
         "tap_side": ['hv', 'lv', 'ext'][item.tap_side],  # 'ext' not implemented
     }
 
-    type_data.update({"tap_changer_type": None})
-    type_data.update({"tap2_changer_type": None})
+    type_data.update({"tap_changer_type": "None"})
+    type_data.update({"tap2_changer_type": "None"})
 
     if item.itapch:
         logger.debug('trafo <%s> has tap changer' % name)
@@ -2451,7 +2451,7 @@ def create_trafo(net, item, export_controller=True, tap_opt="nntap", is_unbalanc
             elif pf_type.tapchtype == 2:
                 tap_changer_type = "Ideal"
             else:
-                tap_changer_type = None
+                tap_changer_type = "None"
 
             tap_dependency_table = True
             id_characteristic_table = new_id_characteristic_table
@@ -2532,7 +2532,7 @@ def create_trafo(net, item, export_controller=True, tap_opt="nntap", is_unbalanc
             elif pf_type.tapchtype == 2:
                 tap_changer_type = "Ideal"
             else:
-                tap_changer_type = None
+                tap_changer_type = "None"
 
             tap_dependency_table = True
             id_characteristic_table = new_id_characteristic_table
@@ -2831,7 +2831,7 @@ def create_trafo3w(net, item, tap_opt='nntap'):
             tap_step_degree = item.GetAttribute('t:ph3tr_' + ts)
 
             if (tap_step_degree is None or tap_step_degree == 0) and (tap_step_percent is None or tap_step_percent == 0):
-                tap_changer_type = None
+                tap_changer_type = "None"
             # ratio/asymmetrical phase shifters
             elif (tap_step_degree != 90 and tap_step_percent is not None and tap_step_percent != 0):
                 tap_changer_type = "Ratio"
--- pandapower/test/converter/test_from_powerfactory.py
+++ pandapower/test/converter/test_from_powerfactory.py
@@ -18,6 +18,7 @@ logger = logging.getLogger(__name__)
 
 try:
     import powerfactory as pf
+
     PF_INSTALLED = True
 except ImportError:
     PF_INSTALLED = False
@@ -65,7 +66,12 @@ def test_pf_export():
     tol = get_tol()
 
     for key, diff in all_diffs.items():
-        delta = diff.abs().max()
+        if isinstance(diff, pd.DataFrame):
+            delta = diff["diff"].abs().max()
+        elif isinstance(diff, pd.Series):
+            delta = diff.abs().max()
+        else:
+            raise (UserWarning, "Diff variable has wrong type!")
         assert delta < tol[key], "%s has too high difference: %f > %f" % (key, delta, tol[key])
 
 
@@ -87,8 +93,14 @@ def test_pf_export_trafo3w():
     # doesn't pass yet due to trafo3w implementation
     # trafo3w implementation is not very accurate
     for key, diff in all_diffs.items():
-        delta = diff.abs().max()
+        if isinstance(diff, pd.DataFrame):
+            delta = diff["diff"].abs().max()
+        elif isinstance(diff, pd.Series):
+            delta = diff.abs().max()
+        else:
+            raise (UserWarning, "Diff variable has wrong type!")
         assert delta < tol[key], "%s has too high difference: %f > %f" % (key, delta, tol[key])
+
 
 def test_trafo_tap2_results():
     path = os.path.join(pp.pp_dir, 'test', 'converter', 'testfiles', 'trafo_tap_model.json')
@@ -101,6 +113,26 @@ def test_trafo_tap2_results():
         else:
             delta = diff['diff'].abs().max()
         assert delta < tol, "%s has too high difference: %f > %f" % (key, delta, tol)
+
+@pytest.mark.skipif(not PF_INSTALLED, reason='powerfactory must be installed')
+def test_pf_export_tap_changer():
+    app = pf.GetApplication()
+    # import the tap changer test grid to powerfactory
+    path = os.path.join(pp.pp_dir, 'test', 'converter', 'testfiles', 'test_tap_changer.pfd')
+    prj = import_project(path, app, 'TEST_PF_CONVERTER', import_folder='TEST_IMPORT', clear_import_folder=True)
+    prj_name = prj.GetFullName()
+
+    net = from_pfd(app, prj_name=prj_name)
+
+    all_diffs = validate_pf_conversion(net, tolerance_mva=1e-9)
+    tol = get_tol()
+
+    for key, diff in all_diffs.items():
+        if type(diff) == pd.Series:
+            delta = diff.abs().max()
+        else:
+            delta = diff['diff'].abs().max()
+        assert delta < tol[key], "%s has too high difference: %f > %f" % (key, delta, tol[key])
 
 if __name__ == '__main__':
     pytest.main([__file__, "-xs"])
