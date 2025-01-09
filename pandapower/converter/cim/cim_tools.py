280aaf51960b19c64e9ca4da6485c554ed08f155
Panos Xenos <xenos.panos@gmail.com>
2025-01-09 13:22:21 +0200

    add bus key in sc dict for extracting measurements of additional elements

--- pandapower/converter/cim/cim_tools.py
+++ pandapower/converter/cim/cim_tools.py
@@ -25,7 +25,7 @@ def get_pp_net_special_columns_dict() -> Dict[str, str]:
                  'pte_id': 'PowerTransformerEnd_id', 'pte_id_hv': 'PowerTransformerEnd_id_hv',
                  'pte_id_mv': 'PowerTransformerEnd_id_mv', 'pte_id_lv': 'PowerTransformerEnd_id_lv',
                  'cnc_id': 'ConnectivityNodeContainer_id', 'sub_id': 'Substation_id', 'src': 'source', 'name': 'name',
-                 'desc': 'description', 'a_id': 'analog_id'})
+                 'desc': 'description', 'a_id': 'analog_id', 'bus': 'terminal'})
 
 
 def extend_pp_net_cim(net: pandapowerNet, override: bool = True) -> pandapowerNet:
