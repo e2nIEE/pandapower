f83fdb5aa8864864c15dca4ed7e94251d836ee0f
Panos Xenos <xenos.panos@gmail.com>
2025-02-13 12:52:45 +0200

    bring back xfails & expand todo in test_contingency.py

--- pandapower/test/contingency/test_contingency.py
+++ pandapower/test/contingency/test_contingency.py
@@ -132,6 +132,7 @@ def test_with_lightsim2grid(get_net, get_case):
                                net.res_trafo[f"{s}_loading_percent"].values, atol=1e-6, rtol=0), s
 
 
+@pytest.mark.xfail(reason="remove this xfail when new version of lightsim2grid available")
 @pytest.mark.skipif(not lightsim2grid_installed, reason="lightsim2grid package is not installed")
 def test_case118():
     net = pp.networks.case118()
@@ -152,6 +153,7 @@ def test_case118():
                                net.res_trafo[f"{s}_loading_percent"].values, atol=1e-6, rtol=0), s
 
 
+@pytest.mark.xfail(reason="remove this xfail when new version of lightsim2grid available")
 @pytest.mark.skipif(not lightsim2grid_installed, reason="lightsim2grid package is not installed")
 def test_unequal_trafo_hv_lv_impedances():
     net = pp.create_empty_network()
@@ -408,7 +410,10 @@ def _randomize_indices(net):
         pp.reindex_elements(net, element, new_index)
 
 
-@pytest.fixture(params=["case9", "case14", "case118"])
+# todo: bring back case118 and remove xfail from test_unequal_trafo_hv_lv_impedances and test_case118 pytests
+#  when lightsim2grid new version is released - see https://github.com/Grid2op/lightsim2grid/issues/88
+# @pytest.fixture(params=["case9", "case14", "case118"])
+@pytest.fixture(params=["case9", "case14"])
 def get_net(request):
     # pandapower and lightsim2grid behave differently when the grid becomes isolated from the ext_grid:
     # pandapower selects next gen and uses it as ext_grid, and lightsim2grid does not and therefore has nan for results
