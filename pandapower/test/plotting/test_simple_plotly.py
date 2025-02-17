daeecfc9be516c0053acf21173b6a889f1a405c6
Jolando Kisse <jolando.kisse@uni-kassel.de>
2025-02-17 13:22:46 +0100

    Update test_simple_plotly.py

--- pandapower/test/plotting/test_simple_plotly.py
+++ pandapower/test/plotting/test_simple_plotly.py
@@ -28,7 +28,7 @@ def test_simple_plotly_coordinates():
     markers_sgen = create_weighted_marker_trace(net, elm_type="sgen", color="green",
                                                 patch_type="circle-open", sizemode="diameter",
                                                 marker_scaling=100, scale_marker_size=0.5)
-    fig = simple_plotly(net, filename=None, auto_open=False,
+    fig = simple_plotly(net, filename=join(gettempdir(), "temp-plot.html"), auto_open=False)
                         additional_traces=[markers_sgen, markers_load])
     assert len(fig.data) == (len(net.line) + 1) + (len(net.trafo) + 1) + 6
                             # +1 for the infofunc traces,
@@ -40,6 +40,16 @@ def test_simple_plotly_coordinates():
 def test_simple_plotly_3w():
     # net with 3W-transformer
     net = nw.example_multivoltage()
+    fig = simple_plotly(net, filename=join(gettempdir(), "temp-plot.html"), auto_open=False)
+    assert len(fig.data) == (len(net.line) + 1) + (len(net.trafo) + 1) + (len(net.trafo3w)*3 + 1) + 2
+    # +1 is for infofunc traces, +2 = 1 bus trace + 1 ext_grid trace
+
+
+@pytest.mark.slow
+@pytest.mark.skipif(not PLOTLY_INSTALLED, reason="plotly functions require the plotly package")
+def test_simple_plotly_no_html():
+    net = nw.example_multivoltage()
+    # fig without generating a HTML
     fig = simple_plotly(net, filename=None, auto_open=False)
     assert len(fig.data) == (len(net.line) + 1) + (len(net.trafo) + 1) + (len(net.trafo3w)*3 + 1) + 2
     # +1 is for infofunc traces, +2 = 1 bus trace + 1 ext_grid trace
