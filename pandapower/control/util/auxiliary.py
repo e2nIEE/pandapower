a1aceb4b4b94c5170c500c0f602fb240d5d61fae
Ghanshyam Malaviya <ghanshyamgit@gmail.com>
2025-03-13 12:14:40 +0100

    FiX: Codacy Static Code Analysis error

--- pandapower/control/util/auxiliary.py
+++ pandapower/control/util/auxiliary.py
@@ -412,7 +412,8 @@ def create_q_capability_curve_characteristics_object(net):
     # create characteristics
     if "q_capability_curve_table" in net.keys() and net['q_capability_curve_table'].index.size > 0:
         time_start = time.time()
-        
+
+        # Set flag reactive_capability_curve
         _set_reactive_capability_curve_flag(net, "gen")
         _set_reactive_capability_curve_flag(net, "sgen")
 
