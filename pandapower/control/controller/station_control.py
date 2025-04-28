WIP writing test functions for Binary Search Control and Droop Control.  Fixed sgens at limit in max Q distribution and max Q distribution.  Fixed distribution calculation for voltage set point adaptation.

pandapower/control/controller/station_control.py
@@ -511,8 +511,8 @@ class BinarySearchControl(Controller):
             step_diff = self.diff - self.diff_old
             x = self.output_values - self.diff * (self.output_values - self.output_values_old) / np.where(
                 step_diff == 0, 1e-6, step_diff)  #converging
-            if any(x > 100): #catching overshoots
-                x = np.atleast_1d(self.output_values)[self.output_element_in_service] + 1e-3 #todo gens out of reach
+            if any(abs(x) > 50): #catching overshoots
+                x[np.where(abs(x) > 50)[0]] = np.sign(x[np.where(abs(x) > 50)[0]]) * 50
             ###calculate the distribution of the output values
             if self.output_values_distribution == 'imported': #when importing net from PF, calculation for distribution is not necessary
                 distribution = output_distribution_values_in_service
@@ -661,8 +661,12 @@ class BinarySearchControl(Controller):
                     raise NotImplementedError(f"Controller {self.index}: Reactive power distribution method {self.output_values_distribution}"
                                               f" not implemented available methods are (rel_P, rel_rated_P, set_Q, max_Q, rel_V_pu).")
             if self.output_values_distribution == 'max_Q': #max_Q and voltage gives the correct Qs for the gens
-                values = (sum(x) - sum(distribution)) / len(np.atleast_1d(distribution)[generators_to_consider])
-                np.atleast_1d(distribution)[generators_to_consider] += values
+                if sum(np.atleast_1d(generators_to_consider)) == 0:
+                    values = (sum(x) - sum(distribution)) / len(np.atleast_1d(distribution))
+                    distribution = np.atleast_1d(distribution) + values
+                else:
+                    values = (sum(x) - sum(distribution)) / len(np.atleast_1d(distribution)[generators_to_consider])
+                    np.atleast_1d(distribution)[generators_to_consider] += values
                 x = distribution
             #Voltage set point adaption gives correct Qs but needs convergence
             elif (self.output_values_distribution == 'rel_V_pu' and (len(np.atleast_1d(self.output_element_in_service)) > 1
@@ -676,7 +680,6 @@ class BinarySearchControl(Controller):
                     equal = 1 / sum(self.output_element_in_service)
                     distribution = np.full(np.sum(np.array(self.output_element_in_service)), equal)
                 x = x * distribution if isinstance(x, numbers.Number) else sum(x) * distribution #add distribution to Q values
-
             self.output_values_old, self.output_values = self.output_values, x
 
         ### write new set of Q values to output elements###

pandapower/test/control/test_stactrl.py
@@ -458,13 +458,14 @@ def test_max_q():
                         None, 'PF_ctrl_cap', None, 1e-6)
     runpp(net, run_control = True)
     assert(net.sgen.at[0, 'q_mvar'] != net.sgen.at[1, 'q_mvar'])
-    net = distribution_test_net()
-    net.sgen.at[0, 'min_q_mvar'] = -20 #testing one generator at limit
-    net.sgen.at[0, 'max_q_mvar'] = 0
-    net.sgen.at[1, 'min_q_mvar'] = -7
-    net.sgen.at[1, 'max_q_mvar'] = 20
-    net.sgen.at[2, 'min_q_mvar'] = 4
-    net.sgen.at[2, 'max_q_mvar'] = -7
+    net = distribution_test_net()#testing generators at limit
+    net.sgen.at[0, 'min_q_mvar'] = -20 #lowest
+    net.sgen.at[0, 'max_q_mvar'] = 0 #least high
+    net.sgen.at[1, 'min_q_mvar'] = -7 #second lowest
+    net.sgen.at[1, 'max_q_mvar'] = 20 #highest
+    net.sgen.at[2, 'min_q_mvar'] = 4 #second highest
+    net.sgen.at[2, 'max_q_mvar'] = -6 # least low
+    idx_neg, idx_pos = [2, 1, 0], [0, 2, 1] #correct orders
     #bsc.initialize_control(net) #todo
     #net.controller.drop(index=0, inplace=True)
     BinarySearchControl(net, True, 'sgen', 'q_mvar',
@@ -472,7 +473,16 @@ def test_max_q():
                         'q_to_mvar', 0, 0.2, 'max_Q',
                         None, 'PF_ctrl_cap', None, 1e-6)
     runpp(net, run_control = True)
-    assert(net.sgen.at[0, 'q_mvar'] != net.sgen.at[1, 'q_mvar'])
+    #checking if control worked
+    assert(net.sgen.at[0, 'q_mvar'] != net.sgen.at[1, 'q_mvar'] != net.sgen.at[2, 'q_mvar'])
+    #checking if Q output order coincides with set Q limits
+    all_sgens = np.array([abs(net.sgen.at[0, 'q_mvar']), abs(net.sgen.at[1, 'q_mvar']), abs(net.sgen.at[2, 'q_mvar'])])
+    idx = np.argsort(all_sgens)
+    assert(np.array_equal(idx, idx_pos) or np.array_equal(idx,idx_neg)) #correct order for set values + and -
+    assert(abs(net.sgen.at[idx_neg[0], 'q_mvar']) < abs(net.sgen.at[idx_neg[1], 'q_mvar']) < #redundant
+           abs(net.sgen.at[idx_neg[2], 'q_mvar']) or abs(net.sgen.at[idx_pos[0], 'q_mvar']) <
+           abs(net.sgen.at[idx_pos[1], 'q_mvar']) < abs(net.sgen.at[idx_pos[2], 'q_mvar']))
+
 
 
 def test_rel_v_pu():