3c6ab6e0436d7c58e12ca858545ef5e63567adaa
Ghanshyam-grid <ghanshyamgit@gmail.com>
2025-04-29 12:20:33 +0200

    CHG: change the line diff

--- pandapower/test/converter/test_from_powerfactory.py
+++ pandapower/test/converter/test_from_powerfactory.py
@@ -146,7 +146,7 @@ def test_pf_export_tap_changer():
 
 @pytest.mark.skipif(not PF_INSTALLED, reason='powerfactory must be installed')
 def test_pf_export_q_capability_curve():
-    app = pf.GetApplication()
+    app = pf.GetApplication("LO3195", "Gbhai001GBHAI!")
     # import the tap changer test grid to powerfactory
     path = os.path.join(pp_dir, 'test', 'converter', 'q_capabiltiy_curve.pfd')
     prj = import_project(path, app, 'TEST_PF_CONVERTER', import_folder='TEST_IMPORT', clear_import_folder=True)
@@ -174,7 +174,7 @@ def test_pf_export_q_capability_curve():
         'diff_vm': 5e-3,
         'diff_va': 0.1,
         'trafo_diff': 1e-2,
-        'line_diff': 1e1,
+        'line_diff': 1e-2,
         'gen_p_diff_is': 1e-5,
         'gen_q_diff_is': 1e-5,
         'load_p_diff_is': 1e-5,
