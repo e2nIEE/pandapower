9106e0d59c95187cc6fe2bb9996e832019af14b5
Dominik Hilbrich <dominik.hilbrich@iee.fraunhofer.de>
2025-05-30 09:45:52 +0200

    Adjusted equivalent calculation for adapted merge_net function.

--- pandapower/grid_equivalents/rei_generation.py
+++ pandapower/grid_equivalents/rei_generation.py
@@ -162,6 +162,13 @@ def _create_net_zpbn(net, boundary_buses, all_internal_buses, all_external_buses
     net_internal, net_external = _get_internal_and_external_nets(
         net, boundary_buses, all_internal_buses, all_external_buses,
         show_computing_time, calc_volt_angles=calc_volt_angles, runpp_fct=runpp_fct, **kwargs)
+    if "trafo_characteristic_table" in net_internal:
+        net_internal.trafo_characteristic_table = net_internal.trafo_characteristic_table[
+            ~net_internal.trafo_characteristic_table.id_characteristic.isin(net_external.trafo.id_characteristic_table)
+        ]
+        net_external.trafo_characteristic_table = net_external.trafo_characteristic_table[
+            ~net_external.trafo_characteristic_table.id_characteristic.isin(net_internal.trafo.id_characteristic_table)
+        ]
     net_zpbn = net_external
     # --- remove buses without power flow results in net_eq
     drop_buses(net_zpbn, net_zpbn.res_bus.index[net_zpbn.res_bus.vm_pu.isnull()])
