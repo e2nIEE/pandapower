bbc5af20e8cd0865a3d4073e7789cd16712661ad
marcopau <marco.pau@iee.fraunhofer.de>
2025-06-13 12:16:45 +0200

    Speed improvements and fix RAM issues

--- pandapower/estimation/algorithm/matrix_base.py
+++ pandapower/estimation/algorithm/matrix_base.py
@@ -4,12 +4,10 @@
 # and Energy System Technology (IEE), Kassel. All rights reserved.
 
 import numpy as np
+from numpy import conj, arange, int64
 from scipy.sparse import vstack, hstack
-
+from scipy.sparse import eye, csr_matrix as sparse
 from pandapower.pypower.idx_brch import F_BUS, T_BUS
-from pandapower.pypower.dSbus_dV import dSbus_dV
-from pandapower.pypower.dSbr_dV import dSbr_dV
-from pandapower.pypower.dIbr_dV import dIbr_dV_new
 
 from pandapower.estimation.ppc_conversion import ExtendedPPCI
 
@@ -58,42 +56,39 @@ class BaseAlgebra:
             E2 = E[-num_clusters:]
         else:
             E1 = E
+        
+        meas_mask = self.eppci.non_nan_meas_mask
         V = self.eppci.E2V(E1)
         Sfe = V[f_bus] * np.conj(self.Yf * V)
         Ste = V[t_bus] * np.conj(self.Yt * V)
         Sbuse = V * np.conj(self.Ybus * V)
-        hx = np.r_[np.real(Sbuse),
-                   np.real(Sfe),
-                   np.real(Ste),
-                   np.imag(Sbuse),
-                   np.imag(Sfe),
-                   np.imag(Ste),
-                   np.abs(V)]
-
-        # if self.any_i_meas or self.any_degree_meas:
-        va = np.angle(V)
         Ife = self.Yf * V
-        ifem = np.abs(Ife)
-        ifea = np.angle(Ife)
         Ite = self.Yt * V
-        item = np.abs(Ite)
-        itea = np.angle(Ite)
-        hx = np.r_[hx,
-                    va,
-                    ifem,
-                    item,
-                    ifea,
-                    itea]
+
+        Pbuse = np.real(Sbuse)[meas_mask["pbus"]]
+        Pfe = np.real(Sfe)[meas_mask["pfrom"]]
+        Pte = np.real(Ste)[meas_mask["pto"]]
+        Qbuse = np.imag(Sbuse)[meas_mask["qbus"]]
+        Qfe = np.imag(Sfe)[meas_mask["qfrom"]]
+        Qte = np.imag(Ste)[meas_mask["qto"]]
+        Vm = np.abs(V)[meas_mask["vm"]]
+        Va = np.angle(V)[meas_mask["va"]]
+        Imfe = np.abs(Ife)[meas_mask["ifrom"]]
+        Imte = np.abs(Ite)[meas_mask["ito"]]
+        # Iafe = np.angle(Ife)[meas_mask["iafrom"]]
+        # Iate = np.angle(Ite)[meas_mask["iato"]]
+        
+        hx = np.r_[Pbuse, Qbuse, Pfe, Qfe, Pte, Qte, Vm, Va, Imfe, Imte]
 
         if self.eppci.algorithm == "af-wls":
-            Pbuse2 = np.sum(np.multiply(E2,self.eppci["rated_power_clusters"][:,:num_clusters]),axis=1)
-            Qbuse2 = np.sum(np.multiply(E2,self.eppci["rated_power_clusters"][:,num_clusters:2*num_clusters]),axis=1)
-            hx = np.r_[hx,
-                       np.real(Sbuse)-Pbuse2,
-                       np.imag(Sbuse)-Qbuse2,
-                       E2]
+            Pb2 = np.real(Sbuse) - np.sum(np.multiply(E2,self.eppci["rated_power_clusters"][:,:num_clusters]),axis=1)
+            Qb2 = np.imag(Sbuse) - np.sum(np.multiply(E2,self.eppci["rated_power_clusters"][:,num_clusters:2*num_clusters]),axis=1)
+            Pbuse2 = Pb2[meas_mask["pbalance"]]
+            Qbuse2 = Qb2[meas_mask["qbalance"]]
+            E2e = E2[meas_mask["afactor"]]
+            hx = np.r_[hx, Pbuse2, Qbuse2, E2e]
         
-        return hx[self.non_nan_meas_selector]
+        return hx
 
     def create_hx_jacobian(self, E):
         # Using sparse matrix in creation sub-jacobian matrix
@@ -103,115 +98,206 @@ class BaseAlgebra:
         else:
             E1 = E
 
+        meas_mask = self.eppci.non_nan_meas_mask
         V = self.eppci.E2V(E1)
+        nvar = 2*len(V)
+        jac = sparse((0, nvar))
 
-        dSbus_dth, dSbus_dv = self._dSbus_dv(V)
-        dSf_dth, dSf_dv, dSt_dth, dSt_dv = self._dSbr_dv(V)
-        dvm_dth, dvm_dv = self._dvmbus_dV(V)
+        if len(meas_mask["pbus"])+len(meas_mask["qbus"])>0:
+            dPbus, dQbus = self._dSbus_dv(V, meas_mask["pbus"], meas_mask["qbus"])
+            jac = vstack((jac, dPbus, dQbus))
 
-        s_jac_th = vstack((dSbus_dth.real,
-                           dSf_dth.real,
-                           dSt_dth.real,
-                           dSbus_dth.imag,
-                           dSf_dth.imag,
-                           dSt_dth.imag))
-        s_jac_v = vstack((dSbus_dv.real,
-                          dSf_dv.real,
-                          dSt_dv.real,
-                          dSbus_dv.imag,
-                          dSf_dv.imag,
-                          dSt_dv.imag))
+        if len(meas_mask["pfrom"])+len(meas_mask["qfrom"])>0:
+            dPf, dQf = self._dSbr_dv(V, "from", meas_mask["pfrom"], meas_mask["qfrom"])
+            jac = vstack((jac, dPf, dQf))
 
-        s_jac = hstack((s_jac_th, s_jac_v)).toarray()
-        vm_jac = np.c_[dvm_dth, dvm_dv]
-        jac = np.r_[s_jac,
-                    vm_jac]
+        if len(meas_mask["pto"])+len(meas_mask["qto"])>0:
+            dPt, dQt = self._dSbr_dv(V, "to", meas_mask["pto"], meas_mask["qto"])
+            jac = vstack((jac, dPt, dQt))
 
-        # if self.any_i_meas or self.any_degree_meas:
-        dva_dth, dva_dv = self._dvabus_dV(V)
-        va_jac = np.c_[dva_dth, dva_dv]
-        difm_dth, difm_dv, ditm_dth, ditm_dv,\
-            difa_dth, difa_dv, dita_dth, dita_dv = self._dimiabr_dV(V)
-        im_jac_th = np.r_[difm_dth,
-                            ditm_dth]
-        im_jac_v = np.r_[difm_dv,
-                            ditm_dv]
-        ia_jac_th = np.r_[difa_dth,
-                            dita_dth]
-        ia_jac_v = np.r_[difa_dv,
-                            dita_dv]
+        dVm = self._dVmbus_dV(V, meas_mask["vm"])
+        jac = vstack((jac, dVm))
 
-        im_jac = np.c_[im_jac_th, im_jac_v]
-        ia_jac = np.c_[ia_jac_th, ia_jac_v]
+        if len(meas_mask["va"])>0:
+            dVa = self._dVabus_dV(V, meas_mask["va"])
+            jac = vstack((jac, dVa))
 
-        jac = np.r_[jac,
-                    va_jac,
-                    im_jac,
-                    ia_jac]
+        if len(meas_mask["ifrom"])>0:
+            dIfm = self._dImbr_dV(V, "from", meas_mask["ifrom"])
+            jac = vstack((jac, dIfm))
+
+        if len(meas_mask["ito"])>0:
+            dItm = self._dImbr_dV(V, "to", meas_mask["ito"])
+            jac = vstack((jac, dItm))
+
+        # dIfa = self._dIabr_dV(V, "from")
+        # dIta = self._dIabr_dV(V, "to")
 
         if self.eppci.algorithm == "af-wls":
-            p_eq_bal_jac_E1 = hstack((dSbus_dth.real, dSbus_dv.real)).toarray()
-            q_eq_bal_jac_E1 = hstack((dSbus_dth.imag, dSbus_dv.imag)).toarray()
-            af_vmeas_E1 = np.zeros((num_clusters,jac.shape[1]))
+            p_bal_jac_E1, q_bal_jac_E1 = self._dSbus_dv(V, meas_mask["pbalance"], meas_mask["qbalance"])
+            af_vmeas_E1 = sparse((num_clusters,jac.shape[1])) 
 
-            jac_E2 = np.zeros((jac.shape[0],num_clusters))
-            p_eq_bal_jac_E2 = - self.eppci["rated_power_clusters"][:,:num_clusters]
-            q_eq_bal_jac_E2 = - self.eppci["rated_power_clusters"][:,num_clusters:2*num_clusters]
-            af_vmeas_E2 = np.identity(num_clusters)
+            jac_E2 = sparse((jac.shape[0],num_clusters))
+            p_bal_jac_E2 = sparse(- self.eppci["rated_power_clusters"][:,:num_clusters])
+            q_bal_jac_E2 = sparse(- self.eppci["rated_power_clusters"][:,num_clusters:2*num_clusters])
+            af_vmeas_E2 = eye(num_clusters, num_clusters, format='csr')
+            p_bal_jac_E2 = p_bal_jac_E2[meas_mask["pbalance"]]
+            q_bal_jac_E2 = q_bal_jac_E2[meas_mask["qbalance"]]
+            af_vmeas_E2 = af_vmeas_E2[meas_mask["afactor"]]
 
-            jac = np.r_[jac,
-                        p_eq_bal_jac_E1,
-                        q_eq_bal_jac_E1,
-                        af_vmeas_E1]
+            jac = vstack((jac, p_bal_jac_E1, q_bal_jac_E1, af_vmeas_E1))
+            jac = jac[:][:, self.delta_v_bus_selector]
+            jac_E2 = vstack((jac_E2, p_bal_jac_E2, q_bal_jac_E2, af_vmeas_E2))
 
-            jac_E2 = np.r_[jac_E2,
-                        p_eq_bal_jac_E2,
-                        q_eq_bal_jac_E2,
-                        af_vmeas_E2]
-
-            jac = jac[self.non_nan_meas_selector, :][:, self.delta_v_bus_selector]
-            jac_E2 = jac_E2[self.non_nan_meas_selector, :][:]
-            jac = np.c_[jac, jac_E2]
+            jac = hstack((jac, jac_E2))
 
         else:
-            jac = jac[self.non_nan_meas_selector, :][:, self.delta_v_bus_selector]
+            jac = jac[:][:, self.delta_v_bus_selector]
 
         return jac
 
-    def _dSbus_dv(self, V):
-        dSbus_dv, dSbus_dth = dSbus_dV(self.Ybus, V)
-        return dSbus_dth, dSbus_dv
+    def _dSbus_dv(self, V, maskP=None, maskQ=None):
+        Ybus = self.Ybus
+        Ibus = Ybus * V
+        b = len(V)
+        ib = arange(b)
 
-    def _dSbr_dv(self, V):
-        dSf_dth, dSf_dv, dSt_dth, dSt_dv, _, _ = dSbr_dV(self.eppci['branch'], self.Yf, self.Yt, V)
-        return dSf_dth, dSf_dv, dSt_dth, dSt_dv
+        maskS, maskP, maskQ = self._define_mask(ib, maskP, maskQ)
 
+        diagV = sparse((V, (ib, ib)))
+        diagV2 = diagV[maskS,:]
+        diagIbus = sparse((Ibus, (ib, ib)))
+        diagIbus2 = diagIbus[maskS,:]
+        diagVnorm = sparse((V / abs(V), (ib, ib)))
+
+        dS_dVm = diagV2 @ conj(Ybus @ diagVnorm) + conj(diagIbus2) @ diagVnorm
+        dS_dVa = 1j * diagV2 @ conj(diagIbus - Ybus @ diagV)
+
+        dP_dth = dS_dVa.real[maskP,:]
+        dP_dv = dS_dVm.real[maskP,:]
+        dQ_dth = dS_dVa.imag[maskQ,:]
+        dQ_dv = dS_dVm.imag[maskQ,:]
+
+        dP = hstack((dP_dth, dP_dv))
+        dQ = hstack((dQ_dth, dQ_dv))
+
+        return dP, dQ
+     
+
+    def _dSbr_dv(self, V, side, maskP=None, maskQ=None):
+        branch = self.eppci['branch']
+        f = branch[:, F_BUS].real.astype(int64)       ## list of "from" buses
+        t = branch[:, T_BUS].real.astype(int64)       ## list of "to" buses
+        nl = len(f)
+        nb = len(V)
+        il = arange(nl)
+        ib = arange(nb)
+        maskS, maskP, maskQ = self._define_mask(il, maskP, maskQ)
+
+        if side == "from":
+            Y = self.Yf
+            s = f
+        elif side == "to":
+            Y = self.Yt
+            s = t
+
+        I = Y * V
+        Vnorm = V / abs(V)
+
+        diagVs = sparse((V[s], (il, il)))
+        diagVs2 = diagVs[maskS,:]
+        diagI = sparse((I, (il, il)))
+        diagI2 = diagI[maskS,:]
+        diagV  = sparse((V, (ib, ib)))
+        diagVnorm = sparse((Vnorm, (ib, ib)))
+
+        shape = (nl, nb)
+        # Partial derivative of S w.r.t voltage phase angle.
+        dS_dVa = 1j * (conj(diagI2) @
+            sparse((V[s], (il, s)), shape) - diagVs2 @ conj(Y @ diagV))
+        # Partial derivative of S w.r.t. voltage amplitude.
+        dS_dVm = diagVs2 @ conj(Y @ diagVnorm) + conj(diagI2) @ \
+            sparse((Vnorm[s], (il, s)), shape)
+        
+        dP_dth = dS_dVa.real[maskP,:]
+        dP_dv = dS_dVm.real[maskP,:]
+        dQ_dth = dS_dVa.imag[maskQ,:]
+        dQ_dv = dS_dVm.imag[maskQ,:]
+
+        dP = hstack((dP_dth, dP_dv))
+        dQ = hstack((dQ_dth, dQ_dv))
+
+        return dP, dQ
+
+    def _dVmbus_dV(self, V, maskVm=None):
+        V_shape = V.shape[0]
+        if maskVm is None:
+            maskVm = arange(V_shape)
+
+        dvm_dth = sparse((len(maskVm), V_shape))  # Sparse zero matrix
+        dvm_dv = eye(V_shape, V_shape, format='csr')
+        dvm_dv = dvm_dv[maskVm,:]
+        dvm = hstack((dvm_dth, dvm_dv))
+        return dvm
+
+    def _dVabus_dV(self, V, maskVa=None):
+        V_shape = V.shape[0]
+        if maskVa is None:
+            maskVa = arange(V_shape)
+
+        dva_dth = eye(V_shape, V_shape, format='csr')  # Sparse identity matrix
+        dva_dth = dva_dth[maskVa,:]
+        dva_dv = sparse((len(maskVa), V_shape))  # Sparse zero matrix
+        dva = hstack((dva_dth, dva_dv))
+        return dva
+    
+    def _dImbr_dV(self, V, side, maskI=None):
+        nl = len(self.eppci['branch'])
+        nb = len(V)
+        il = arange(nl)
+        vb = arange(nb)
+
+        if maskI is None:
+            maskI = il
+
+        if side == "from":
+            Y = self.Yf
+        elif side == "to":
+            Y = self.Yt
+
+        # Compute branch currents.
+        I = Y * V
+
+        diagV = sparse((V, (vb, vb)))
+        diagVnorm = sparse((V / abs(V), (vb, vb)))
+        idx = abs(I) != 0
+        I = I[idx]
+        il = il[idx]
+        diagInorm = sparse((conj(I) / abs(I), (il, il)), shape=(nl,nl))
+        diagInorm2 = diagInorm[maskI,:]
+        a = diagInorm2 @ Y @ diagV
+        b = diagInorm2 @ Y @ diagVnorm
+        dIm_dth = - a.imag
+        dIm_dv = b.real
+        dIm = hstack((dIm_dth, dIm_dv))
+
+        return dIm
+    
+    def _define_mask(self, idx, mask1, mask2):
+        if mask1 is None:
+            mask1 = idx
+        if mask2 is None:
+            mask2 = idx
+        masktot, mask1, mask2 = self._merge_mask(mask1, mask2)
+
+        return masktot, mask1, mask2
+    
     @staticmethod
-    def _dvmbus_dV(V):
-        dvm_dth, dvm_dv = np.zeros((V.shape[0], V.shape[0])), np.eye(V.shape[0], V.shape[0])
-        return dvm_dth, dvm_dv
-
-    @staticmethod
-    def _dvabus_dV(V):
-        dva_dth, dva_dv = np.eye(V.shape[0], V.shape[0]), np.zeros((V.shape[0], V.shape[0]))
-        return dva_dth, dva_dv
-
-    def _dimiabr_dV(self, V):
-        # for current we only interest in the magnitude at the moment
-        difm_dth, difm_dv, ditm_dth, ditm_dv = dIbr_dV_new(self.eppci['branch'], self.Yf, self.Yt, V)
-        difm_dth, difm_dv, ditm_dth, ditm_dv = map(lambda m: m.toarray(), (difm_dth, difm_dv, ditm_dth, ditm_dv))
-        difa_dth, difa_dv, dita_dth, dita_dv = 0*difm_dth, 0*difm_dv, 0*ditm_dth, 0*ditm_dv
-        # dif_dth, dif_dv, dit_dth, dit_dv, If, It = dIbr_dV(self.eppci['branch'], self.Yf, self.Yt, V)
-        # dif_dth, dif_dv, dit_dth, dit_dv = map(lambda m: m.toarray(), (dif_dth, dif_dv, dit_dth, dit_dv))
-        # difm_dth = (np.abs(1e-5 * dif_dth + If.reshape((-1, 1))) - np.abs(If.reshape((-1, 1))))/1e-5
-        # difm_dv = (np.abs(1e-5 * dif_dv + If.reshape((-1, 1))) - np.abs(If.reshape((-1, 1))))/1e-5
-        # ditm_dth = (np.abs(1e-5 * dit_dth + It.reshape((-1, 1))) - np.abs(It.reshape((-1, 1))))/1e-5
-        # ditm_dv = (np.abs(1e-5 * dit_dv + It.reshape((-1, 1))) - np.abs(It.reshape((-1, 1))))/1e-5
-        # difa_dth = (np.angle(1e-5 * dif_dth + If.reshape((-1, 1))) - np.angle(If.reshape((-1, 1))))/1e-5
-        # difa_dv = (np.angle(1e-5 * dif_dv + If.reshape((-1, 1))) - np.angle(If.reshape((-1, 1))))/1e-5
-        # dita_dth = (np.angle(1e-5 * dit_dth + It.reshape((-1, 1))) - np.angle(It.reshape((-1, 1))))/1e-5
-        # dita_dv = (np.angle(1e-5 * dit_dv + It.reshape((-1, 1))) - np.angle(It.reshape((-1, 1))))/1e-5
-        return difm_dth, difm_dv, ditm_dth, ditm_dv, difa_dth, difa_dv, dita_dth, dita_dv
+    def _merge_mask(mask1, mask2):
+        new_mask_tot = np.unique(np.concatenate((mask1,mask2),0))
+        new_mask1 = np.in1d(new_mask_tot, mask1)
+        new_mask2 = np.in1d(new_mask_tot, mask2)
+        return new_mask_tot, new_mask1, new_mask2
 
 
 class BaseAlgebraZeroInjConstraints(BaseAlgebra):
