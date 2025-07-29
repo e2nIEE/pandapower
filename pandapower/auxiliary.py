2dac5ad98f3e54bb83c54b622624a355285b6146
Mike Vogt <mike.vogt@iee.fraunhofer.de>
2025-07-29 13:32:08 +0200

    added source dc as an element to pandapower and adapted vsc to not overwrite all buses with DC_P

--- pandapower/auxiliary.py
+++ pandapower/auxiliary.py
@@ -45,7 +45,8 @@ from typing_extensions import deprecated
 from pandapower.pypower.idx_brch import F_BUS, T_BUS, BR_STATUS
 from pandapower.pypower.idx_brch_dc import DC_BR_STATUS, DC_F_BUS, DC_T_BUS
 from pandapower.pypower.idx_bus import BUS_I, BUS_TYPE, NONE, PD, QD, VM, VA, REF, PQ, VMIN, VMAX, PV
-from pandapower.pypower.idx_bus_dc import DC_VMAX, DC_VMIN, DC_BUS_I, DC_BUS_TYPE, DC_NONE, DC_REF, DC_B2B, DC_P
+from pandapower.pypower.idx_bus_dc import DC_VMAX, DC_VMIN, DC_BUS_I, DC_BUS_TYPE, DC_NONE, DC_REF, DC_B2B, DC_P, \
+    DC_SOURCE
 from pandapower.pypower.idx_gen import PMIN, PMAX, QMIN, QMAX
 from pandapower.pypower.idx_ssc import SSC_STATUS, SSC_BUS, SSC_INTERNAL_BUS
 from pandapower.pypower.idx_tcsc import TCSC_STATUS, TCSC_F_BUS, TCSC_T_BUS
@@ -889,6 +890,7 @@ def _check_connectivity(ppc):
         br_dc_status = ppc['branch_dc'][:, DC_BR_STATUS].astype(bool)
         nobranch_dc = ppc['branch_dc'][br_dc_status, :].shape[0]
         slacks_dc = ppc['bus_dc'][(ppc['bus_dc'][:, DC_BUS_TYPE] == DC_REF) |
+                                  (ppc['bus_dc'][:, DC_BUS_TYPE] == DC_SOURCE) |
                                   (ppc['bus_dc'][:, DC_BUS_TYPE] == DC_B2B), BUS_I]
 
         bus_from_dc = ppc['branch_dc'][br_dc_status, DC_F_BUS].real.astype(np.int64)
@@ -1034,17 +1036,15 @@ def _select_is_elements_numba(net, isolated_nodes=None, isolated_nodes_dc=None, 
     #    mode = net["_options"]["mode"]
     elements_ac = ["load", "motor", "sgen", "asymmetric_load", "asymmetric_sgen", "gen",
                    "ward", "xward", "shunt", "ext_grid", "storage", "svc", "ssc", "vsc"]  # ,"impedance_load"
-    elements_dc = ["vsc"]
+    elements_dc = ["vsc", "load_dc", "source_dc"]
     is_elements = dict()
-    for element_table_list, bus_table, bis in zip((elements_ac, elements_dc),
-                                                  ("bus", "bus_dc"), (bus_in_service, bus_dc_in_service)):
+    for element_table_list, bus_table, bis in zip((elements_ac, elements_dc), ("bus", "bus_dc"), (bus_in_service, bus_dc_in_service)):
         for element_table in element_table_list:
             num_elements = len(net[element_table].index)
             element_in_service = np.zeros(num_elements, dtype=bool)
             if num_elements > 0:
                 element_df = net[element_table]
-                set_elements_oos(element_df[bus_table].values, element_df["in_service"].values,
-                                 bis, element_in_service)
+                set_elements_oos(element_df[bus_table].values, element_df["in_service"].values, bis, element_in_service)
             # load, sgen, storage only in elements_ac so this will only be executed once:
             if net["_options"]["mode"] == "opf" and element_table in ["load", "sgen", "storage"]:
                 if "controllable" in net[element_table]:
@@ -1067,12 +1067,16 @@ def _select_is_elements_numba(net, isolated_nodes=None, isolated_nodes_dc=None, 
         #                    ppc_bus_isolated[net["_pd2ppc_lookups"]["aux"]["vsc"]] |
         #                    ppc_bus_isolated[net._ppc["vsc"][:, VSC_BUS].astype(np.int64)]]
         net._ppc["bus"][vsc_aux_isolated, BUS_TYPE] = NONE
+
         # if there are no in service VSC that define the DC slack node, we must change the DC slack to type P
         bus_dc_slack = net._ppc["bus_dc"][:, DC_BUS_TYPE] == DC_REF
         bus_dc_with_vsc = np.r_[
-            net._ppc["vsc"][is_elements["vsc"], VSC_BUS_DC], net._ppc["vsc"][is_elements["vsc"], VSC_INTERNAL_BUS_DC]]
+            net._ppc["vsc"][is_elements["vsc"], VSC_BUS_DC],
+            net._ppc["vsc"][is_elements["vsc"], VSC_INTERNAL_BUS_DC]
+        ]
         bus_dc_to_change = bus_dc_slack & (~np.isin(net._ppc["bus_dc"][:, DC_BUS_I], bus_dc_with_vsc))
-        net._ppc["bus_dc"][bus_dc_to_change, DC_BUS_TYPE] = DC_P
+        # TODO: changing this will also delete all voltage sources but there seems to be a problem
+        #net._ppc["bus_dc"][bus_dc_to_change, DC_BUS_TYPE] = DC_P
 
         # if the AC bus is defined as REF only because it is connected to a vsc, and the vsc is out of service,
         # it cannot be a REF bus anymore
@@ -1625,6 +1629,11 @@ def _add_b2b_vsc(net: pandapowerNet):
         create_vsc(net, ac_bus, bus_dc_plus, r_ohm/2., x_ohm/2., r_dc_ohm/2., pl_dc_mw=pl_dc_mw,
                    control_mode_ac=control_mode_ac, control_value_ac=control_value_ac, name=str(name)+"+",
                    control_mode_dc=control_mode_dc, control_value_dc=control_value_dc)
+
+        #in_service = True
+        #if control_mode_dc == 'vm_pu':
+        #    # control_value_dc = 1e-6
+        #    in_service = False
 
         create_vsc(net, ac_bus, bus_dc_minus, r_ohm/2., x_ohm/2., r_dc_ohm/2., pl_dc_mw=pl_dc_mw,
                    control_mode_ac=control_mode_ac, control_value_ac=control_value_ac, name=str(name)+"-",
