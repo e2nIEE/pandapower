11926f2a5fe5fec7209a80e980d2e388b7851451
David Heck <david.heck@iee.fraunhofer.de>
2025-10-01 08:19:10 +0200

    add descriptions to bus schema

--- pandapower/network_schema/bus.py
+++ pandapower/network_schema/bus.py
@@ -2,13 +2,13 @@ import pandera.pandas as pa
 
 schema = pa.DataFrameSchema(  # in methodcall but not parameter docu: geodata, coords
     {
-        "name": pa.Column(str),
-        "vn_kv": pa.Column(float, pa.Check.gt(0)),
-        "type": pa.Column(str, pa.Check.isin(["n", "b", "m"])),
-        "zone": pa.Column(str, nullable=True),
-        "max_vm_pu": pa.Column(float, pa.Check.gt(0), required=False),
-        "min_vm_pu": pa.Column(float, pa.Check.gt(0), required=False),
-        "in_service": pa.Column(bool),
+        "name": pa.Column(str, description='name of the bus'),
+        "vn_kv": pa.Column(float, pa.Check.gt(0), description="rated voltage of the bus [kV]"),
+        "type": pa.Column(str, pa.Check.isin(["n", "b", "m"]), description="type variable to classify buses"),
+        "zone": pa.Column(str, nullable=True, description="can be used to group buses, for example network groups / regions"),
+        "max_vm_pu": pa.Column(float, pa.Check.gt(0), required=False, description="Maximum voltage"),
+        "min_vm_pu": pa.Column(float, pa.Check.gt(0), required=False, description="Minimum voltage"),
+        "in_service": pa.Column(bool, description="specifies if the bus is in service."),
         "geo": pa.Column(str),  # missing in docu, not a create method parameter, kwargs?
     },
     strict=False,
