ba8299c2dcee6db7666010e6a52e77695387bb89
furqan463 <ahmadfurqanc@gmail.com>
2025-10-02 10:21:02 +0500

    Revert "comment out ValueError, to avoid test failure, and also resolve Sonarcloud issue"
    This reverts commit a22dafcaeac87f4bbe11d55058e1e81e4cfeb145.
    
    Signed-off-by: furqan463 <ahmadfurqanc@gmail.com>

--- pandapower/protection/utility_functions.py
+++ pandapower/protection/utility_functions.py
@@ -909,10 +909,10 @@ def switch_geodatas(net, size, distance_to_bus):
         pos_tb = np.zeros(2)
 
         pos_tb = _get_coords_from_bus_idx(net, target_bus)
-        # if len(pos_sb) > 1:
-        #    raise ValueError(f"Bus {sb} has multiple geodata entries: {pos_sb}")
-        # if len(pos_sb) == 0:
-        #    raise ValueError(f"Bus {sb} has no geodata entry.")
+        if len(pos_sb) > 1:
+            raise ValueError(f"Bus {sb} has multiple geodata entries: {pos_sb}")
+        if len(pos_sb) == 0:
+            raise ValueError(f"Bus {sb} has no geodata entry.")
         pos_tb = pos_tb[0]
 
         # position of switch symbol
