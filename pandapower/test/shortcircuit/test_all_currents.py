a2269af146040352ce1759fe348ebbd4378220e2
Johannes_sorg <johannes.sorg@envelio.de>
2025-10-14 15:45:03 +0200

    fix failing test

--- pandapower/test/shortcircuit/test_all_currents.py
+++ pandapower/test/shortcircuit/test_all_currents.py
@@ -715,7 +715,7 @@ def test_trafo_impedance(trafo_impedance_case):
     net = create_empty_network(sn_mva=0.16)
     create_bus(net, 20)
     create_buses(net, 2, 0.4)
-    create_ext_grid(net, 0, s_sc_max_mva=346.4102, rx_max=0.1)
+    create_ext_grid(net, 0, s_sc_max_mva=346.4102, rx_max=0.1, s_sc_min_mva=346.4102, rx_min=0.1)
     v_lv = 410
     create_transformer_from_parameters(net, 0, 1, 0.4, 20, v_lv / 1e3, 1.15, 4, 0, 0)
     create_line_from_parameters(net, 1, 2, 0.004, 0.208, 0.068, 0, 1, parallel=2, endtemp_degree=20)
@@ -730,7 +730,7 @@ def test_trafo_impedance(trafo_impedance_case):
     x_tlv = np.sqrt(z_tlv ** 2 - r_tlv ** 2)
     z_tlv = r_tlv + 1j * x_tlv
     x_t = x_tlv * 400 * 1e3 / (v_lv ** 2)
-    k_t = 0.95 * 1.05 / (1 + 0.6 * x_t)
+    k_t = 0.95 * 1.05 / (1 + 0.6 * x_t) if case == "max" else 1.0
     z_tk = k_t * z_tlv
 
     calc_sc(net, case=case, lv_tol_percent=6., bus=2, branch_results=True, use_pre_fault_voltage=False)
