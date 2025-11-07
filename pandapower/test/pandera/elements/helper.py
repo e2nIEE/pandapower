b38db52e896da4be46e7766123267fc044a75d65
David Heck <david.heck@iee.fraunhofer.de>
2025-11-07 11:42:35 +0100

    add zero number so helper

--- pandapower/test/pandera/elements/helper.py
+++ pandapower/test/pandera/elements/helper.py
@@ -35,17 +35,21 @@ others = [
 ]
 # Numeric types
 # ints
-zero = [np.int64(0), 0]
+zero_int = [np.int64(0), 0]
 positiv_ints = [1, 42, np.int64(1)]
+positiv_ints_plus_zero = [*positiv_ints, *zero_int]
 negativ_ints = [-1, -42, np.int64(-1)]
-all_allowed_ints = [*zero, *positiv_ints, *negativ_ints]
+negativ_ints_plus_zero = [*negativ_ints, *zero_int]
+all_allowed_ints = [*zero_int, *positiv_ints, *negativ_ints]
 not_allowed_ints = [np.int8(1), np.int16(1), np.int32(1), np.uint8(1), np.uint16(1), np.uint32(1)]
 all_ints = [*all_allowed_ints, *not_allowed_ints]
 # floats
-float_zero = [0.0, np.float64(0.0)]
+zero_float = [0.0, np.float64(0.0)]
 positiv_floats = [1.0, float("inf"), np.float64(1.0)]
+positiv_floats_plus_zero = [*positiv_floats, *zero_float]
 negativ_floats = [-1.0, float("-inf"), np.float64(-1.0)]
-all_allowed_floats = [*float_zero, *positiv_floats, *negativ_floats]
+negativ_floats_plus_zero = [*negativ_floats, *zero_float]
+all_allowed_floats = [*zero_float, *positiv_floats, *negativ_floats]
 not_allowed_floats = [
     np.float32(1.0),
     np.float16(1.0),
