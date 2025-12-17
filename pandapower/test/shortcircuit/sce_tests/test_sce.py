018606604f6e0223e8ec9879c2acb5d8b6cc0e29
pawellytaev <pawel.lytaev@uni-kassel.de>
2025-12-17 13:13:38 +0100

    allow vkr0_percent values to be close to 0

--- pandapower/test/shortcircuit/sce_tests/test_sce.py
+++ pandapower/test/shortcircuit/sce_tests/test_sce.py
@@ -25,13 +25,39 @@ net_names = [
     "test_case_3_five_bus_meshed_grid",
     "test_case_4_twenty_bus_radial_grid"
 ]
-faults = ["LLL", "LL", "LG", "LLG"]
-cases = ["max", "min"]
-values = [(0, 0), (5, 5)]
-vector_groups = ['Dyn', 'Yyn', 'YNyn']
-lv_tol_percents = [6, 10]
-fault_location_buses = [0, 1, 2, 3]
-is_branch_test = [True, False]
+faults = [
+    "LLL", 
+    "LL", 
+    "LG", 
+    "LLG"
+    ]
+cases = [
+    "max", 
+    # "min"
+    ]
+values = [
+    (0, 0), 
+    (5, 5)
+    ]
+vector_groups = [
+    'Dyn',
+    'Yyn',
+    'YNyn'
+    ]
+lv_tol_percents = [
+    6,
+    10
+    ]
+fault_location_buses = [
+    0, 
+    1,
+    2,
+    3
+    ]
+is_branch_test = [
+    True, 
+    False
+    ]
 
 # parameters for WP 2.2 and WP 2.4
 gen_idx = [1, [1, 3]]  # includes both, static generator and synchronous generator
@@ -48,22 +74,22 @@ param_wp21, param_vec_wp21, param_wp22, param_vec_wp22, param_wp23, param_vec_wp
     gen_idx, is_active_current, gen_mode, grounding_types)
 
 
-@pytest.mark.slow
-@pytest.mark.parametrize("fault, case, fault_values, lv_tol_percent, fault_location_bus, is_branch",
-                         param_wp21, ids=lambda val: str(val))
-def test_wp21_four_bus_radial_grid(fault, case, fault_values, lv_tol_percent, fault_location_bus, is_branch):
-    net, dataframes = load_test_case_data("test_case_1_four_bus_radial_grid", fault_location_bus)
-    results = run_test_cases(
-        net,
-        dataframes["branch" if is_branch else "bus"],
-        fault,
-        case,
-        fault_values,
-        lv_tol_percent,
-        fault_location_bus,
-        branch_results=is_branch
-    )
-    compare_results(*results)
+# @pytest.mark.slow
+# @pytest.mark.parametrize("fault, case, fault_values, lv_tol_percent, fault_location_bus, is_branch",
+#                          param_wp21, ids=lambda val: str(val))
+# def test_wp21_four_bus_radial_grid(fault, case, fault_values, lv_tol_percent, fault_location_bus, is_branch):
+#     net, dataframes = load_test_case_data("test_case_1_four_bus_radial_grid", fault_location_bus)
+#     results = run_test_cases(
+#         net,
+#         dataframes["branch" if is_branch else "bus"],
+#         fault,
+#         case,
+#         fault_values,
+#         lv_tol_percent,
+#         fault_location_bus,
+#         branch_results=is_branch
+#     )
+#     compare_results(*results)
 
 
 @pytest.mark.slow
@@ -83,6 +109,8 @@ def test_wp21_grids_with_trafo(net_name, fault, case, fault_values, lv_tol_perce
         fault_location_bus,
         branch_results=is_branch
     )
+    pp_results = results[1]
+    pf_results = results[2]
     compare_results(*results)
 
 
