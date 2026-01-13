1b64066a53a5ba20bcd31be07b0f102887c1e0e8
KS_HTK <2981026+KS-HTK@users.noreply.github.com>
2026-01-13 10:33:25 +0100

    reenabled deprecated create_empty_network

--- pandapower/create/network_create.py
+++ pandapower/create/network_create.py
@@ -3,12 +3,12 @@
 # Copyright (c) 2016-2026 by University of Kassel and Fraunhofer Institute for Energy Economics
 # and Energy System Technology (IEE), Kassel. All rights reserved.
 
-# from typing_extensions import deprecated
-#
-# from pandapower.auxiliary import pandapowerNet
-#
-# @deprecated('Use the pandapowerNet constructor instead: pandapowerNet(name="Name", ...)')
-# def create_empty_network(
-#     name: str = "", f_hz: float = 50.0, sn_mva: float = 1, add_stdtypes: bool = True
-# ) -> pandapowerNet:
-#     return pandapowerNet(name=name, f_hz=f_hz, sn_mva=sn_mva, add_stdtypes=add_stdtypes)
+from typing_extensions import deprecated
+
+from pandapower.auxiliary import pandapowerNet
+
+@deprecated('Use the pandapowerNet constructor instead: pandapowerNet(name="Name", ...)')
+def create_empty_network(
+    name: str = "", f_hz: float = 50.0, sn_mva: float = 1, add_stdtypes: bool = True
+) -> pandapowerNet:
+    return pandapowerNet(name=name, f_hz=f_hz, sn_mva=sn_mva, add_stdtypes=add_stdtypes)
