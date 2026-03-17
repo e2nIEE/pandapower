8a49bb711cd6c1adb10d143b088155e355fcfbdf
KS_HTK <2981026+KS-HTK@users.noreply.github.com>
2026-03-17 15:08:53 +0100

    added missing columns min_q_mvar and max_q_mvar to sgen for BinarySearchControl

--- pandapower/test/control/test_stactrl.py
+++ pandapower/test/control/test_stactrl.py
@@ -211,6 +211,10 @@ def test_qlimits_with_capability_curve(v, p):
     net.sgen.at[0, "id_q_capability_characteristic"] = 0
     net.sgen['curve_style'] = "straightLineYValues"
     create_q_capability_characteristics_object(net)
+    # min_q_mvar and max_q_mvar columns required for BinarySearchControl to work correctly
+    #  (see station_control.py _update_min_max_q_mvar function)
+    add_column_to_df(net, "sgen", "min_q_mvar")
+    add_column_to_df(net, "sgen", "max_q_mvar")
     BinarySearchControl(net, name="BSC1", ctrl_in_service=True,
                         output_element="sgen", output_variable="q_mvar", output_element_index=[0],
                         output_element_in_service=[True], output_values_distribution=[1],
