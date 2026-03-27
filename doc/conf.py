acf5c4ec603e4c207142e1920bf40a1f66c118be
KS_HTK <2981026+KS-HTK@users.noreply.github.com>
2026-03-27 16:16:53 +0100

    added updated logo and text logo for custom theming

--- doc/conf.py
+++ doc/conf.py
@@ -127,7 +127,9 @@ html_theme = "sphinx_rtd_theme"
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = {
+    "logo_only": True,
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 html_theme_path = ["_themes"]
@@ -141,7 +143,7 @@ html_theme_path = ["_themes"]
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-#html_logo = None
+html_logo = "pics/pp_text.svg"
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
