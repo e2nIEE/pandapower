07d22ed72fa458032901bad87901490b809246bc
KS_HTK <2981026+KS-HTK@users.noreply.github.com>
2026-03-27 09:24:08 +0100

    added css for rtd theme

--- doc/conf.py
+++ doc/conf.py
@@ -151,7 +151,11 @@ html_theme_path = ["_themes"]
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-#html_static_path = ['']
+html_static_path = ['_static']
+
+# These paths are either relative to html_static_path
+# or fully qualified paths (eg. https://...)
+html_css_files = ['theme.css']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
