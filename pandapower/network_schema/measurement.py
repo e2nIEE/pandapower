6929bbf7f5aa07419256b75249a3f242798f557e
KS_HTK <2981026+KS-HTK@users.noreply.github.com>
2026-04-09 10:48:49 +0200

    added tests for create_measurement updated schema removed checks in create_measurement when covered by typing removed forced element_type to bus, there is a raise UserWarning below for this case.

--- pandapower/network_schema/measurement.py
+++ pandapower/network_schema/measurement.py
@@ -6,7 +6,11 @@ import pandera.pandas as pa
 
 measurement_schema = pa.DataFrameSchema(
     {
-        "name": pa.Column(pd.StringDtype, description="Name of measurement"),
+        "name": pa.Column(
+            pd.StringDtype,
+            nullable=True,
+            description="Name of measurement"
+        ),
         "measurement_type": pa.Column(
             str, pa.Check.isin(["p", "q", "i", "v"]), description="Defines what physical quantity is measured"
         ),
@@ -30,13 +34,9 @@ measurement_schema = pa.DataFrameSchema(
             int,
             description="If the element_type is “line”, “trafo”, “trafo3w”, “load”, “gen”, “sgen”, “shunt”, “ward”, “xward” or “ext_grid”, element is the index of the relevant element. For “bus” measurements, it is None (default)",
         ),
-        "check_existing": pa.Column(
-            bool,
-            description="Checks if a measurement of the type already exists and overwrites it. If set to False, the measurement may be added twice (unsafe behaviour), but the performance increases",
-            metadata={"default": False},
-        ),  # TODO: shouldn't this be called overwrite?
         "side": pa.Column(
             str,
+            nullable=True,
             description="Only used for measured lines or transformers. Side defines at which end of the branch the measurement is gathered. For lines this may be “from“, “to“ to denote the side with the from_bus or to_bus. It can also be the index of the from_bus or to_bus. For transformers, it can be “hv“, “mv“ or “lv“ or the corresponding bus index, respectively.",
         ),  # TODO: check nur wenn element_type trafo oder line
         "origin_id": pa.Column(
