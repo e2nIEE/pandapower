5061da99e84a4dba979459519954dac4b46651d0
sgrigorevski <wenzelgri@gmail.com>
2026-05-13 14:51:30 +0200

    updated test_pandera_ssc_elements.py to test nullable values

--- pandapower/test/network_schema/elements/test_pandera_ssc_elements.py
+++ pandapower/test/network_schema/elements/test_pandera_ssc_elements.py
@@ -1,6 +1,7 @@
 # test_pandera_ssc_elements.py
 
 import itertools
+import numpy as np
 import pandas as pd
 import pandera as pa
 import pytest
@@ -69,14 +70,14 @@ class TestSscRequiredFields:
         "parameter,invalid_value",
         list(
             itertools.chain(
-                itertools.product(["bus"], [*negativ_ints, *not_ints_list]),
-                itertools.product(["r_ohm"], [*negativ_floats, *not_floats_list]),
-                itertools.product(["x_ohm"], [*positiv_floats, *not_floats_list]),
-                itertools.product(["set_vm_pu"], not_floats_list),
-                itertools.product(["vm_internal_pu"], not_floats_list),
-                itertools.product(["va_internal_degree"], not_floats_list),
-                itertools.product(["controllable"], not_boolean_list),
-                itertools.product(["in_service"], not_boolean_list),
+                itertools.product(["bus"], [float(np.nan), pd.NA, *negativ_ints, *not_ints_list]),
+                itertools.product(["r_ohm"], [float(np.nan), pd.NA, *negativ_floats, *not_floats_list]),
+                itertools.product(["x_ohm"], [float(np.nan), pd.NA, *positiv_floats, *not_floats_list]),
+                itertools.product(["set_vm_pu"], [float(np.nan), pd.NA, *not_floats_list]),
+                itertools.product(["vm_internal_pu"], [float(np.nan), pd.NA, *not_floats_list]),
+                itertools.product(["va_internal_degree"], [float(np.nan), pd.NA, *not_floats_list]),
+                itertools.product(["controllable"], [float(np.nan), pd.NA, *not_boolean_list]),
+                itertools.product(["in_service"], [float(np.nan), pd.NA, *not_boolean_list]),
             )
         ),
     )
@@ -188,7 +189,7 @@ class TestSscOptionalFields:
 
     @pytest.mark.parametrize(
         "parameter,invalid_value",
-        list(itertools.chain(itertools.product(["name"], not_strings_list))),
+        list(itertools.chain(itertools.product(["name"], [float(np.nan), *not_strings_list]))),
     )
     def test_invalid_optional_values(self, parameter, invalid_value):
         """Test: invalid optional values are rejected"""
@@ -234,6 +235,49 @@ class TestSscForeignKey:
         net.ssc["bus"] = 9999
         with pytest.raises(pa.errors.SchemaError):
             validate_network(net)
+
+    def test_valid_bus_index_non_sequential(self):
+        """Test: bus FK works with non-sequential bus indices"""
+        net = create_empty_network()
+        create_bus(net, 0.4, index=10)
+        create_bus(net, 0.4, index=42)
+        create_bus(net, 0.4, index=100)
+
+        create_ssc(
+            net,
+            bus=10,
+            r_ohm=0.0,
+            x_ohm=-0.1,
+            set_vm_pu=1.00,
+            vm_internal_pu=1.01,
+            va_internal_degree=0.0,
+            controllable=True,
+            in_service=True,
+        )
+        create_ssc(
+            net,
+            bus=42,
+            r_ohm=0.1,
+            x_ohm=-0.2,
+            set_vm_pu=0.98,
+            vm_internal_pu=0.99,
+            va_internal_degree=-1.0,
+            controllable=False,
+            in_service=False,
+        )
+        create_ssc(
+            net,
+            bus=100,
+            r_ohm=0.2,
+            x_ohm=-0.3,
+            set_vm_pu=1.02,
+            vm_internal_pu=1.03,
+            va_internal_degree=2.0,
+            controllable=True,
+            in_service=True,
+        )
+
+        validate_network(net)
 
 
 class TestSscResults:
