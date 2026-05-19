7f60a8713c1913825616d9742176f80d9cdc57e8
Jan Wiemer <jan.wiemer@iee.fraunhofer.de>
2026-05-19 17:05:34 +0200

    fix failing runpp(net, consider_line_temperature=True) for grids with a mix of lines with alpha and without added warning

--- pandapower/build_branch.py
+++ pandapower/build_branch.py
@@ -1555,9 +1555,10 @@ def _end_temperature_correction_factor(net, short_circuit=False, dc=False):
         delta_t_degree_celsius = net[element].temperature_degree_celsius.values.astype(np.float64) - 20
 
         if 'alpha' in net[element].columns:
-            alpha = net[element].alpha.values.astype(np.float64)
+            alpha = np.nan_to_num(net[element].alpha.values.astype(np.float64), nan=1.0, copy=True)
         else:
             alpha = 4e-3
+            warnings.warn(f"'alpha' is assumed to {alpha} and required for the calculation of the temperature based resistance.")
 
     r_correction_for_temperature = 1 + alpha * delta_t_degree_celsius
 
