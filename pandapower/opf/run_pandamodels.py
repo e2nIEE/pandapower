f443bc89ad15d1cbeca373f81c1298061371876b
Engr. Ahmad Furqan <ahmadfurqanc@gmail.com>
2026-05-24 17:49:07 +0500

    Enhance ImportError logging in run_pandamodels.py
    Improve error handling for ImportError in _call_pandamodels function.

--- pandapower/opf/run_pandamodels.py
+++ pandapower/opf/run_pandamodels.py
@@ -60,7 +60,8 @@ def _runpm(
 def _call_pandamodels(buffer_file, julia_file, dev_mode):  # pragma: no cover
     try:
         from juliacall import Main, Base, Pkg  # type: ignore
-    except ImportError:
+    except ImportError as e:
+        logger.debug(e)
         raise ImportError(
             "Please install juliacall properly to run pandapower with PandaModels.jl.")
 
