4b35c89fb578ff4ae39c2514293a016cc3b13bc4
mrichter <mario.richter@iee.fraunhofer.de>
2026-06-02 12:01:12 +0200

    cim2pp: mapping SV data to the assets with flag use_sv_data_for_assets=True

--- pandapower/converter/cim/cim2pp/convert_measurements.py
+++ pandapower/converter/cim/cim2pp/convert_measurements.py
@@ -352,3 +352,44 @@ class CreateMeasurements:
         self._set_measurement_element_datatype()
 
         self.logger.info("Needed time for creating the measurements: %ss" % (time.time() - time_start))
+
+    def map_sv_data_from_assets(self):
+        self.logger.info("--------------------------- Updating assets from SV ---------------------------")
+        time_start = time.time()
+        sc = cim_tools.get_pp_net_special_columns_dict()
+        # get the measurements from the sv profile and set the Terminal as index
+        sv_powerflow_g = self.cim['sv']['SvPowerFlow'][['Terminal', 'p', 'q']]
+        sv_powerflow_g['p'] *= -1
+        sv_powerflow_g['q'] *= -1
+        sv_powerflow_g = sv_powerflow_g.set_index('Terminal').to_dict()
+        sv_powerflow_l = self.cim['sv']['SvPowerFlow'][['Terminal', 'p', 'q']].set_index('Terminal').to_dict()
+
+        # update sgen
+        self.net.sgen.p_mw = self.net.sgen[sc['t']].map(sv_powerflow_g['p']).fillna(self.net.sgen.p_mw)
+        self.net.sgen.q_mvar = self.net.sgen[sc['t']].map(sv_powerflow_g['q']).fillna(self.net.sgen.q_mvar)
+        # update gen
+        self.net.gen.p_mw = self.net.gen[sc['t']].map(sv_powerflow_g['p']).fillna(self.net.gen.p_mw)
+
+        # update load
+        self.net.load.p_mw = self.net.load[sc['t']].map(sv_powerflow_l['p']).fillna(self.net.load.p_mw)
+        self.net.load.q_mvar = self.net.load[sc['t']].map(sv_powerflow_l['q']).fillna(self.net.load.q_mvar)
+
+        # update ward
+        self.net.ward.ps_mw = self.net.ward[sc['t']].map(sv_powerflow_l['p']).fillna(self.net.ward.ps_mw)
+        self.net.ward.qs_mvar = self.net.ward[sc['t']].map(sv_powerflow_l['q']).fillna(self.net.ward.qs_mvar)
+        # update xward
+        self.net.xward.ps_mw = self.net.xward[sc['t']].map(sv_powerflow_l['p']).fillna(self.net.xward.ps_mw)
+        self.net.xward.qs_mvar = self.net.xward[sc['t']].map(sv_powerflow_l['q']).fillna(self.net.xward.qs_mvar)
+
+        # update shunts
+        sv_scs = self.cim['sv']['SvShuntCompensatorSections'][['ShuntCompensator', 'sections']]
+        sv_scs = sv_scs.set_index('ShuntCompensator')
+        self.net.shunt.step = self.net.shunt[sc['o_id']].map(sv_scs['sections']).fillna(self.net.shunt.step)
+
+        # update trafo tap changer position
+        sv_ts = self.cim['sv']['SvTapStep'][['TapChanger', 'position']].set_index('TapChanger')
+        self.net.trafo.tap_pos = self.net.trafo[sc['tc_id']].map(sv_ts['position']).fillna(self.net.trafo.tap_pos)
+        self.net.trafo.tap2_pos = self.net.trafo[sc['tc2_id']].map(sv_ts['position']).fillna(self.net.trafo.tap2_pos)
+        self.net.trafo3w.tap_pos = self.net.trafo3w[sc['tc_id']].map(sv_ts['position']).fillna(self.net.trafo3w.tap_pos)
+
+        self.logger.info(f"Needed time for updating the assets: {time.time() - time_start}s")
