c5817325ce77cbf54b54df2c056b4bca8003ef04
David Heck <david.heck@iee.fraunhofer.de>
2026-06-02 15:15:52 +0200

    add GeographicalRegion to cim_classes

--- pandapower/converter/cim/cim2pp/converter_classes/connectivitynodes/connectivityNodesCim16.py
+++ pandapower/converter/cim/cim2pp/converter_classes/connectivitynodes/connectivityNodesCim16.py
@@ -286,8 +286,7 @@ class ConnectivityNodesCim16:
         regions = pd.merge(geo_regions, regions, left_on="rdfId", right_on="Region_SubGeographicalRegion")
         regions = regions.rename(columns={'name': 'GeographicalRegion_name', 'name_SubGeographicalRegion': 'SubGeographicalRegion_name',
                                           'rdfId': 'GeographicalRegion_id', 'rdfId_SubGeographicalRegion': 'SubGeographicalRegion_id'})
-        regions = regions.drop(columns=['name_substation', 'Region_substation', 'Region_SubGeographicalRegion',
-                                        'masUri'])
+        regions = regions.drop(columns=['name_substation', 'Region_substation', 'Region_SubGeographicalRegion'])
         # drop duplicates due to some bugs in several CGMES exports
         regions = regions.drop_duplicates(subset=['rdfId_substation'], keep='first')
         connectivity_nodes = pd.merge(connectivity_nodes, regions, how='left', left_on='Substation', right_on='rdfId_substation')
