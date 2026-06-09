38a3c17864c768b0b085180e5297b708762c4c0c
KS_HTK <2981026+KS-HTK@users.noreply.github.com>
2026-06-09 10:10:45 +0200

    added filter for expected warnings

--- pandapower/test/api/test_file_io.py
+++ pandapower/test/api/test_file_io.py
@@ -614,6 +614,7 @@ def test_multi_index():
     assert_frame_equal(df, df2)
 
 
+@pytest.mark.filterwarnings("ignore:.*ContinuousTapControl2.*:UserWarning")  # ignore warnings that are expected.
 def test_ignore_unknown_objects():
     net = create_kerber_dorfnetz()
     ContinuousTapControl(net, 0, 1.02)
