b88a50d0426266975f82646f316a8b9b615298cd
pawellytaev <pawel.lytaev@uni-kassel.de>
2026-06-22 14:02:50 +0200

    fix tests

--- pandapower/test/converter/test_from_pypowsybl.py
+++ pandapower/test/converter/test_from_pypowsybl.py
@@ -2153,7 +2153,7 @@ def _run_conversion_roundtrip_cycle(
     )
 
     converter = PyPowSyBlConverter()
-    pandapower_network, _, json_path = converter._powsybl_to_pandapower(
+    pandapower_network, _, json_path, _ = converter._powsybl_to_pandapower(
         filename=str(xiidm_path),
         log_static_comparison=False,
         log_loadflow_comparison=False,
@@ -2375,7 +2375,7 @@ def _run_conversion_pandapower_cycle(
 
     converter = PyPowSyBlConverter()
 
-    roundtrip_pp_net, _, json_path = converter._powsybl_to_pandapower(
+    roundtrip_pp_net, _, json_path, _ = converter._powsybl_to_pandapower(
         filename=str(xiidm_path),
         log_static_comparison=False,
         log_loadflow_comparison=False,
