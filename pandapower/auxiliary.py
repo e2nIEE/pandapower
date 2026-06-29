a0f0252679cc5f6686b034c59e71a3922e29b9c4
Mike Vogt <mike.vogt@iee.fraunhofer.de>
2026-06-29 08:45:44 +0200

    typing fixes.

--- pandapower/auxiliary.py
+++ pandapower/auxiliary.py
@@ -67,7 +67,7 @@ except ImportError:
     lightsim2grid_available = False
 
 try:
-    from helmpy.core import helm
+    from helmpy.core import helm  # type: ignore[import-not-found]
     helmpy_available = True
 except ImportError:
     helmpy_available = False
