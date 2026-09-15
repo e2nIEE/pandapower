ade3d99238c29e75aefa305c5ef5a549a4c654df
KS_HTK <2981026+KS-HTK@users.noreply.github.com>
2026-09-15 14:35:50 +0200

    merged develop into v4.0.0 (#3137)

--- pandapower/plotting/patch_makers.py
+++ pandapower/plotting/patch_makers.py
@@ -34,6 +34,9 @@ if TYPE_CHECKING:
     from matplotlib.patches import Patch
     from matplotlib.path import Path
 
+if TYPE_CHECKING:
+    from matplotlib.path import Path
+
 logger = logging.getLogger(__name__)
 
 
